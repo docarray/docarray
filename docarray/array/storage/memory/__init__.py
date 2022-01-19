34ad9e150bfa19a3a469cbc74c99de453edebe01
Han Xiao <han.xiao@jina.ai>
2022-01-19 12:20:19 +0100

    feat(array): add storage backend

--- docarray/array/storage/memory/__init__.py
+++ docarray/array/storage/memory/__init__.py
@@ -1,9 +1,10 @@
 from .backend import MemoryBackendMixin
 from .getsetdel import GetSetDelMixin
 from .seqlike import SequenceLikeMixin
+from abc import ABC
 
 __all__ = ['MemoryStorageMixins']
 
 
-class MemoryStorageMixins(MemoryBackendMixin, GetSetDelMixin, SequenceLikeMixin):
+class MemoryStorageMixins(MemoryBackendMixin, GetSetDelMixin, SequenceLikeMixin, ABC):
     ...
--- docarray/array/storage/sqlite/__init__.py
+++ docarray/array/storage/sqlite/__init__.py
@@ -1,9 +1,10 @@
 from .backend import SqliteBackendMixin
 from .getsetdel import GetSetDelMixin
 from .seqlike import SequenceLikeMixin
+from abc import ABC
 
 __all__ = ['SqliteStorageMixins']
 
 
-class SqliteStorageMixins(SqliteBackendMixin, GetSetDelMixin, SequenceLikeMixin):
+class SqliteStorageMixins(SqliteBackendMixin, GetSetDelMixin, SequenceLikeMixin, ABC):
     ...
