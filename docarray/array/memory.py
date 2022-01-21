c6a6d6f20fcc2993779dd7d827770e580acbb7e6
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-01-21 12:09:36 +0100

    fix: fix set_doc_by_id

--- docarray/array/memory.py
+++ docarray/array/memory.py
@@ -5,11 +5,3 @@ from .storage.memory import StorageMixins
 class DocumentArrayInMemory(StorageMixins, DocumentArray):
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
-"""
-    def __delitem__(self):
-        pass
-    def __getitem__(self, item):
-        print('getting item')
-        pass
-    def __setitem__(self):
-        pass"""
--- docarray/array/storage/sqlite/getsetdel.py
+++ docarray/array/storage/sqlite/getsetdel.py
@@ -27,7 +27,7 @@ class GetSetDelMixin(BaseGetSetDelMixin):
     def _set_doc_by_id(self, _id: str, value: 'Document'):
         self._sql(
             f'UPDATE {self._table_name} SET serialized_value=? WHERE doc_id=?',
-            (_id, value),
+            (value, _id),
         )
         self._commit()
 
