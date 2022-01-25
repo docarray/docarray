d9f1c6fdd13c2ee6468aa028fed1005afd9fdce5
Han Xiao <han.xiao@jina.ai>
2022-01-25 12:55:52 +0100

    style: fix black style

--- docarray/array/document.py
+++ docarray/array/document.py
@@ -1,3 +1,20 @@
-from .base import DocumentArray
+from .base import BaseDocumentArray
+from .mixins import AllMixins
 
-__all__ = ['DocumentArray']
+
+class DocumentArray(AllMixins, BaseDocumentArray):
+    def __new__(cls, *args, storage: str = 'memory', **kwargs):
+        if cls is DocumentArray:
+            if storage == 'memory':
+                from .memory import DocumentArrayInMemory
+
+                instance = super().__new__(DocumentArrayInMemory)
+            elif storage == 'sqlite':
+                from .sqlite import DocumentArraySqlite
+
+                instance = super().__new__(DocumentArraySqlite)
+            else:
+                raise ValueError(f'storage=`{storage}` is not supported.')
+        else:
+            instance = super().__new__(cls)
+        return instance
