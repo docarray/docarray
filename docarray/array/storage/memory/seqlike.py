94b23881f5648c96b79ca7baf09004e4e9b57ffc
samsja <55492238+samsja@users.noreply.github.com>
2022-01-27 14:20:05 +0100

    fix(seqlike): insert use id2offset now
    Co-authored-by: AlaeddineAbdessalem <alaeddine-13@live.fr>

--- docarray/array/storage/memory/seqlike.py
+++ docarray/array/storage/memory/seqlike.py
@@ -60,5 +60,5 @@ class SequenceLikeMixin(MutableSequence[Document]):
     def extend(self, values: Iterable['Document']) -> None:
         self._data.extend(values)
         # extend _id_to_index, no rebuilt needed
-        last_idx = len(self._id_to_index)
+        last_idx = len(self._id2offset)
         self._id_to_index.update({d.id: i + last_idx for i, d in enumerate(values)})
