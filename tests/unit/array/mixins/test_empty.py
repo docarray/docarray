a8c4d07ceb417d74d28406c184bec537f4a7daa5
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-02-01 15:38:42 +0100

    test: test_empty covers weaviate

--- tests/unit/array/mixins/test_empty.py
+++ tests/unit/array/mixins/test_empty.py
@@ -2,10 +2,13 @@ import pytest
 
 from docarray import DocumentArray
 from docarray.array.sqlite import DocumentArraySqlite
+from docarray.array.weaviate import DocumentArrayWeaviate
 
 
-@pytest.mark.parametrize('da_cls', [DocumentArray, DocumentArraySqlite])
-def test_empty_non_zero(da_cls):
+@pytest.mark.parametrize(
+    'da_cls', [DocumentArray, DocumentArraySqlite, DocumentArrayWeaviate]
+)
+def test_empty_non_zero(da_cls, start_weaviate):
     da = DocumentArray.empty(10)
     assert len(da) == 10
     da = DocumentArray.empty()
