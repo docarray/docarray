d8f9fdea75c1ac226f7505b5cde70aeeb5c6cc7e
David Buchaca Prats <davidbuchaca@gmail.com>
2022-02-03 17:44:55 +0100

    refactor: generalize find to all weaviate types

--- docarray/array/storage/weaviate/find.py
+++ docarray/array/storage/weaviate/find.py
@@ -32,7 +32,7 @@ class FindMixin:
     def find(self, query: 'WeaviateArrayType'):
         ...
 
-    def _find_similar_vectors(self, q, limit=10):
+    def _find_similar_vectors(self, q: 'WeaviateArrayType', limit=10):
         query_dict = {'vector': q}
         results = (
             self._client.query.get(
@@ -59,16 +59,45 @@ class FindMixin:
 
         return DocumentArray(docs)
 
-    def find(self, query, limit=10):
-        """
+    def _check_single_query(self, query: 'WeaviateArrayType'):
+        """Return whether query contains a single or multiple queries across all compatible Weaviate types
 
-        Weaviate returns `certainty` values. To get cosine similarities one needs to use `cosine_sim = 2*certainty - 1`
+        Examples
 
-        https://www.semi.technology/developers/weaviate/current/more-resources/faq.html#q-how-do-i-get-the-cosine-similarity-from-weaviates-certainty
+        >>> _check_single_query([1,2,3])
+        True
+        >>> _check_single_query([[1,2,3],[4,5,6]])
+        Flase
+        >>> _check_single_query(np.array([1,2,3]))
+        True
+        >>> _check_single_query(np.array([[1,2,3], [4,5,6]]))
+        False
+        >>> _check_single_query(tf.constant([1,2,3]))
+        True
+        >>> _check_single_query(tf.constant([[1.0, 2.0], [3.0, 4.0]]))
+        False
         """
-        if query.ndim == 1:
+        if isinstance(query, (np.ndarray, torch.Tensor, tf.Tensor)):
+            return True if query.ndim >= 2 else False
+        if isinstance(query, list, tuple):
+            return True if isinstance(q[0], (list, tuple)) else False
+
+    def find(
+        self, query: 'WeaviateArrayType', limit=10
+    ) -> Union[DocumentArray, List[DocumentArray]]:
+        """
+        :param query: input supported to be stored in Weaviate. This includes any from the list '[np.ndarray, tensorflow.Tensor, torch.Tensor, Sequence[float]]'
+        :param limit: number of retrieved items
+
+        :return: DocumentArray containing the closest documents to the query if it is a single query, otherwise a list of DocumentArrays containing
+           the closest Document objects for each of the queries in `query`.
+
+        Note: Weaviate returns `certainty` values. To get cosine similarities one needs to use `cosine_sim = 2*certainty - 1` as explained here:
+                  https://www.semi.technology/developers/weaviate/current/more-resources/faq.html#q-how-do-i-get-the-cosine-similarity-from-weaviates-certainty
+        """
+        if self._check_single_query(query):
             return self._find_similar_vectors(query, limit=limit)
-        elif query.ndim == 2:
+        else:
             closest_docs = []
             for q in query:
                 da = self._find_similar_vectors(q, limit=limit)
