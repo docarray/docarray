ab7be5fc5a40ddc02306ed5c96ed05b6c7692261
David Buchaca Prats <davidbuchaca@gmail.com>
2022-02-03 08:47:49 +0100

    feat: allow empty to get kwargs

--- docarray/array/mixins/empty.py
+++ docarray/array/mixins/empty.py
@@ -10,11 +10,11 @@ class EmptyMixin:
     """Helper functions for building arrays with empty Document."""
 
     @classmethod
-    def empty(cls: Type['T'], size: int = 0) -> 'T':
+    def empty(cls: Type['T'], size: int = 0, *args, **kwargs) -> 'T':
         """Create a :class:`DocumentArray`  object with :attr:`size` empty
         :class:`Document` objects.
 
         :param size: the number of empty Documents in this container
         :return: a :class:`DocumentArray` object
         """
-        return cls(Document() for _ in range(size))
+        return cls((Document() for _ in range(size)), *args, **kwargs)
