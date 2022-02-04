a73669511247689b97a3617b102f7c16bd6f6baf
David Buchaca Prats <davidbuchaca@gmail.com>
2022-02-04 19:48:08 +0100

    refactor: simplify tests removing if else branches

--- tests/unit/array/mixins/test_io.py
+++ tests/unit/array/mixins/test_io.py
@@ -21,22 +21,17 @@ def docs():
 @pytest.mark.parametrize(
     'da_cls,config',
     [
-        (DocumentArrayInMemory, None),
-        (DocumentArraySqlite, None),
-        (DocumentArrayWeaviate, WeaviateConfig(n_dim=10)),
+        (DocumentArrayInMemory, lambda: None),
+        (DocumentArraySqlite, lambda: None),
+        (DocumentArrayWeaviate, lambda: WeaviateConfig(n_dim=10)),
     ],
 )
 def test_document_save_load(docs, method, tmp_path, da_cls, config, start_weaviate):
     tmp_file = os.path.join(tmp_path, 'test')
-    if config:
-        da = da_cls(docs, config=config)
-    else:
-        da = da_cls(docs)
+    da = da_cls(docs, config=config())
     da.save(tmp_file, file_format=method)
-    if config:
-        da_r = type(da).load(tmp_file, file_format=method, config=config)
-    else:
-        da_r = type(da).load(tmp_file, file_format=method)
+
+    da_r = type(da).load(tmp_file, file_format=method, config=config())
 
     assert type(da) is type(da_r)
     assert len(da) == len(da_r)
@@ -50,17 +45,14 @@ def test_document_save_load(docs, method, tmp_path, da_cls, config, start_weavia
 @pytest.mark.parametrize(
     'da_cls,config',
     [
-        (DocumentArrayInMemory, None),
-        (DocumentArraySqlite, None),
-        (DocumentArrayWeaviate, WeaviateConfig(n_dim=10)),
+        (DocumentArrayInMemory, lambda: None),
+        (DocumentArraySqlite, lambda: None),
+        (DocumentArrayWeaviate, lambda: WeaviateConfig(n_dim=10)),
     ],
 )
 def test_da_csv_write(docs, flatten_tags, tmp_path, da_cls, config, start_weaviate):
     tmpfile = os.path.join(tmp_path, 'test.csv')
-    if config:
-        da = da_cls(docs, config=config)
-    else:
-        da = da_cls(docs)
+    da = da_cls(docs, config=config())
     da.save_csv(tmpfile, flatten_tags)
     with open(tmpfile) as fp:
         assert len([v for v in fp]) == len(da) + 1
@@ -69,16 +61,13 @@ def test_da_csv_write(docs, flatten_tags, tmp_path, da_cls, config, start_weavia
 @pytest.mark.parametrize(
     'da_cls,config',
     [
-        (DocumentArrayInMemory, None),
-        (DocumentArraySqlite, None),
-        (DocumentArrayWeaviate, WeaviateConfig(n_dim=256)),
+        (DocumentArrayInMemory, lambda: None),
+        (DocumentArraySqlite, lambda: None),
+        (DocumentArrayWeaviate, lambda: WeaviateConfig(n_dim=256)),
     ],
 )
 def test_from_ndarray(da_cls, config, start_weaviate):
-    if config:
-        _da = da_cls.from_ndarray(np.random.random([10, 256]), config=config)
-    else:
-        _da = da_cls.from_ndarray(np.random.random([10, 256]))
+    _da = da_cls.from_ndarray(np.random.random([10, 256]), config=config())
 
     assert len(_da) == 10
 
@@ -86,23 +75,18 @@ def test_from_ndarray(da_cls, config, start_weaviate):
 @pytest.mark.parametrize(
     'da_cls,config',
     [
-        (DocumentArrayInMemory, None),
-        (DocumentArraySqlite, None),
-        (DocumentArrayWeaviate, WeaviateConfig(n_dim=256)),
+        (DocumentArrayInMemory, lambda: None),
+        (DocumentArraySqlite, lambda: None),
+        (DocumentArrayWeaviate, lambda: WeaviateConfig(n_dim=256)),
     ],
 )
 def test_from_files(da_cls, config, start_weaviate):
-    if config:
-        assert (
-            len(
-                da_cls.from_files(
-                    patterns='*.*', to_dataturi=True, size=1, config=config
-                )
-            )
-            == 1
+    assert (
+        len(
+            da_cls.from_files(patterns='*.*', to_dataturi=True, size=1, config=config())
         )
-    else:
-        assert len(da_cls.from_files(patterns='*.*', to_dataturi=True, size=1)) == 1
+        == 1
+    )
 
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
@@ -111,51 +95,39 @@ cur_dir = os.path.dirname(os.path.abspath(__file__))
 @pytest.mark.parametrize(
     'da_cls,config',
     [
-        (DocumentArrayInMemory, None),
-        (DocumentArraySqlite, None),
-        (DocumentArrayWeaviate, WeaviateConfig(n_dim=256)),
+        (DocumentArrayInMemory, lambda: None),
+        (DocumentArraySqlite, lambda: None),
+        (DocumentArrayWeaviate, lambda: WeaviateConfig(n_dim=256)),
     ],
 )
 def test_from_ndjson(da_cls, config, start_weaviate):
     with open(os.path.join(cur_dir, 'docs.jsonlines')) as fp:
-        if config:
-            _da = da_cls.from_ndjson(fp, config=config)
-        else:
-            _da = da_cls.from_ndjson(fp)
+        _da = da_cls.from_ndjson(fp, config=config())
         assert len(_da) == 2
 
 
 @pytest.mark.parametrize(
     'da_cls, config',
     [
-        (DocumentArrayInMemory, None),
-        (DocumentArraySqlite, None),
+        (DocumentArrayInMemory, lambda: None),
+        (DocumentArraySqlite, lambda: None),
         (DocumentArrayWeaviate, lambda: WeaviateConfig(n_dim=3)),
     ],
 )
 def test_from_to_pd_dataframe(da_cls, config, start_weaviate):
 
-    if config:
-        df = da_cls.empty(2, config=config()).to_dataframe()
-        assert len(da_cls.from_dataframe(df, config=config())) == 2
-    else:
-        assert len(da_cls.from_dataframe(da_cls.empty(2).to_dataframe())) == 2
+    df = da_cls.empty(2, config=config()).to_dataframe()
+    assert len(da_cls.from_dataframe(df, config=config())) == 2
 
     # more complicated
-    if config:
-        da = da_cls.empty(2, config=config())
-    else:
-        da = da_cls.empty(2)
+    da = da_cls.empty(2, config=config())
 
     da[:, 'embedding'] = [[1, 2, 3], [4, 5, 6]]
     da[:, 'tensor'] = [[1, 2], [2, 1]]
     da[0, 'tags'] = {'hello': 'world'}
     df = da.to_dataframe()
 
-    if config:
-        da2 = da_cls.from_dataframe(df, config=config())
-    else:
-        da2 = da_cls.from_dataframe(df)
+    da2 = da_cls.from_dataframe(df, config=config())
 
     assert da2[0].tags == {'hello': 'world'}
     assert da2[1].tags == {}
@@ -188,16 +160,14 @@ def test_from_to_bytes(da_cls, config, start_weaviate):
 @pytest.mark.parametrize(
     'da_cls, config',
     [
-        (DocumentArrayInMemory, None),
-        (DocumentArraySqlite, None),
+        (DocumentArrayInMemory, lambda: None),
+        (DocumentArraySqlite, lambda: None),
         (DocumentArrayWeaviate, lambda: WeaviateConfig(n_dim=256)),
     ],
 )
 def test_push_pull_io(da_cls, config, show_progress, start_weaviate):
-    if config:
-        da1 = da_cls.empty(10, config=config())
-    else:
-        da1 = da_cls.empty(10)
+
+    da1 = da_cls.empty(10, config=config())
 
     da1[:, 'embedding'] = np.random.random([len(da1), 256])
     random_texts = [str(uuid.uuid1()) for _ in da1]
@@ -205,10 +175,7 @@ def test_push_pull_io(da_cls, config, show_progress, start_weaviate):
 
     da1.push('myda', show_progress=show_progress)
 
-    if config:
-        da2 = da_cls.pull('myda', show_progress=show_progress, config=config())
-    else:
-        da2 = da_cls.pull('myda', show_progress=show_progress)
+    da2 = da_cls.pull('myda', show_progress=show_progress, config=config())
 
     assert len(da1) == len(da2) == 10
     assert da1.texts == da2.texts == random_texts
