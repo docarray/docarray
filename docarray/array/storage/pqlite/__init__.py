9bbbcb62aeb27d6d422098c0a3d3ea237232e2d6
David Buchaca Prats <davidbuchaca@gmail.com>
2022-02-04 15:50:54 +0100

    feat: add find method pqlite

--- docarray/array/storage/pqlite/__init__.py
+++ docarray/array/storage/pqlite/__init__.py
@@ -3,9 +3,10 @@ from abc import ABC
 from .backend import BackendMixin, PqliteConfig
 from .getsetdel import GetSetDelMixin
 from .seqlike import SequenceLikeMixin
+from .find import FindMixin
 
 __all__ = ['StorageMixins', 'PqliteConfig']
 
 
-class StorageMixins(BackendMixin, GetSetDelMixin, SequenceLikeMixin, ABC):
+class StorageMixins(BackendMixin, GetSetDelMixin, SequenceLikeMixin, FindMixin):
     ...
---
+++ docarray/array/storage/pqlite/find.py
@@ -0,0 +1,70 @@
+import numpy as np
+from typing import (
+    Union,
+    TYPE_CHECKING,
+    TypeVar,
+    Sequence,
+    Optional,
+    List,
+    Iterable,
+    Tuple,
+    overload,
+)
+
+from .... import Document, DocumentArray
+from ....math import ndarray
+
+if TYPE_CHECKING:
+    import tensorflow
+    import torch
+    import numpy as np
+
+    PQLiteArrayType = TypeVar(
+        'PQLiteArrayType',
+        np.ndarray,
+    )
+
+
+class FindMixin:
+    @overload
+    def find(self, query: 'PQLiteArrayType'):
+        ...
+
+    def _find_similar_vectors(self, q: 'PQLiteArrayType', limit=10):
+
+        """
+        if q.ndim == 1:
+            input = DocumentArray(Document(embedding=q))
+        else:
+            input = DocumentArray(Document(embedding=q_k) for q_k in q)
+        docs = self._pqlite.search(input, limit=limit)
+        return DocumentArray(docs)
+        """
+
+        if q.ndim == 1:
+            q = q.reshape((1, -1))
+
+        _, list_of_docs = self._pqlite._search_documents(q, limit=limit)
+
+        if len(list_of_docs) == 1:
+            # this is a single DocumentArray
+            return list_of_docs[0]
+        else:
+            # this is a list of DocumentArrays
+            return list_of_docs
+
+    def find(
+        self, query: 'PQLiteArrayType', limit=10
+    ) -> Union[DocumentArray, List[DocumentArray]]:
+        """Returns approximate nearest neighbors given a batch of input queries.
+        :param query: input supported to be stored in Weaviate. This includes any from the list '[np.ndarray, tensorflow.Tensor, torch.Tensor, Sequence[float]]'
+        :param limit: number of retrieved items
+
+        :return: DocumentArray containing the closest documents to the query if it is a single query, otherwise a list of DocumentArrays containing
+           the closest Document objects for each of the queries in `query`.
+
+        Note: Weaviate returns `certainty` values. To get cosine similarities one needs to use `cosine_sim = 2*certainty - 1` as explained here:
+                  https://www.semi.technology/developers/weaviate/current/more-resources/faq.html#q-how-do-i-get-the-cosine-similarity-from-weaviates-certainty
+        """
+
+        return self._find_similar_vectors(query, limit=limit)
