7706b2b7864b38b7e9d269d6d336a70e693dd634
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-02-04 21:19:15 +0100

    test: fix test_contruct

--- tests/unit/array/test_construct.py
+++ tests/unit/array/test_construct.py
@@ -7,47 +7,77 @@ from docarray.array.weaviate import DocumentArrayWeaviate, WeaviateConfig
 
 
 def test_construct_docarray_weaviate(start_weaviate):
-    daw = DocumentArrayWeaviate()
+    daw = DocumentArrayWeaviate(config=WeaviateConfig(n_dim=10))
     daw.extend([Document(text='a'), Document(text='b'), Document(text='c')])
     name = daw.name
     del daw
 
-    daw2 = DocumentArrayWeaviate(config=WeaviateConfig(name=name))
+    daw2 = DocumentArrayWeaviate(config=WeaviateConfig(n_dim=10, name=name))
     assert len(daw2) == 3
     assert daw2.texts == ['a', 'b', 'c']
 
 
 @pytest.mark.parametrize(
-    'da_cls', [DocumentArrayInMemory, DocumentArrayWeaviate, DocumentArraySqlite]
+    'da_cls,config',
+    [
+        (DocumentArrayInMemory, None),
+        (DocumentArraySqlite, None),
+        (DocumentArrayWeaviate, WeaviateConfig(n_dim=128)),
+    ],
 )
-def test_construct_docarray(da_cls, start_weaviate):
-    da = da_cls()
-    assert len(da) == 0
+def test_construct_docarray(da_cls, config, start_weaviate):
+    if config:
+        da = da_cls(config=config)
+        assert len(da) == 0
 
-    da = da_cls(Document())
-    assert len(da) == 1
+        da = da_cls(Document(), config=config)
+        assert len(da) == 1
 
-    da = da_cls([Document(), Document()])
-    assert len(da) == 2
+        da = da_cls([Document(), Document()], config=config)
+        assert len(da) == 2
 
-    da = da_cls((Document(), Document()))
-    assert len(da) == 2
+        da = da_cls((Document(), Document()), config=config)
+        assert len(da) == 2
 
-    da = da_cls((Document() for _ in range(10)))
-    assert len(da) == 10
+        da = da_cls((Document() for _ in range(10)), config=config)
+        assert len(da) == 10
+    else:
+        da = da_cls()
+        assert len(da) == 0
 
-    if da_cls is DocumentArrayInMemory:
-        da1 = da_cls(da)
-        assert len(da1) == 10
+        da = da_cls(Document())
+        assert len(da) == 1
+
+        da = da_cls([Document(), Document()])
+        assert len(da) == 2
+
+        da = da_cls((Document(), Document()))
+        assert len(da) == 2
+
+        da = da_cls((Document() for _ in range(10)))
+        assert len(da) == 10
+
+        if da_cls is DocumentArrayInMemory:
+            da1 = da_cls(da)
+            assert len(da1) == 10
 
 
 @pytest.mark.parametrize(
-    'da_cls', [DocumentArrayInMemory, DocumentArraySqlite, DocumentArrayWeaviate]
+    'da_cls,config',
+    [
+        (DocumentArrayInMemory, None),
+        (DocumentArraySqlite, None),
+        (DocumentArrayWeaviate, WeaviateConfig(n_dim=128)),
+    ],
 )
 @pytest.mark.parametrize('is_copy', [True, False])
-def test_docarray_copy_singleton(da_cls, is_copy, start_weaviate):
+def test_docarray_copy_singleton(da_cls, config, is_copy, start_weaviate):
     d = Document()
-    da = da_cls(d, copy=is_copy)
+    if config:
+        da = da_cls(d, copy=is_copy, config=config)
+    else:
+        da = da_cls(d, copy=is_copy)
+
     d.id = 'hello'
     if da_cls == DocumentArrayInMemory:
         if is_copy:
@@ -59,13 +89,21 @@ def test_docarray_copy_singleton(da_cls, is_copy, start_weaviate):
 
 
 @pytest.mark.parametrize(
-    'da_cls', [DocumentArrayInMemory, DocumentArraySqlite, DocumentArrayWeaviate]
+    'da_cls,config',
+    [
+        (DocumentArrayInMemory, None),
+        (DocumentArraySqlite, None),
+        (DocumentArrayWeaviate, WeaviateConfig(n_dim=128)),
+    ],
 )
 @pytest.mark.parametrize('is_copy', [True, False])
-def test_docarray_copy_da(da_cls, is_copy, start_weaviate):
+def test_docarray_copy_da(da_cls, config, is_copy, start_weaviate):
     d1 = Document()
     d2 = Document()
-    da = da_cls([d1, d2], copy=is_copy)
+    if config:
+        da = da_cls([d1, d2], copy=is_copy, config=config)
+    else:
+        da = da_cls([d1, d2], copy=is_copy)
     d1.id = 'hello'
     if da_cls == DocumentArrayInMemory:
         if is_copy:
