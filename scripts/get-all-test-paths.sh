58794c88c97326ec23c4a978e1c50e1c1acd7199
winstonww <winstonwongww2@gmail.com>
2022-02-17 10:43:20 +0800

    test: reduce test time

--- scripts/get-all-test-paths.sh
+++ scripts/get-all-test-paths.sh
@@ -2,8 +2,12 @@
 
 set -ex
 
-declare -a array1=( "tests/unit/*.py")
-declare -a array2=( $(ls -d tests/unit/*/ | grep -v '__pycache__' ))
-dest=( "${array1[@]}" "${array2[@]}" )
+BATCH_SIZE=5
+declare -a array1=( "tests/unit/*.py" )
+declare -a array2=( $(ls -d tests/unit/*/ | grep -v '__pycache__' | grep -v 'array') )
+declare -a array3=( "tests/unit/array/*.py" )
+declare -a mixins=( $(find tests/unit/array/mixins -name "*.py") )
+declare -a array4=( "$(echo "${mixins[@]}" | xargs -n$BATCH_SIZE)" )
+dest=( "${array1[@]}" "${array2[@]}" "${array3[@]}" "${array4[@]}" )
 
 printf '%s\n' "${dest[@]}" | jq -R . | jq -cs .
