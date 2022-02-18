43a7455c2f41987b7a03742384d350537825899c
winstonww <winstonwongww2@gmail.com>
2022-02-18 16:37:31 +0800

    refactor: add more paths

--- scripts/get-all-test-paths.sh
+++ scripts/get-all-test-paths.sh
@@ -8,6 +8,9 @@ declare -a array2=( $(ls -d tests/unit/*/ | grep -v '__pycache__' | grep -v 'arr
 declare -a array3=( "tests/unit/array/*.py" )
 declare -a mixins=( $(find tests/unit/array/mixins -name "*.py") )
 declare -a array4=( "$(echo "${mixins[@]}" | xargs -n$BATCH_SIZE)" )
-dest=( "${array1[@]}" "${array2[@]}" "${array3[@]}" "${array4[@]}" )
+# array5 is currently empty because in the array/ directory, mixins is the only directory
+# but add the following in case new directories are created in array/
+declare -a array5=( $(ls -d tests/unit/array/*/ | grep -v '__pycache__' | grep -v 'mixins') )
+dest=( "${array1[@]}" "${array2[@]}" "${array3[@]}" "${array4[@]}" "${array5[@]}" )
 
 printf '%s\n' "${dest[@]}" | jq -R . | jq -cs .
