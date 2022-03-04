e6657cbf570bf4df6c100b7f69a2108a1c2e642d
numb3r3 <wangfelix87@gmail.com>
2022-03-04 18:32:08 +0800

    fix: unittest

--- tests/unit/array/mixins/test_filter.py
+++ tests/unit/array/mixins/test_filter.py
@@ -15,37 +15,35 @@ def docs():
 
 
 def test_empty_filter(docs):
-    result = docs._filter({})
+    result = docs.find({})
     assert len(result) == 5
 
 
 def test_sample_filter(docs):
-    result = docs._filter({'text': {'$eq': 'hello'}})
+    result = docs.find({'text': {'$eq': 'hello'}})
     assert len(result) == 1
     assert result[0].text == 'hello'
 
-    result = docs._filter({'tags__x': {'$gte': 0.5}})
+    result = docs.find({'tags__x': {'$gte': 0.5}})
     assert len(result) == 1
     assert result[0].tags['x'] == 0.8
 
 
 def test_logic_filter(docs):
-    result = docs._filter({'$or': {'tags__x': {'$gte': 0.1}, 'tags__y': {'$gte': 0.5}}})
+    result = docs.find({'$or': {'tags__x': {'$gte': 0.1}, 'tags__y': {'$gte': 0.5}}})
     assert len(result) == 2
     assert result[0].tags['x'] == 0.3 and result[1].tags['x'] == 0.8
 
-    result = docs._filter(
+    result = docs.find(
         {'$or': {'tags__x': {'$gte': 0.1}, 'tags__y': {'$gte': 0.5}}}, limit=1
     )
     assert len(result) == 1
     assert result[0].tags['x'] == 0.3
 
-    result = docs._filter({'tags__x': {'$gte': 0.1, '$lte': 0.5}})
+    result = docs.find({'tags__x': {'$gte': 0.1, '$lte': 0.5}})
     assert len(result) == 1
     assert result[0].tags['y'] == 0.6
 
-    result = docs._filter(
-        {'$and': {'tags__x': {'$gte': 0.1}, 'tags__y': {'$gte': 0.5}}}
-    )
+    result = docs.find({'$and': {'tags__x': {'$gte': 0.1}, 'tags__y': {'$gte': 0.5}}})
     assert len(result) == 1
     assert result[0].tags['y'] == 0.6
