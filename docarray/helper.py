13a578b70095916e17aa725839338d836a1b92ef
Han Xiao <han.xiao@jina.ai>
2022-03-09 09:15:29 +0100

    refactor: use rich progressbar disable arg

--- docarray/helper.py
+++ docarray/helper.py
@@ -178,6 +178,30 @@ def get_full_version() -> Dict:
     }
 
 
+assigned_ports = set()
+unassigned_ports = []
+DEFAULT_MIN_PORT = 49153
+MAX_PORT = 65535
+
+
+def reset_ports():
+    def _get_unassigned_ports():
+        # if we are running out of ports, lower default minimum port
+        if MAX_PORT - DEFAULT_MIN_PORT - len(assigned_ports) < 100:
+            min_port = int(os.environ.get('JINA_RANDOM_PORT_MIN', '16384'))
+        else:
+            min_port = int(
+                os.environ.get('JINA_RANDOM_PORT_MIN', str(DEFAULT_MIN_PORT))
+            )
+        max_port = int(os.environ.get('JINA_RANDOM_PORT_MAX', str(MAX_PORT)))
+        return set(range(min_port, max_port + 1)) - set(assigned_ports)
+
+    unassigned_ports.clear()
+    assigned_ports.clear()
+    unassigned_ports.extend(_get_unassigned_ports())
+    random.shuffle(unassigned_ports)
+
+
 def random_port() -> Optional[int]:
     """
     Get a random available port number.
