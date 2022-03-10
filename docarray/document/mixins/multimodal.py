74f8fe17bccc64752ff089e856cb1348bb0e7343
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-03-10 13:49:40 +0100

    fix: fix iterable multi modal annotation

--- docarray/document/mixins/multimodal.py
+++ docarray/document/mixins/multimodal.py
@@ -8,7 +8,7 @@ from docarray.types import ImageDocument, BlobDocument, TextDocument
 class MultiModalMixin:
     @classmethod
     def from_dataclass(cls, obj):
-        if not is_dataclass:
+        if not is_dataclass(obj):
             raise ValueError('Object is not a dataclass instance')
 
         from docarray import Document
@@ -17,14 +17,8 @@ class MultiModalMixin:
         tags = {}
         for position, (key, field) in enumerate(obj.__dataclass_fields__.items()):
             attribute = getattr(obj, key)
-            if is_dataclass(field.type):
-                root.chunks.append(cls.from_dataclass(attribute))
-            elif field.type == ImageDocument:
-                root.chunks.append(Document(tensor=attribute, modality='image'))
-            elif field.type == BlobDocument:
-                root.chunks.append(Document(blob=attribute, modality='blob'))
-            elif field.type == TextDocument:
-                root.chunks.append(Document(text=attribute, modality='text'))
+            if field.type in [str, int, float]:
+                tags[key] = attribute
             elif isinstance(field.type, typing._GenericAlias):
                 if field.type._name in ['List', 'Iterable']:
                     sub_type = field.type.__args__[0]
@@ -33,12 +27,30 @@ class MultiModalMixin:
                     else:
                         chunk = Document()
                         for element in attribute:
-                            chunk.chunks.append(cls.from_dataclass(element))
-
-            elif field.type in [str, int, float]:
-                tags[key] = attribute
+                            chunk.chunks.append(cls._from_obj(element, sub_type))
+                        root.chunks.append(chunk)
+                else:
+                    raise ValueError(f'Unsupported type annotation {field.type._name}')
+            else:
+                root.chunks.append(cls._from_obj(attribute, field.type))
 
         # TODO: may have to modify this?
         root.tags = tags
 
         return root
+
+    @classmethod
+    def _from_obj(cls, obj, obj_type):
+        from docarray import Document
+
+        if is_dataclass(obj_type):
+            doc = cls.from_dataclass(obj)
+        elif obj_type == ImageDocument:
+            doc = Document(tensor=obj, modality='image')
+        elif obj_type == BlobDocument:
+            doc = Document(blob=obj, modality='blob')
+        elif obj_type == TextDocument:
+            doc = Document(text=obj, modality='text')
+        else:
+            raise ValueError(f'Unsupported type annotation')
+        return doc
