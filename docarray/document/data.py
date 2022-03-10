d40a3358f904fded03aa52192e12a604f7df1798
Han Xiao <han.xiao@jina.ai>
2022-03-10 12:35:55 +0100

    perf: optimize Document init

--- docarray/document/data.py
+++ docarray/document/data.py
@@ -35,7 +35,9 @@ _all_mime_types = set(mimetypes.types_map.values())
 @dataclass(unsafe_hash=True)
 class DocumentData:
     _reference_doc: 'Document' = field(hash=False, compare=False)
-    id: str = field(default_factory=random.getrandbits(128).to_bytes(16, 'big').hex)
+    id: str = field(
+        default_factory=lambda: random.getrandbits(128).to_bytes(16, 'big').hex()
+    )
     parent_id: Optional[str] = None
     granularity: Optional[int] = None
     adjacency: Optional[int] = None
