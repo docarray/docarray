c5002b4c5676bf2cfa595faa7143c7b4ce9fdc48
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-03-10 16:02:03 +0100

    feat: store multi modal schema in meta_tags

--- docarray/document/mixins/multimodal.py
+++ docarray/document/mixins/multimodal.py
@@ -1,8 +1,17 @@
 from dataclasses import is_dataclass
 
 import typing
+from enum import Enum
 
 from docarray.types import ImageDocument, BlobDocument, TextDocument
+
+
+class AttributeType(Enum):
+    DOCUMENT = 1
+    PRIMITIVE = 2
+    ITERABLE_PRIMITIVE = 3
+    ITERABLE_DOCUMENT = 4
+    NESTED = 5
 
 
 class MultiModalMixin:
@@ -15,36 +24,63 @@ class MultiModalMixin:
 
         root = Document()
         tags = {}
-        for position, (key, field) in enumerate(obj.__dataclass_fields__.items()):
+        multi_modal_schema = {}
+        for key, field in obj.__dataclass_fields__.items():
             attribute = getattr(obj, key)
             if field.type in [str, int, float]:
                 tags[key] = attribute
+                multi_modal_schema[key] = {
+                    'attribute_type': AttributeType.PRIMITIVE,
+                    'type': field.type.__name__,
+                }
+
             elif isinstance(field.type, typing._GenericAlias):
                 if field.type._name in ['List', 'Iterable']:
                     sub_type = field.type.__args__[0]
                     if sub_type in [str, int, float]:
                         tags[key] = attribute
+                        multi_modal_schema[key] = {
+                            'attribute_type': AttributeType.ITERABLE_PRIMITIVE,
+                            'type': field.type.__name__,
+                        }
+
                     else:
                         chunk = Document()
                         for element in attribute:
-                            chunk.chunks.append(cls._from_obj(element, sub_type))
+                            doc, _ = cls._from_obj(element, sub_type)
+                            chunk.chunks.append(doc)
+                        multi_modal_schema[key] = {
+                            'attribute_type': AttributeType.ITERABLE_DOCUMENT,
+                            'type': field.type.__name__,
+                            'position': len(root.chunks),
+                        }
                         root.chunks.append(chunk)
                 else:
                     raise ValueError(f'Unsupported type annotation {field.type._name}')
             else:
-                root.chunks.append(cls._from_obj(attribute, field.type))
+                doc, attribute_type = cls._from_obj(attribute, field.type)
+                multi_modal_schema[key] = {
+                    'attribute_type': attribute_type,
+                    'type': field.type.__name__,
+                    'position': len(root.chunks),
+                }
+                root.chunks.append(doc)
 
         # TODO: may have to modify this?
         root.tags = tags
+        root.meta_tags['multi_modal_schema'] = multi_modal_schema
 
         return root
 
     @classmethod
-    def _from_obj(cls, obj, obj_type):
+    def _from_obj(cls, obj, obj_type) -> typing.Tuple['Document', AttributeType]:
         from docarray import Document
+
+        attribute_type = AttributeType.DOCUMENT
 
         if is_dataclass(obj_type):
             doc = cls.from_dataclass(obj)
+            attribute_type = AttributeType.NESTED
         elif obj_type == ImageDocument:
             doc = Document(tensor=obj, modality='image')
         elif obj_type == BlobDocument:
@@ -53,4 +89,4 @@ class MultiModalMixin:
             doc = Document(text=obj, modality='text')
         else:
             raise ValueError(f'Unsupported type annotation')
-        return doc
+        return doc, attribute_type
