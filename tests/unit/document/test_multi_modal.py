f084c0d5ba78467a20838d9eaecc6bd4bfd653e2
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-03-10 09:10:12 +0100

    test: add multi modal nested test

--- tests/unit/document/test_multi_modal.py
+++ tests/unit/document/test_multi_modal.py
@@ -1,5 +1,5 @@
 from docarray import Document
-from docarray.types import TextDocument, ImageDocument
+from docarray.types import TextDocument, ImageDocument, BlobDocument
 from dataclasses import dataclass
 import pytest
 import numpy as np
@@ -20,8 +20,48 @@ def test_simple():
 
 
 def test_nested():
-    ...
+    @dataclass
+    class SubDocument:
+        audio: BlobDocument
+        date: str
+        version: float
+
+    @dataclass
+    class MMDocument:
+        sub_doc: SubDocument
+        image: ImageDocument
+        value: str
+
+    obj = MMDocument(
+        sub_doc=SubDocument(audio=b'1234', date='10.03.2022', version=1.5),
+        image=np.random.rand(10, 10, 3),
+        value='abc',
+    )
+
+    doc = Document.from_dataclass(obj)
+    assert doc.tags['value'] == 'abc'
+
+    assert doc.chunks[0].tags['date'] == '10.03.2022'
+    assert doc.chunks[0].tags['version'] == 1.5
+    assert doc.chunks[0].chunks[0].blob == b'1234'
+
+    assert doc.chunks[1].tensor.shape == (10, 10, 3)
 
 
 def test_with_tags():
+    @dataclass
+    class MMDocument:
+        attr1: str
+        attr2: int
+        attr3: float
+
+    obj = MMDocument(attr1='123', attr2=10, attr3=1.1)
+
+    doc = Document.from_dataclass(obj)
+    assert doc.tags['attr1'] == '123'
+    assert doc.tags['attr2'] == 10
+    assert doc.tags['attr3'] == 1.1
+
+
+def test_iterable():
     ...
