3380b3be1ae5317202c5f8d9c23c813f2eb2fd70
Han Xiao <han.xiao@jina.ai>
2022-03-16 11:15:24 +0100

    fix: ws scheme (#210)

--- tests/unit/array/mixins/test_post.py
+++ tests/unit/array/mixins/test_post.py
@@ -12,7 +12,7 @@ from docarray.helper import random_port
     [
         (dict(protocol='grpc'), 'grpc://127.0.0.1:$port/'),
         (dict(protocol='grpc'), 'grpc://127.0.0.1:$port'),
-        (dict(protocol='websocket'), 'websocket://127.0.0.1:$port'),
+        (dict(protocol='websocket'), 'ws://127.0.0.1:$port'),
         # (dict(protocol='http'), 'http://127.0.0.1:$port'),  this somehow does not work on GH workflow
     ],
 )
