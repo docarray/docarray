5fb183fc538a74747a055843e8076850fe71fd0f
Han Xiao <han.xiao@jina.ai>
2022-03-30 15:09:44 +0200

    feat(array): qps show in pbar (#240)

--- docarray/array/mixins/io/pbar.py
+++ docarray/array/mixins/io/pbar.py
@@ -5,15 +5,33 @@ from rich.progress import (
     MofNCompleteColumn,
     TextColumn,
     TimeRemainingColumn,
+    Text,
 )
 
 
-def get_progressbar(description, disable, total):
-    progress = Progress(
+class QPSColumn(TextColumn):
+    def render(self, task) -> Text:
+        if task.speed:
+            _text = f'{task.speed:.0f} QPS'
+        else:
+            _text = 'unknown'
+        if self.markup:
+            text = Text.from_markup(_text, style=self.style, justify=self.justify)
+        else:
+            text = Text(_text, style=self.style, justify=self.justify)
+        if self.highlighter:
+            self.highlighter.highlight(text)
+        return text
+
+
+def get_pbar(disable):
+    return Progress(
         SpinnerColumn(),
-        TextColumn(description),
+        TextColumn('[bold]{task.description}'),
         BarColumn(),
         MofNCompleteColumn(),
+        '•',
+        QPSColumn('{task.speed} QPS', justify='right', style='progress.data.speed'),
         '•',
         TimeRemainingColumn(),
         '•',
@@ -25,5 +43,9 @@ def get_progressbar(description, disable, total):
         transient=True,
         disable=disable,
     )
+
+
+def get_progressbar(description, disable, total):
+    progress = get_pbar(disable)
     task = progress.add_task(description, total=total, start=False, total_size=0)
     return progress, task
