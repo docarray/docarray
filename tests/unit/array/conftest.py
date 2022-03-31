0cae644fc937452d36e3fbf584dd0f2657dbcd89
AlaeddineAbdessalem <alaeddine-13@live.fr>
2022-03-31 15:36:41 +0200

    fix: remove flakyness from elastic tests (#243)

--- tests/unit/array/conftest.py
+++ tests/unit/array/conftest.py
@@ -2,6 +2,7 @@ import os
 import time
 
 import pytest
+from elasticsearch import Elasticsearch
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 compose_yml = os.path.abspath(os.path.join(cur_dir, 'docker-compose.yml'))
@@ -13,7 +14,10 @@ def start_storage():
         f"docker-compose -f {compose_yml} --project-directory . up  --build -d "
         f"--remove-orphans"
     )
-    time.sleep(20)
+    es = Elasticsearch(hosts='http://localhost:9200/')
+    while not es.ping():
+        time.sleep(0.5)
+
     yield
     os.system(
         f"docker-compose -f {compose_yml} --project-directory . down "
