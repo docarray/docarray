9f77119bae27119a91cffaa22ef24708906645f7
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-04-10 16:49:03 +0200

    chore: add error handling

--- scripts/benchmarking.py
+++ scripts/benchmarking.py
@@ -7,10 +7,8 @@ from docarray import Document, DocumentArray
 from rich.console import Console
 from rich.table import Table
 
-n_index = 1_000_000
-# n_index = [10_000, 100_000, 500_000, 1_000_000]
+n_index = 100_000
 n_query = 1
-# n_query = [1, 8, 64]
 D = 128
 TENSOR_SHAPE = (512, 256)
 
@@ -90,49 +88,53 @@ table.add_column("Delete (D)")
 table.add_column("Find by vector")
 table.add_column("Find by condition")
 
-for backend, config in storage_backends:
-    print('Backend:', backend.title())
-    # for n_i in n_index:
-    if not config:
-        da = DocumentArray(storage=backend)
-    else:
-        da = DocumentArray(storage=backend, config=config)
-
-    print(f'generating {n_index} docs...')
-    docs = get_docs(n_index, D, TENSOR_SHAPE, n_query)
-
-    print(f'indexing {n_index} docs ...')
-    create_time = create(da, docs)
-
-    # for n_q in n_query:
-    print(f'reading {n_query} docs ...')
-    read_time = read(
-        da,
-        random.sample([d.id for d in docs], n_query),
-    )
-
-    print(f'updating {n_query} docs ...')
-    update_time = update(da, random.sample(docs, n_query))
-
-    docs_to_delete = random.sample(docs, n_query)
-    print(f'deleting {n_query} docs ...')
-    delete_time = delete(da, [d.id for d in docs_to_delete])
-
-    print(f'finding {n_query} docs by vector ...')
-    find_by_vector_time = find_by_vector(da, np.random.rand(n_query, D))
-
-    print(f'finding {n_query} docs by condition ...')
-    find_by_condition_time = find_by_condition(da, {'tags__i': {'$eq': 0}})
-
-    table.add_row(
-        backend.title(),
-        fmt(create_time),
-        fmt(read_time),
-        fmt(update_time),
-        fmt(delete_time),
-        fmt(find_by_vector_time),
-        fmt(find_by_condition_time),
-    )
-
 console = Console()
+
+for backend, config in storage_backends:
+    try:
+        console.print('Backend:', backend.title())
+        # for n_i in n_index:
+        if not config:
+            da = DocumentArray(storage=backend)
+        else:
+            da = DocumentArray(storage=backend, config=config)
+
+        console.print(f'generating {n_index} docs...')
+        docs = get_docs(n_index, D, TENSOR_SHAPE, n_query)
+
+        console.print(f'indexing {n_index} docs ...')
+        create_time = create(da, docs)
+
+        # for n_q in n_query:
+        console.print(f'reading {n_query} docs ...')
+        read_time = read(
+            da,
+            random.sample([d.id for d in docs], n_query),
+        )
+
+        console.print(f'updating {n_query} docs ...')
+        update_time = update(da, random.sample(docs, n_query))
+
+        docs_to_delete = random.sample(docs, n_query)
+        console.print(f'deleting {n_query} docs ...')
+        delete_time = delete(da, [d.id for d in docs_to_delete])
+
+        console.print(f'finding {n_query} docs by vector ...')
+        find_by_vector_time = find_by_vector(da, np.random.rand(n_query, D))
+
+        console.print(f'finding {n_query} docs by condition ...')
+        find_by_condition_time = find_by_condition(da, {'tags__i': {'$eq': 0}})
+
+        table.add_row(
+            backend.title(),
+            fmt(create_time),
+            fmt(read_time),
+            fmt(update_time),
+            fmt(delete_time),
+            fmt(find_by_vector_time),
+            fmt(find_by_condition_time),
+        )
+    except Exception as e:
+        console.print(f'Storage Backend {backend} failed: {e}')
+
 console.print(table)
