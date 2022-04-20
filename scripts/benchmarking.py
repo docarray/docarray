371334b30337d1c580876274e3686058d7739394
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-04-20 15:33:24 +0200

    chore: add plot

--- scripts/benchmarking.py
+++ scripts/benchmarking.py
@@ -4,11 +4,15 @@ import random
 from time import perf_counter
 
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 from docarray import Document, DocumentArray
 from rich.console import Console
 from rich.table import Table
 
-n_index = 1000_000
+n_index_values = [10, 100, 1000]
 n_query = 1
 D = 128
 TENSOR_SHAPE = (512, 256)
@@ -125,7 +129,7 @@ else:
     ]
 
 table = Table(
-    title=f'DocArray Benchmarking n_index={n_index} n_query={n_query} D={D} K={K}'
+    title=f'DocArray Benchmarking n_index={n_index_values[-1]} n_query={n_query} D={D} K={K}'
 )
 table.add_column('Storage Backend')
 table.add_column('Indexing time (C)')
@@ -137,75 +141,98 @@ table.add_column(f'Recall at k={K} for vector search')
 table.add_column('Find by condition')
 
 console = Console()
+find_by_vector_values = {str(n_index): [] for n_index in n_index_values}
+for idx, n_index in enumerate(n_index_values):
+    console.print(f'generating {n_index} docs...')
+    docs = get_docs(n_index, D, TENSOR_SHAPE, n_query)
+    docs_to_delete = random.sample(docs, n_query)
+    docs_to_update = random.sample(docs, n_query)
 
-console.print(f'generating {n_index} docs...')
-docs = get_docs(n_index, D, TENSOR_SHAPE, n_query)
-docs_to_delete = random.sample(docs, n_query)
-docs_to_update = random.sample(docs, n_query)
+    vector_queries = [np.random.rand(n_query, D) for _ in range(n_vector_queries)]
+    ground_truth = []
 
-vector_queries = [np.random.rand(n_query, D) for _ in range(n_vector_queries)]
-ground_truth = []
+    for backend, config in storage_backends:
+        try:
+            console.print('Backend:', backend.title())
+            # for n_i in n_index:
+            if not config:
+                da = DocumentArray(storage=backend)
+            else:
+                da = DocumentArray(storage=backend, config=config)
 
-for backend, config in storage_backends:
-    try:
-        console.print('Backend:', backend.title())
-        # for n_i in n_index:
-        if not config:
-            da = DocumentArray(storage=backend)
-        else:
-            da = DocumentArray(storage=backend, config=config)
+            console.print(f'indexing {n_index} docs ...')
+            create_time, _ = create(da, docs)
 
-        console.print(f'indexing {n_index} docs ...')
-        create_time, _ = create(da, docs)
+            # for n_q in n_query:
+            console.print(f'reading {n_query} docs ...')
+            read_time, _ = read(
+                da,
+                random.sample([d.id for d in docs], n_query),
+            )
 
-        # for n_q in n_query:
-        console.print(f'reading {n_query} docs ...')
-        read_time, _ = read(
-            da,
-            random.sample([d.id for d in docs], n_query),
-        )
+            console.print(f'updating {n_query} docs ...')
+            update_time, _ = update(da, docs_to_update)
 
-        console.print(f'updating {n_query} docs ...')
-        update_time, _ = update(da, docs_to_update)
+            console.print(f'deleting {n_query} docs ...')
+            delete_time, _ = delete(da, [d.id for d in docs_to_delete])
 
-        console.print(f'deleting {n_query} docs ...')
-        delete_time, _ = delete(da, [d.id for d in docs_to_delete])
+            console.print(
+                f'finding {n_query} docs by vector averaged {n_vector_queries} times ...'
+            )
+            if backend == 'sqlite':
+                find_by_vector_time, result = find_by_vector(da, vector_queries[0])
+                recall_at_k = recall(result, ground_truth[0], K)
+            else:
+                recall_at_k_values = []
+                find_by_vector_times = []
+                for i, query in enumerate(vector_queries):
+                    find_by_vector_time, results = find_by_vector(da, query)
+                    find_by_vector_times.append(find_by_vector_time)
+                    if backend == 'memory':
+                        ground_truth.append(results)
+                        recall_at_k_values.append(1)
+                    else:
+                        recall_at_k_values.append(recall(results, ground_truth[i], K))
 
-        console.print(
-            f'finding {n_query} docs by vector averaged {n_vector_queries} times ...'
-        )
-        if backend == 'sqlite':
-            find_by_vector_time, result = find_by_vector(da, vector_queries[0])
-            recall_at_k = recall(result, ground_truth[0], K)
-        else:
-            recall_at_k_values = []
-            find_by_vector_times = []
-            for i, query in enumerate(vector_queries):
-                find_by_vector_time, results = find_by_vector(da, query)
-                find_by_vector_times.append(find_by_vector_time)
-                if backend == 'memory':
-                    ground_truth.append(results)
-                    recall_at_k_values.append(1)
-                else:
-                    recall_at_k_values.append(recall(results, ground_truth[i], K))
+                recall_at_k = sum(recall_at_k_values) / len(recall_at_k_values)
+                find_by_vector_time = sum(find_by_vector_times) / len(
+                    find_by_vector_times
+                )
 
-            recall_at_k = sum(recall_at_k_values) / len(recall_at_k_values)
-            find_by_vector_time = sum(find_by_vector_times) / len(find_by_vector_times)
+            console.print(f'finding {n_query} docs by condition ...')
+            find_by_condition_time, _ = find_by_condition(da, {'tags__i': {'$eq': 0}})
 
-        console.print(f'finding {n_query} docs by condition ...')
-        find_by_condition_time, _ = find_by_condition(da, {'tags__i': {'$eq': 0}})
+            if idx == len(n_index_values) - 1:
+                table.add_row(
+                    backend.title(),
+                    fmt(create_time, 's'),
+                    fmt(read_time * 1000, 'ms'),
+                    fmt(update_time * 1000, 'ms'),
+                    fmt(delete_time * 1000, 'ms'),
+                    fmt(find_by_vector_time, 's'),
+                    '{:.3f}'.format(recall_at_k),
+                    fmt(find_by_condition_time, 's'),
+                )
 
-        table.add_row(
-            backend.title(),
-            fmt(create_time, 's'),
-            fmt(read_time * 1000, 'ms'),
-            fmt(update_time * 1000, 'ms'),
-            fmt(delete_time * 1000, 'ms'),
-            fmt(find_by_vector_time, 's'),
-            '{:.3f}'.format(recall_at_k),
-            fmt(find_by_condition_time, 's'),
-        )
-    except Exception as e:
-        console.print(f'Storage Backend {backend} failed: {e}')
+            find_by_vector_values[str(n_index)].append(find_by_vector_time)
+        except Exception as e:
+            console.print(f'Storage Backend {backend} failed: {e}')
+
+df = pd.DataFrame(find_by_vector_values)
+df.index = [backend for backend, _ in storage_backends]
+fig, ax = plt.subplots()
+
+df.plot(
+    kind="bar",
+    ax=ax,
+    color=sns.color_palette('muted')[1:4],
+    title='Find by vector per backend and dataset size',
+    ylabel='milliseconds',
+    rot=0,
+)
+
+threshold = 0.3
+ax.hlines(y=threshold, xmin=-20, xmax=20, linewidth=2, color='r', linestyle='--')
+plt.savefig('benchmark.svg')
 
 console.print(table)
