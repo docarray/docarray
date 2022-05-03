b665b392c74511bf23ad60f3d638c855b0d3e846
Roshan Jossy <roshan.jossy@jina.ai>
2022-05-03 23:34:59 +0200

    docs(tracking): update refer links to other docs with utm_source

---
+++ docs/_static/source-in-links.js
@@ -0,0 +1,20 @@
+function addUTMSourceToLinks(href) {
+    try {
+        var url = new URL(href);
+        url.searchParams.set("utm_source", "docarray");
+        return url.href
+    }
+    catch{}
+}
+
+function addSourceToAllLinks() {
+    var anchors = document.getElementsByTagName("a");
+
+    for (var i = 0; i < anchors.length; i++) {
+        anchors[i].href = addUTMSourceToLinks(anchors[i].href)
+    }
+}
+
+window.onload = function () {
+    addSourceToAllLinks()
+}
