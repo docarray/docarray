9d9e6d121052237caa78687eca1f004a8b6a2da2
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-05-04 11:55:29 +0100

    fix: try fixing jina version

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
             'transformers>=4.16.2',
             'weaviate-client~=3.3.0',
             'annlite>=0.3.0',
-            'jina<=3.3.17',
+            'jina<=3.3.11',
         ],
     },
     classifiers=[
