f52e8e23a7db63f0e64cef10af4c56dcce11f5c3
AlaeddineAbdessalem <alaeddine-13@live.fr>
2022-05-11 16:14:02 +0100

    fix: apply hnsw parameters to the main class not the meta class (#337)

---
+++ tests/unit/array/test_backend_configuration.py
@@ -0,0 +1,25 @@
+import requests
+
+from docarray import DocumentArray
+
+
+def test_weaviate_hnsw(start_storage):
+    da = DocumentArray(
+        storage='weaviate',
+        config={'n_dim': 100, 'ef': 100, 'ef_construction': 100, 'max_connections': 16},
+    )
+
+    result = requests.get('http://localhost:8080/v1/schema').json()
+
+    classes = result.get('classes', [])
+    assert len(classes) == 2
+    main_class = list(
+        filter(lambda class_element: class_element['class'] == da._config.name, classes)
+    )
+    assert len(main_class) == 1
+
+    main_class = main_class[0]
+
+    assert main_class.get('vectorIndexConfig', {}).get('maxConnections') == 16
+    assert main_class.get('vectorIndexConfig', {}).get('efConstruction') == 100
+    assert main_class.get('vectorIndexConfig', {}).get('ef') == 100
