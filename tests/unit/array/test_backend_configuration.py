56a0d60d20f96ffdc3833884facbbb3ce5a8aeb4
Joni Latvala <joni.latvala@gmail.com>
2022-05-12 21:02:40 +0300

    feat: added support for all Weaviate HNSW configuration parameters

--- tests/unit/array/test_backend_configuration.py
+++ tests/unit/array/test_backend_configuration.py
@@ -6,19 +6,9 @@ from docarray import DocumentArray
 def test_weaviate_hnsw(start_storage):
     da = DocumentArray(
         storage='weaviate',
-        config={
-            'n_dim': 100,
-            'ef': 100,
-            'ef_construction': 100,
-            'max_connections': 16,
-            'dynamic_ef_min': 50,
-            'dynamic_ef_max': 300,
-            'dynamic_ef_factor': 4,
-            'vector_cache_max_objects': '1M',
-            'flat_search_cutoff': 20000,
-            'cleanup_interval_seconds': 1000,
-            'skip': True,
-        },
+        config={'n_dim': 100, 'ef': 100, 'ef_construction': 100, 'max_connections': 16,
+                'dynamic_ef_min': 50, 'dynamic_ef_max': 300, 'dynamic_ef_factor': 4, 'vector_cache_max_objects': '1M',
+                'flat_search_cutoff': 20000, 'cleanup_interval_seconds': 1000, 'skip': True},
     )
 
     result = requests.get('http://localhost:8080/v1/schema').json()
@@ -35,14 +25,10 @@ def test_weaviate_hnsw(start_storage):
     assert main_class.get('vectorIndexConfig', {}).get('maxConnections') == 16
     assert main_class.get('vectorIndexConfig', {}).get('efConstruction') == 100
     assert main_class.get('vectorIndexConfig', {}).get('ef') == 100
-    assert main_class.get('vectorIndexConfig', {}).get('dynamic_ef_min') == 50
-    assert main_class.get('vectorIndexConfig', {}).get('dynamic_ef_max') == 300
-    assert main_class.get('vectorIndexConfig', {}).get('dynamic_ef_factor') == 4
-    assert (
-        main_class.get('vectorIndexConfig', {}).get('vector_cache_max_objects') == '1M'
-    )
-    assert main_class.get('vectorIndexConfig', {}).get('flat_search_cutoff') == 20000
-    assert (
-        main_class.get('vectorIndexConfig', {}).get('cleanup_interval_seconds') == 1000
-    )
+    assert main_class.get('vectorIndexConfig', {}).get('dynamicEfMin') == 50
+    assert main_class.get('vectorIndexConfig', {}).get('dynamicEfMax') == 300
+    assert main_class.get('vectorIndexConfig', {}).get('dynamicEfFactor') == 4
+    assert main_class.get('vectorIndexConfig', {}).get('vectorCacheMaxObjects') == 1000000
+    assert main_class.get('vectorIndexConfig', {}).get('flatSearchCutoff') == 20000
+    assert main_class.get('vectorIndexConfig', {}).get('cleanupIntervalSeconds') == 1000
     assert main_class.get('vectorIndexConfig', {}).get('skip') is True
