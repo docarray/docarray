d85a597f8b7a7c7ccd7c35205917dd91ad8ceecf
AlaeddineAbdessalem <alaeddine-13@live.fr>
2022-05-26 04:24:00 +0200

    fix: fix protobuf dependency (#370)
    * fix: fix protobuf dependency
    
    * fix: fix protobuf dependency

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     extras_require={
         # req usage, please see https://docarray.jina.ai/#install
         'common': [
-            'protobuf>=3.13.0',
+            'protobuf>=3.13.0,<=3.20.1',
             'lz4',
             'requests',
             'matplotlib',
@@ -52,7 +52,7 @@ setup(
             'uvicorn',
         ],
         'full': [
-            'protobuf>=3.13.0',
+            'protobuf>=3.13.0,<=3.20.1',
             'lz4',
             'requests',
             'matplotlib',
