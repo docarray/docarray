4ec8b6123a5812f3f5f0342d69ac2cc6f506afd0
David Buchaca Prats <davidbuchaca@gmail.com>
2022-06-01 18:10:09 +0200

    refactor: remove unneeded list in weaviate find

--- docarray/array/storage/weaviate/find.py
+++ docarray/array/storage/weaviate/find.py
@@ -104,9 +104,7 @@ class FindMixin:
                 f'filter failed, please check your filter query. Errors: \n{errors}'
             )
 
-        found_results = (
-            results.get('data', {}).get('Get', {}).get(self._class_name, []) or []
-        )
+        found_results = results.get('data', {}).get('Get', {}).get(self._class_name, [])
 
         # The serialized document is stored in results['data']['Get'][self._class_name]
         for result in found_results:
