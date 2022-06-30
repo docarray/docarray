13113cd113caf59ac3d039d8a2786b2ddd5c5b09
Joan Fontanals Martinez <joan.martinez@jina.ai>
2022-06-30 11:40:59 +0200

    refactor: small changes

--- docarray/array/storage/qdrant/seqlike.py
+++ docarray/array/storage/qdrant/seqlike.py
@@ -66,6 +66,5 @@ class SequenceLikeMixin(BaseSequenceLikeMixin):
         return f'<DocumentArray[Qdrant] (length={len(self)}) at {id(self)}>'
 
     def extend(self, docs: Iterable['Document']):
-        docs = list(docs)
         self._upload_batch(docs)
         self._offset2ids.extend([doc.id for doc in docs])
