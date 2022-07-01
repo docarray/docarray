bd56f52c491a9c23f3e2ab152108ef9e7da0431f
Alvin Prayuda <alvinprayuda@gmail.com>
2022-07-01 17:04:39 +0700

    fix(elastic): inconsistent offset on bulk index error and improve bulk performance (#422)

--- tests/unit/array/storage/elastic/test_add.py
+++ tests/unit/array/storage/elastic/test_add.py
@@ -1,5 +1,7 @@
 from docarray import Document, DocumentArray
 
+import pytest
+
 
 def test_add_ignore_existing_doc_id(start_storage):
     elastic_doc = DocumentArray(
@@ -41,3 +43,44 @@ def test_add_ignore_existing_doc_id(start_storage):
     assert len(elastic_doc) == len(elastic_doc[:, 'embedding'])
     assert len(elastic_doc) == indexed_offset_count
     assert len(elastic_doc[:, 'embedding']) == 7
+
+
+def test_add_skip_wrong_data_type_and_fix_offset(start_storage):
+    elastic_doc = DocumentArray(
+        storage='elasticsearch',
+        config={
+            'n_dim': 3,
+            'columns': [('price', 'int')],
+            'index_name': 'test_add_skip_wrong_data_type_and_fix_offset',
+        },
+    )
+
+    with elastic_doc:
+        elastic_doc.extend(
+            [
+                Document(id='0', price=1000),
+                Document(id='1', price=20000),
+                Document(id='2', price=103000),
+            ]
+        )
+
+    with pytest.raises(IndexError):
+        with elastic_doc:
+            elastic_doc.extend(
+                [
+                    Document(id='0', price=10000),
+                    Document(id='1', price=20000),
+                    Document(id='3', price=30000),
+                    Document(id='4', price=100000000000),  # overflow int32
+                    Document(id='5', price=2000),
+                    Document(id='6', price=100000000000),  # overflow int32
+                    Document(id='7', price=30000),
+                ]
+            )
+
+    expected_ids = ['0', '1', '2', '3', '5', '7']
+
+    assert len(elastic_doc) == 6
+    assert len(elastic_doc[:, 'id']) == 6
+    assert elastic_doc[:, 'id'] == expected_ids
+    assert elastic_doc._offset2ids.ids == expected_ids
