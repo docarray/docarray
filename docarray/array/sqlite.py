07baef1b49b595fd500b0b193e534b7cd0e25ce8
Johannes Messner <messnerjo@gmail.com>
2022-07-05 10:55:21 +0200

    docs: add explanation to sqlite backend

--- docarray/array/sqlite.py
+++ docarray/array/sqlite.py
@@ -8,6 +8,7 @@ __all__ = ['SqliteConfig', 'DocumentArraySqlite']
 class DocumentArraySqlite(StorageMixins, DocumentArray):
     """
     DocumentArray that stores Documents in a `SQLite database <https://www.sqlite.org/index.html>`_.
+    This stores Documents on disk instead of keeping them in memory, and offers the simplest way of persisting data with DocArray.
 
     With this implementation, :meth:`match` and :meth:`find` perform exact (exhaustive) vector search.
 
