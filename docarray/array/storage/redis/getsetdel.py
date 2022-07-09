8da776e2d46e018311f633a26d8fb80cb4f6ac9a
AnneY <evangeline-lun@foxmail.com>
2022-07-09 11:42:12 +0800

    fix: redis storage getsetdel

--- docarray/array/storage/redis/getsetdel.py
+++ docarray/array/storage/redis/getsetdel.py
@@ -9,30 +9,19 @@ class GetSetDelMixin(BaseGetSetDelMixin):
     """Provide concrete implementation for ``__getitem__``, ``__setitem__``,
     and ``__delitem__`` for ``DocumentArrayRedis``"""
 
-    def _getitem(self, doc_id: str) -> 'Document':
-        """Helper method for getting item with weaviate as storage
-
-        :param doc_id:  id of the document
-        :raises KeyError: raise error when redis id does not exist in storage
-        :return: Document
-        """
-        try:
-            result = self._client.get(self._config.key_prefix + doc_id)
-            # doc = Document.from_base64(result['_source']['blob'])
-            # return doc
-
-        except Exception as ex:
-            raise KeyError(doc_id) from ex
-
     def _get_doc_by_id(self, _id: str) -> 'Document':
         """Concrete implementation of base class' ``_get_doc_by_id``
 
         :param _id: the id of the document
         :return: the retrieved document from redis
         """
-        return self._getitem(_id)
+        try:
+            doc = self._client.hgetall(self._config.key_prefix + str(_id))
+            return doc
+        except Exception as ex:
+            raise KeyError(_id) from ex
 
-    def _set_doc_by_id(self, _id: str, value: 'Document', flush: bool = True):
+    def _set_doc_by_id(self, _id: str, value: 'Document'):
         """Concrete implementation of base class' ``_set_doc_by_id``
 
         :param _id: the id of doc to update
@@ -42,14 +31,14 @@ class GetSetDelMixin(BaseGetSetDelMixin):
             self._del_doc_by_id(_id)
 
         payload = self._document_to_redis(value)
-        self._add(_id, payload)
+        self._client.hset(self._config.key_prefix + _id, mapping=payload)
 
     def _del_doc_by_id(self, _id: str):
         """Concrete implementation of base class' ``_del_doc_by_id``
 
         :param _id: the id of the document to delete
         """
-        if self._doc_id_exists(self._config.key_prefix + _id):
+        if self._doc_id_exists(_id):
             self._client.delete(self._config.key_prefix + _id)
 
     def _document_to_redis(self, doc: 'Document') -> Dict:
@@ -74,3 +63,7 @@ class GetSetDelMixin(BaseGetSetDelMixin):
 
     def _save_offset2ids(self):
         self._update_offset2ids_meta()
+
+    # TODO finish this method
+    # def _clear_storage(self):
+    #     """Concrete implementation of base class' ``_clear_storage``"""
