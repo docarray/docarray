bd87400ce8162df6bf9f6d29009239f1834fdf67
AnneY <evangeline-lun@foxmail.com>
2022-07-09 11:46:43 +0800

    feat: add redis storage seqlike

---
+++ docarray/array/storage/redis/seqlike.py
@@ -0,0 +1,64 @@
+from typing import Iterable, Iterator, Union
+
+from ..base.seqlike import BaseSequenceLikeMixin
+from .... import Document
+
+
+class SequenceLikeMixin(BaseSequenceLikeMixin):
+    """Implement sequence-like methods for DocumentArray with Redis as storage"""
+
+    def __eq__(self, other):
+        """Compare this object to the other, returns True if and only if other
+        as the same type as self and other has the same meta information
+
+        :param other: the other object to check for equality
+        :return: ``True`` if other is equal to self
+        """
+        # two DAW are considered as the same if they have the same client meta data
+        return (
+            type(self) is type(other)
+            and self._client.client_info() == other._client.client_info()
+            and self._config == other._config
+        )
+
+    def __len__(self):
+        """Return the length of :class:`DocumentArray` that uses Redisd as storage
+
+        :return: the length of this :class:`DocumentArrayRedis` object
+        """
+        try:
+            return self._client.llen(self._offset2id_key)
+        except:
+            return 0
+
+    def __contains__(self, x: Union[str, 'Document']):
+        """Check if ``x`` is contained in this :class:`DocumentArray` with Redis storage
+
+        :param x: the id of the document to check or the document object itself
+        :return: True if ``x`` is contained in self
+        """
+        if isinstance(x, str):
+            return self._doc_id_exists(x)
+        elif isinstance(x, Document):
+            return self._doc_id_exists(x.id)
+        else:
+            return False
+
+    def __del__(self):
+        """Delete this :class:`DocumentArrayElastic` object"""
+        self._save_offset2ids()
+
+        # TODO whether to delete everything
+        # if not self._persist:
+        #    self._offset2ids.clear()
+
+    def __repr__(self):
+        """Return the string representation of :class:`DocumentArrayRedis` object
+        :return: string representation of this object
+        """
+        return f'<DocumentArray[Redis] (length={len(self)}) at {id(self)}>'
+
+    def extend(self, docs: Iterable['Document']):
+        for doc in docs:
+            self._set_doc_by_id(doc.id, doc)
+        self._offset2ids.extend([doc.id for doc in docs])
