5f6c68333bbd8f701e6b6bfc8a66d8c8ba397883
AnneY <evangeline-lun@foxmail.com>
2022-07-09 11:47:36 +0800

    feat: add redis storage find

---
+++ docarray/array/storage/redis/find.py
@@ -0,0 +1,73 @@
+from typing import (
+    TYPE_CHECKING,
+    TypeVar,
+    Sequence,
+    List,
+    Union,
+    Optional,
+    Dict,
+)
+
+from redis.commands.search.query import NumericFilter, Query
+from redis.commands.search.field import VectorField
+
+import numpy as np
+
+from .... import Document, DocumentArray
+from ....math import ndarray
+from ....math.ndarray import to_numpy_array
+from ....score import NamedScore
+from ....array.mixins.find import FindMixin as BaseFindMixin
+
+
+if TYPE_CHECKING:
+    import tensorflow
+    import torch
+
+    RedisArrayType = TypeVar(
+        'RedisArrayType',
+        np.ndarray,
+        tensorflow.Tensor,
+        torch.Tensor,
+        Sequence[float],
+        Dict,
+    )
+
+
+class FindMixin(BaseFindMixin):
+    def _find_similar_vectors(
+        self, query: 'RedisArrayType', filter: Optional[Dict] = None, limit=10
+    ):
+        q = (
+            Query("*=>[KNN " + limit + " @embedding $vec]")
+            .return_field("__embedding_score")
+            .sort_by("__embedding_score")
+        )
+        query_params = {"vec": to_numpy_array(query)}
+        query_params.update(filter)
+        results = self.searchclient.search(q, query_params).docs
+
+        da = DocumentArray()
+        for res in results:
+            doc = Document.from_base64(res['blob'])
+            doc.embedding = res['embedding']
+            doc.scores['score'] = NamedScore(value=res['__v_score'])
+            da.append(doc)
+        return da
+
+    def _find(
+        self,
+        query: 'RedisArrayType',
+        limit: int = 10,
+        filter: Optional[Dict] = None,
+        **kwargs
+    ) -> List['DocumentArray']:
+
+        query = np.array(query)
+        num_rows, n_dim = ndarray.get_array_rows(query)
+        if n_dim != 2:
+            query = query.reshape((num_rows, -1))
+
+        return [
+            self._find_similar_vectors(q, filter=filter, limit=limit) for q in query
+        ]
