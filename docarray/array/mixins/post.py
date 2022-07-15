ac5c670b9ad0b9fce2b51b326734e64784a60775
bwanglzu <bo.wang@jina.ai>
2022-07-15 09:56:52 +0200

    fix: add unit tests

--- docarray/array/mixins/post.py
+++ docarray/array/mixins/post.py
@@ -12,39 +12,38 @@ _VERSION_PATTERN = '(v\d.\d.\d|v\d.\d|v\d|latest)((-(c|g)pu)?)'
 _ParsedHost = namedtuple('ParsedHost', 'on host port version scheme')
 
 
+def _parse_host(host: str) -> NamedTuple:
+    """Parse a host string into namedtuple object.
+
+    A parsed host's components are `on`, `host`, `port`, `version`, `scheme`.
+    :param host: a host string. Can be one of the following:
+        - `grpc://192.168.0.123:8080/endpoint`
+        - `ws://192.168.0.123:8080/endpoint`
+        - `http://192.168.0.123:8080/endpoint`
+        - `jinahub://Hello/endpoint`
+        - `jinahub+docker://Hello/endpoint`
+        - `jinahub+docker://Hello/v0.0.1/endpoint`
+        - `jinahub+docker://Hello/latest/endpoint`
+        - `jinahub+sandbox://Hello/endpoint`
+    """
+    r = urlparse(host)
+    on = r.path or '/'
+    host = (
+        r._replace(netloc=r.netloc.replace(f':{r.port}', ''))._replace(path='').geturl()
+    )
+    port = r.port or None
+    version = None
+    scheme = r.scheme
+    version_match = re.search(_VERSION_PATTERN, r.path)
+    if version_match:
+        version = version_match.group(0)
+        on = r.path[version_match.end(0) :]
+        host = host + '/' + version
+    return _ParsedHost(on=on, host=host, port=port, version=version, scheme=scheme)
+
+
 class PostMixin:
     """Helper functions for posting DocumentArray to Jina Flow."""
-
-    def _parse_host(self, host: str) -> NamedTuple:
-        """Parse a host string into namedtuple object.
-
-        A parsed host's components are `on`, `host`, `port`, `version`, `scheme`.
-        :param host: a host string. Can be one of the following:
-            - `grpc://192.168.0.123:8080/endpoint`
-            - `ws://192.168.0.123:8080/endpoint`
-            - `http://192.168.0.123:8080/endpoint`
-            - `jinahub://Hello/endpoint`
-            - `jinahub+docker://Hello/endpoint`
-            - `jinahub+docker://Hello/v0.0.1/endpoint`
-            - `jinahub+docker://Hello/latest/endpoint`
-            - `jinahub+sandbox://Hello/endpoint`
-        """
-        r = urlparse(host)
-        on = r.path or '/'
-        host = (
-            r._replace(netloc=r.netloc.replace(f':{r.port}', ''))
-            ._replace(path='')
-            .geturl()
-        )
-        port = r.port or None
-        version = None
-        scheme = r.scheme
-        version_match = re.search(_VERSION_PATTERN, r.path)
-        if version_match:
-            version = version_match.group(0)
-            on = r.path[version_match.end(0) :]
-            host = host + '/' + version
-        return _ParsedHost(on=on, host=host, port=port, version=version, scheme=scheme)
 
     def post(
         self,
@@ -75,17 +74,15 @@ class PostMixin:
         if not self:
             return
 
-        parsed_host = self._parse_host(host)
+        parsed_host = _parse_host(host)
 
         batch_size = batch_size or len(self)
 
         scheme = parsed_host.scheme
         host = parsed_host.host
-        tls = False
 
         if scheme in ('grpcs', 'https', 'wss'):
             scheme = scheme[:-1]
-            tls = True
 
         if scheme == 'ws':
             scheme = 'websocket'  # temp fix for the core
