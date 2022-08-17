aa0399b3ba55c05da1b002e2d469523a0c2ae69c
Johannes Messner <messnerjo@gmail.com>
2022-08-17 13:15:09 +0200

    refactor: clean up inports

--- docarray/array/storage/base/backend.py
+++ docarray/array/storage/base/backend.py
@@ -1,4 +1,3 @@
-import dataclasses
 from abc import ABC, abstractmethod
 from collections import namedtuple
 from dataclasses import is_dataclass, asdict
@@ -32,7 +31,7 @@ class BaseBackendMixin(ABC):
                 config_subindex = (
                     dict() if config_subindex is None else config_subindex
                 )  # allow None as input
-                if dataclasses.is_dataclass(config_subindex):
+                if is_dataclass(config_subindex):
                     config_subindex = asdict(config_subindex)
                 config_joined = {**config, **config_subindex}
                 config_joined = self._ensure_unique_config(
--- tests/unit/array/test_advance_indexing.py
+++ tests/unit/array/test_advance_indexing.py
@@ -1,5 +1,3 @@
-import copy
-
 import numpy as np
 import pytest
 
