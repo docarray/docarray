24be6ba8481690d449fa16f1ca8fd2f7875ba5b8
Joan Fontanals <joan.martinez@jina.ai>
2022-08-27 00:01:50 +0800

    feat: bump protobuf (#371)
    * feat: bump protobuf
    
    * fix: fix flake8
    
    * fix(protobuf): set python parsing for tf and paddle
    
    * test: do not use special flag
    
    * feat: flexible build proto script
    
    * feat: flexible import of proto modules
    
    * chore: update proto compiling instructions
    
    * chore: update usages of proto
    
    * chore: remove print
    
    * chore: free protobuf version
    
    * chore: update config files
    
    * ci: install test tag before full
    
    * fix: change proto usage
    
    * chore: expose all pb2 module for dependents
    
    * chore: add comment to explain
    
    * chore: increase version
    
    * ci: install modified jina in CI
    
    * fix: preserver file structure for compatibility
    
    * Revert "chore: update usages of proto"
    
    This reverts commit 420acb5642343cd55209350b9ddc8f5a8bbdfa3d.
    
    * Revert "fix: change proto usage"
    
    This reverts commit 1a9569f99297607b70df404e134864882d0298fe.
    
    Co-authored-by: Han Xiao <han.xiao@jina.ai>
    Co-authored-by: Alaeddine Abdessalem <alaeddine-13@live.fr>

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     extras_require={
         # req usage, please see https://docarray.jina.ai/#install
         'common': [
-            'protobuf>=3.13.0,<=3.20.1',
+            'protobuf>=3.13.0',
             'lz4',
             'requests',
             'matplotlib',
@@ -53,7 +53,7 @@ setup(
             'jina-hubble-sdk>=0.11.0',
         ],
         'full': [
-            'protobuf>=3.13.0,<=3.20.1',
+            'protobuf>=3.13.0',
             'lz4',
             'requests',
             'matplotlib',
