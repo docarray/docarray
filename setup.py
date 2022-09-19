5693b3a5fde47c5be1e51792489c4c2d50c50cde
AlaeddineAbdessalem <alaeddine-13@live.fr>
2022-09-19 08:33:03 +0100

    fix: adapt to annlite changes (#543)

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
             'qdrant-client~=0.7.3',
         ],
         'annlite': [
-            'annlite>=0.3.10',
+            'annlite>=0.3.12',
         ],
         'weaviate': [
             'weaviate-client~=3.3.0',
@@ -103,7 +103,7 @@ setup(
             'jupyterlab',
             'transformers>=4.16.2',
             'weaviate-client~=3.3.0',
-            'annlite>=0.3.10',
+            'annlite>=0.3.12',
             'elasticsearch>=8.2.0',
             'redis>=4.3.0',
             'jina',
