be8564a02c5d529468631c5c058f7d4b38109787
Jonathan Rowley <jonathan@dco.ai>
2022-09-23 11:15:42 -0500

    test(weaviate): add test cases for weaviate

--- tests/unit/array/storage/weaviate/test_additional_properties.py
+++ tests/unit/array/storage/weaviate/test_additional_properties.py
@@ -1,8 +1,29 @@
-import unittest
+from docarray import Document, DocumentArray
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-if __name__ == '__main__':
-    unittest.main()
+def test_get_additional(start_storage):
+    da = DocumentArray(
+        storage="weaviate", config={"name": "Test", "host": "localhost", "port": 8080}
+    )
+
+    with da:
+        da.extend(
+            [
+                Document(text="r0", embedding=[0, 0, 0]),
+                Document(text="r2", embedding=[2, 2, 2]),
+                Document(text="r4", embedding=[4, 4, 4]),
+                Document(text="r5", embedding=[2, 2, 2]),
+                Document(text="r6", embedding=[4, 4, 4]),
+            ]
+        )
+
+    additional = ["creationTimeUnix", "lastUpdateTimeUnix"]
+    results = da.find(
+        DocumentArray([Document(text="r5", embedding=[2, 2, 2])]),
+        limit=1,
+        additional=additional,
+    )
+
+    for res in results:
+        assert res[:, "tags__creationTimeUnix"][0] is not None
+        assert res[:, "tags__lastUpdateTimeUnix"][0] is not None
--- tests/unit/array/storage/weaviate/test_query_params.py
+++ tests/unit/array/storage/weaviate/test_query_params.py
@@ -2,8 +2,16 @@ from docarray import Document, DocumentArray
 import numpy as np
 
 
+def find_random(da, target_certainty):
+    return da.find(
+        DocumentArray([Document(embedding=np.random.randint(10, size=10))]),
+        query_params={"certainty": target_certainty},
+    )[0]
+
+
 def test_certainty_filter(start_storage):
     nrof_docs = 100
+    target_certainty = 0.98
     da = DocumentArray(
         storage="weaviate", config={"name": "Test", "host": "localhost", "port": 8080}
     )
@@ -11,22 +19,15 @@ def test_certainty_filter(start_storage):
     with da:
         da.extend(
             [
-                Document(id=f'r{i}', embedding=np.ones((3,)) * i)
-                for i in range(nrof_docs)
+                Document(id=f"r{i}", embedding=np.random.randint(10, size=10))
+                for i in range(1, nrof_docs)
             ],
         )
 
-    results = da.find(
-        DocumentArray([Document(embedding=[1, 2, 3])]),
-        query_params={"certainty": 0.9}
-    )
+    while True:
+        results = find_random(da, target_certainty)
+        if len(results) > 0:
+            break
 
-    #,
-    #query_params = {"certainty": 0.1}
-    print(len(results[0]))
-    for res in results[0]:
-        print(res[:, 'embedding'])
-        print(res[:, 'scores'])
-        print(res[:, 'scores'][0]['weaviate_certainty'])
-        assert True
-        #assert res[:, 'tags__lastUpdateTimeUnix'][0] is not None
+    for res in results:
+        assert res.scores["weaviate_certainty"].value >= target_certainty
