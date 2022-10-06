5c1c727a8e2df20b6bad96a3c96db1691ec64999
Han Xiao <han.xiao@jina.ai>
2022-10-06 11:03:13 +0200

    fix: better error message on bad selector
    Signed-off-by: Han Xiao <han.xiao@jina.ai>

--- docarray/document/mixins/multimodal.py
+++ docarray/document/mixins/multimodal.py
@@ -104,7 +104,9 @@ class MultiModalMixin:
             )
 
         if attr not in self._metadata['multi_modal_schema']:
-            raise ValueError(f'the Document schema does not contain attribute `{attr}`')
+            raise ValueError(
+                f'the Document schema does not contain attribute `{attr}`, typo?'
+            )
 
         pos = self._metadata['multi_modal_schema'][attr].get('position')
         if pos is None:
