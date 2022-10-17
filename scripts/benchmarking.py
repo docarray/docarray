08bbee328225fbc98c3a2a35f69082882356f0b7
AnneY <evangeline-lun@foxmail.com>
2022-10-17 16:39:44 +0800

    refactor: reuse code

--- scripts/benchmarking.py
+++ scripts/benchmarking.py
@@ -1,401 +1,52 @@
 import argparse
-import functools
-import random
-from time import perf_counter
 
 import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-from docarray import Document, DocumentArray
-from rich.console import Console
-from rich.table import Table
-
-np.random.seed(123)
-
-n_index_values = [1_000_000]
-n_query = 1
-D = 128
-TENSOR_SHAPE = (512, 256)
-K = 10
-n_vector_queries = 1000
-np.random.seed(123)
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '--default-hnsw',
-    help='Whether to use default HNSW configurations',
-    action='store_true',
+from benchmarking_utils import (
+    get_configuration_storage_backends,
+    plot_results,
+    run_benchmark2,
+    save_benchmark_df,
 )
 
-parser.add_argument(
-    '--exclude-backends',
-    help='list of comma separated backends to exclude from the benchmarks',
-    type=str,
-)
-args = parser.parse_args()
+if __name__ == "__main__":
 
-times = {}
+    # Parameters settable by the user
+    n_index_values = [1000]
+    n_query = 1
+    D = 128
+    TENSOR_SHAPE = (512, 256)
+    K = 10
+    n_vector_queries = 100
+    np.random.seed(123)
 
+    # Benchmark
+    storage_backends = get_configuration_storage_backends(argparse, D)
+    find_by_vector_values = {str(n_index): [] for n_index in n_index_values}
+    create_values = {str(n_index): [] for n_index in n_index_values}
 
-def timer(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        start = perf_counter()
-        res = func(*args, **kwargs)
-        return (perf_counter() - start, res)
+    for idx, n_index in enumerate(n_index_values):
+        train = [np.random.rand(D) for _ in range(n_index)]
+        test = [np.random.rand(D) for _ in range(n_vector_queries)]
+        ground_truth = []
 
-    return wrapper
-
-
-@timer
-def create(da, docs):
-    da.extend(docs)
-
-
-@timer
-def read(da, ids):
-    da[ids]
-
-
-@timer
-def update(da, docs):
-    da[[d.id for d in docs]] = docs
-
-
-@timer
-def delete(da, ids):
-    del da[ids]
-
-
-@timer
-def find_by_condition(da, query):
-    da.find(query)
-
-
-@timer
-def find_by_vector(da, query):
-    return da.find(query, limit=K)
-
-
-def get_docs(n, n_dim, tensor_shape, n_query):
-    return [
-        Document(
-            embedding=np.random.rand(n_dim),
-            # tensor=np.random.rand(*tensor_shape),
-            tags={'i': int(i / n_query)},
+        find_by_vector_time_all, create_time_all, benchmark_df = run_benchmark2(
+            train,
+            test,
+            ground_truth,
+            n_index,
+            n_vector_queries,
+            n_query,
+            storage_backends,
+            K,
+            D,
         )
-        for i in range(n)
-    ]
 
+        # store find_by_vector time
+        find_by_vector_values[str(n_index)] = find_by_vector_time_all
+        create_values[str(n_index)] = create_time_all
+        save_benchmark_df(benchmark_df, n_index)
 
-def fmt(value, unit):
-    return '{:.3f} {}'.format(value, unit)
-
-
-def recall(predicted, relevant, eval_at):
-    if eval_at == 0:
-        return 0.0
-    predicted_at_k = predicted[:eval_at]
-    n_predicted_and_relevant = len(
-        set(predicted_at_k[:, 'id']).intersection(set(relevant[:, 'id']))
+    plot_results(
+        find_by_vector_values, storage_backends, create_values, plot_legend=False
     )
-    return n_predicted_and_relevant / len(relevant)
-
-
-if args.default_hnsw:
-    storage_backends = [
-        ('memory', None),
-        ('sqlite', None),
-        (
-            'annlite',
-            {
-                'n_dim': D,
-                'columns': {'i': 'int'},
-            },
-        ),
-        (
-            'qdrant',
-            {
-                'n_dim': D,
-                'scroll_batch_size': 8,
-                'port': '41233',
-            },
-        ),
-        (
-            'weaviate',
-            {
-                'n_dim': D,
-                'port': '41234',
-                'columns': {'i': 'int'},
-            },
-        ),
-        (
-            'elasticsearch',
-            {
-                'n_dim': D,
-                'hosts': 'http://localhost:41235',
-                'columns': {'i': 'int'},
-            },
-        ),
-        (
-            'redis',
-            {
-                'n_dim': D,
-                'port': '41236',
-                'columns': {'i': 'int'},
-            },
-        ),
-    ]
-else:
-    storage_backends = [
-        ('memory', None),
-        ('sqlite', None),
-        (
-            'annlite',
-            {
-                'n_dim': D,
-                'ef_construction': 100,
-                'ef_search': 100,
-                'max_connection': 16,
-                'columns': {'i': 'int'},
-            },
-        ),
-        (
-            'qdrant',
-            {
-                'n_dim': D,
-                'scroll_batch_size': 8,
-                'ef_construct': 100,
-                'm': 16,
-                'port': '41233',
-            },
-        ),
-        (
-            'weaviate',
-            {
-                'n_dim': D,
-                'ef': 100,
-                'ef_construction': 100,
-                'max_connections': 16,
-                'port': '41234',
-                'columns': {'i': 'int'},
-            },
-        ),
-        (
-            'elasticsearch',
-            {
-                'n_dim': D,
-                'ef_construction': 100,
-                'm': 16,
-                'hosts': 'http://localhost:41235',
-                'columns': {'i': 'int'},
-            },
-        ),
-        (
-            'redis',
-            {
-                'n_dim': D,
-                'ef_construction': 100,
-                'm': 16,
-                'ef_runtime': 100,
-                'port': '41236',
-            },
-        ),
-    ]
-
-storage_backends = [
-    (backend, config)
-    for backend, config in storage_backends
-    if backend not in (args.exclude_backends or '').split(',')
-]
-
-storage_backend_filters = {
-    'memory': {'tags__i': {'$eq': 0}},
-    'sqlite': {'tags__i': {'$eq': 0}},
-    'annlite': {'i': {'$eq': 0}},
-    'qdrant': {'tags__i': {'$eq': 0}},
-    'weaviate': {'path': 'i', 'operator': 'Equal', 'valueInt': 0},
-    'elasticsearch': {'match': {'i': 0}},
-    'redis': {'i': {'$eq': 0}},
-}
-
-table = Table(
-    title=f'DocArray Benchmarking n_index={n_index_values[-1]} n_query={n_query} D={D} K={K}'
-)
-benchmark_df = pd.DataFrame(
-    {
-        'Storage Backend': [],
-        'Indexing time (C)': [],
-        'Query (R)': [],
-        'Update (U)': [],
-        'Delete (D)': [],
-        'Find by vector': [],
-        f'Recall at k={K} for vector search': [],
-        'Find by condition': [],
-    }
-)
-
-for col in benchmark_df.columns:
-    table.add_column(col)
-
-console = Console()
-find_by_vector_values = {str(n_index): [] for n_index in n_index_values}
-create_values = {str(n_index): [] for n_index in n_index_values}
-
-
-for idx, n_index in enumerate(n_index_values):
-    console.print(f'generating {n_index} docs...')
-    docs = get_docs(n_index, D, TENSOR_SHAPE, n_query)
-    docs_to_delete = random.sample(docs, n_query)
-    docs_to_update = random.sample(docs, n_query)
-
-    vector_queries = [np.random.rand(n_query, D) for _ in range(n_vector_queries)]
-    ground_truth = []
-
-    for backend, config in storage_backends:
-        try:
-            console.print('Backend:', backend.title())
-            # for n_i in n_index:
-            if not config:
-                da = DocumentArray(storage=backend)
-            else:
-                da = DocumentArray(storage=backend, config=config)
-
-            console.print(f'indexing {n_index} docs ...')
-            create_time, _ = create(da, docs)
-
-            # for n_q in n_query:
-            console.print(f'reading {n_query} docs ...')
-            read_time, _ = read(
-                da,
-                random.sample([d.id for d in docs], n_query),
-            )
-
-            console.print(f'updating {n_query} docs ...')
-            update_time, _ = update(da, docs_to_update)
-
-            console.print(f'deleting {n_query} docs ...')
-            delete_time, _ = delete(da, [d.id for d in docs_to_delete])
-
-            console.print(
-                f'finding {n_query} docs by vector averaged {n_vector_queries} times ...'
-            )
-            if backend == 'sqlite':
-                find_by_vector_time, result = find_by_vector(
-                    da, vector_queries[0].squeeze()
-                )
-                recall_at_k = recall(result, ground_truth[0], K)
-            else:
-                recall_at_k_values = []
-                find_by_vector_times = []
-                for i, query in enumerate(vector_queries):
-                    find_by_vector_time, results = find_by_vector(da, query.squeeze())
-                    find_by_vector_times.append(find_by_vector_time)
-                    if backend == 'memory':
-                        ground_truth.append(results)
-                        recall_at_k_values.append(1)
-                    else:
-                        recall_at_k_values.append(recall(results, ground_truth[i], K))
-
-                recall_at_k = sum(recall_at_k_values) / len(recall_at_k_values)
-                find_by_vector_time = sum(find_by_vector_times) / len(
-                    find_by_vector_times
-                )
-
-            console.print(f'finding {n_query} docs by condition ...')
-            find_by_condition_time, _ = find_by_condition(
-                da, storage_backend_filters[backend]
-            )
-
-            if idx == len(n_index_values) - 1:
-                table.add_row(
-                    backend.title(),
-                    fmt(create_time, 's'),
-                    fmt(read_time * 1000, 'ms'),
-                    fmt(update_time * 1000, 'ms'),
-                    fmt(delete_time * 1000, 'ms'),
-                    fmt(find_by_vector_time, 's'),
-                    '{:.3f}'.format(recall_at_k),
-                    fmt(find_by_condition_time, 's'),
-                )
-                benchmark_df.loc[len(benchmark_df.index)] = [
-                    backend.title(),
-                    create_time,
-                    read_time,
-                    update_time,
-                    delete_time,
-                    find_by_vector_time,
-                    recall_at_k,
-                    find_by_condition_time,
-                ]
-
-            find_by_vector_values[str(n_index)].append(find_by_vector_time)
-            create_values[str(n_index)].append(create_time)
-        except Exception as e:
-            console.print(f'Storage Backend {backend} failed')
-            raise e
-
-find_df = pd.DataFrame(find_by_vector_values)
-find_df.index = [backend for backend, _ in storage_backends]
-find_df = find_df.drop(['sqlite'], errors='ignore')
-print(find_df)
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(17, 5))
-
-find_df.plot(
-    kind="bar",
-    ax=ax1,
-    fontsize=16,
-    color=sns.color_palette('muted')[1:4],
-    # title='Find by vector per backend and dataset size',
-    # ylabel='seconds',
-    rot=0,
-)
-ax1.set_ylabel('seconds', fontsize=18)
-ax1.set_title('Find by vector per backend and dataset size', fontsize=18)
-
-threshold = 0.3
-ax1.hlines(y=threshold, xmin=-20, xmax=20, linewidth=2, color='r', linestyle='--')
-
-create_df = pd.DataFrame(create_values)
-create_df.index = [backend for backend, _ in storage_backends]
-
-create_df = create_df.drop(['memory'])
-print(create_df)
-create_df.plot(
-    kind="bar",
-    ax=ax2,
-    fontsize=16,
-    color=sns.color_palette('muted')[1:4],
-    # title='Indexing per backend and dataset size',
-    # ylabel='seconds',
-    rot=0,
-)
-
-ax2.set_ylabel('seconds', fontsize=18)
-ax2.set_title('Indexing per backend and dataset size', fontsize=18)
-
-plt.tight_layout()
-ax1.legend(fontsize=15)
-ax2.legend(fontsize=15)
-
-plt.savefig('benchmark.svg')
-console.print(table)
-
-benchmark_df.to_csv('benchmark-seconds.csv')
-
-benchmark_df['Indexing time (C)'] = benchmark_df['Indexing time (C)'].apply(
-    lambda value: 1_000_000 / value
-)
-benchmark_df['Query (R)'] = benchmark_df['Query (R)'].apply(lambda value: 1 / value)
-benchmark_df['Update (U)'] = benchmark_df['Update (U)'].apply(lambda value: 1 / value)
-benchmark_df['Delete (D)'] = benchmark_df['Delete (D)'].apply(lambda value: 1 / value)
-benchmark_df['Find by vector'] = benchmark_df['Find by vector'].apply(
-    lambda value: 1 / value
-)
-benchmark_df['Find by condition'] = benchmark_df['Find by condition'].apply(
-    lambda value: 1 / value
-)
-
-benchmark_df.to_csv('benchmark-qps.csv')
--- scripts/benchmarking_utils.py
+++ scripts/benchmarking_utils.py
@@ -2,6 +2,14 @@ import functools
 from time import perf_counter
 from typing import Iterable
 
+import random
+
+import pandas as pd
+
+from rich.console import Console
+from rich.table import Table
+
+from docarray import DocumentArray
 from docarray import Document
 import numpy as np
 import seaborn as sns
@@ -64,6 +72,143 @@ def fmt(value, unit):
     return '{:.3f} {}'.format(value, unit)
 
 
+def get_configuration_storage_backends(argparse, D):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--default-hnsw',
+        help='Whether to use default HNSW configurations',
+        action='store_true',
+    )
+
+    parser.add_argument(
+        '--exclude-backends',
+        help='list of comma separated backends to exclude from the benchmarks',
+        type=str,
+    )
+
+    args = parser.parse_args()
+
+    if args.default_hnsw:
+        storage_backends = [
+            ('memory', None),
+            ('sqlite', None),
+            (
+                'annlite',
+                {
+                    'n_dim': D,
+                    'columns': {'i': 'int'},
+                },
+            ),
+            (
+                'qdrant',
+                {
+                    'n_dim': D,
+                    'scroll_batch_size': 8,
+                    'port': '41233',
+                },
+            ),
+            (
+                'weaviate',
+                {
+                    'n_dim': D,
+                    'port': '41234',
+                    'columns': {'i': 'int'},
+                },
+            ),
+            (
+                'elasticsearch',
+                {
+                    'n_dim': D,
+                    'hosts': 'http://localhost:41235',
+                    'columns': {'i': 'int'},
+                    'es_config': {'timeout': 1000},
+                },
+            ),
+            (
+                'redis',
+                {
+                    'n_dim': D,
+                    'port': '41236',
+                    'columns': {'i': 'int'},
+                },
+            ),
+        ]
+    else:
+        storage_backends = [
+            ('memory', None),
+            ('sqlite', None),
+            (
+                'annlite',
+                {
+                    'n_dim': D,
+                    'ef_construction': 100,
+                    'ef_search': 100,
+                    'max_connection': 16,
+                    'columns': {'i': 'int'},
+                },
+            ),
+            (
+                'qdrant',
+                {
+                    'n_dim': D,
+                    'scroll_batch_size': 8,
+                    'ef_construct': 100,
+                    'm': 16,
+                    'port': '41233',
+                },
+            ),
+            (
+                'weaviate',
+                {
+                    'n_dim': D,
+                    'ef': 100,
+                    'ef_construction': 100,
+                    'max_connections': 16,
+                    'port': '41234',
+                    'columns': {'i': 'int'},
+                },
+            ),
+            (
+                'elasticsearch',
+                {
+                    'n_dim': D,
+                    'ef_construction': 100,
+                    'm': 16,
+                    'hosts': 'http://localhost:41235',
+                    'columns': {'i': 'int'},
+                },
+            ),
+            (
+                'redis',
+                {
+                    'n_dim': D,
+                    'ef_construction': 100,
+                    'm': 16,
+                    'ef_runtime': 100,
+                    'port': '41236',
+                },
+            ),
+        ]
+
+    storage_backends = [
+        (backend, config)
+        for backend, config in storage_backends
+        if backend not in (args.exclude_backends or '').split(',')
+    ]
+    return storage_backends
+
+
+storage_backend_filters = {
+    'memory': {'tags__i': {'$eq': 0}},
+    'sqlite': {'tags__i': {'$eq': 0}},
+    'annlite': {'i': {'$eq': 0}},
+    'qdrant': {'tags__i': {'$eq': 0}},
+    'weaviate': {'path': 'i', 'operator': 'Equal', 'valueInt': 0},
+    'elasticsearch': {'match': {'i': 0}},
+    'redis': {'i': {'$eq': 0}},
+}
+
+
 def recall(predicted, relevant, eval_at):
     if eval_at == 0:
         return 0.0
@@ -94,8 +239,276 @@ def recall_from_numpy(predicted, relevant, eval_at: int):
     return n_predicted_and_relevant / len(relevant)
 
 
-def save_benchmark_df(benchmark_df):
-    benchmark_df.to_csv('benchmark-seconds.csv')
+def run_benchmark(
+    X_tr,
+    X_te,
+    dataset,
+    n_index_values,
+    n_vector_queries,
+    n_query,
+    storage_backends,
+    K,
+    D,
+):
+    table = Table(
+        title=f'DocArray Benchmarking n_index={n_index_values[-1]} n_query={n_query} D={D} K={K}'
+    )
+    benchmark_df = pd.DataFrame(
+        {
+            'Storage Backend': [],
+            'Indexing time (C)': [],
+            'Query (R)': [],
+            'Update (U)': [],
+            'Delete (D)': [],
+            'Find by vector': [],
+            f'Recall at k={K} for vector search': [],
+            'Find by condition': [],
+        }
+    )
+
+    for col in benchmark_df.columns:
+        table.add_column(col)
+
+    console = Console()
+    find_by_vector_values = {str(n_index): [] for n_index in n_index_values}
+    create_values = {str(n_index): [] for n_index in n_index_values}
+
+    console.print(f'Reading dataset')
+    docs = get_docs(X_tr)
+    docs_to_delete = random.sample(docs, n_query)
+    docs_to_update = random.sample(docs, n_query)
+    vector_queries = [x for x in X_te]
+    ground_truth = [x[0:K] for x in dataset['neighbors'][0 : len(vector_queries)]]
+
+    for idx, n_index in enumerate(n_index_values):
+        for backend, config in storage_backends:
+            try:
+                console.print('\nBackend:', backend.title())
+                # for n_i in n_index:
+                if not config:
+                    da = DocumentArray(storage=backend)
+                else:
+                    da = DocumentArray(storage=backend, config=config)
+
+                console.print(f'\tindexing {n_index} docs ...')
+                create_time, _ = create(da, docs)
+
+                # for n_q in n_query:
+                console.print(f'\treading {n_query} docs ...')
+                read_time, _ = read(
+                    da,
+                    random.sample([d.id for d in docs], n_query),
+                )
+
+                console.print(f'\tupdating {n_query} docs ...')
+                update_time, _ = update(da, docs_to_update)
+
+                console.print(f'\tdeleting {n_query} docs ...')
+                delete_time, _ = delete(da, [d.id for d in docs_to_delete])
+
+                console.print(
+                    f'\tfinding {n_query} docs by vector averaged {n_vector_queries} times ...'
+                )
+                if backend == 'memory':
+                    find_by_vector_time, results = find_by_vector(
+                        da, vector_queries[0], limit=K
+                    )
+                    recall_at_k = recall_from_numpy(
+                        np.array(results[:, 'tags__i']), ground_truth[0], K
+                    )
+                elif backend == 'sqlite':
+                    find_by_vector_time, result = find_by_vector(
+                        da, vector_queries[0], limit=K
+                    )
+                    # recall_at_k = 1
+                    recall_at_k = recall_from_numpy(
+                        np.array(results[:, 'tags__i']), ground_truth[0], K
+                    )
+                else:
+                    recall_at_k_values = []
+                    find_by_vector_times = []
+                    for i, query in enumerate(vector_queries):
+                        find_by_vector_time, results = find_by_vector(
+                            da, query, limit=K
+                        )
+                        find_by_vector_times.append(find_by_vector_time)
+                        recall_at_k_values.append(
+                            recall_from_numpy(
+                                np.array(results[:, 'tags__i']), ground_truth[i], K
+                            )
+                        )
+
+                    recall_at_k = np.mean(recall_at_k_values)
+                    find_by_vector_time = np.mean(find_by_vector_times)
+
+                console.print(f'\tfinding {n_query} docs by condition ...')
+                find_by_condition_time, _ = find_by_condition(
+                    da, storage_backend_filters[backend]
+                )
+
+                if idx == len(n_index_values) - 1:
+                    table.add_row(
+                        backend.title(),
+                        fmt(create_time, 's'),
+                        fmt(read_time * 1000, 'ms'),
+                        fmt(update_time * 1000, 'ms'),
+                        fmt(delete_time * 1000, 'ms'),
+                        fmt(find_by_vector_time, 's'),
+                        '{:.3f}'.format(recall_at_k),
+                        fmt(find_by_condition_time, 's'),
+                    )
+                    benchmark_df.loc[len(benchmark_df.index)] = [
+                        backend.title(),
+                        create_time,
+                        read_time,
+                        update_time,
+                        delete_time,
+                        find_by_vector_time,
+                        recall_at_k,
+                        find_by_condition_time,
+                    ]
+
+                # store find_by_vector time
+                find_by_vector_values[str(n_index)].append(find_by_vector_time)
+                create_values[str(n_index)].append(create_time)
+                da.clear()
+                del da
+            except Exception as e:
+                console.print(f'Storage Backend {backend} failed: {e}')
+
+        console.print(table)
+        return find_by_vector_values, create_values, benchmark_df
+
+
+def run_benchmark2(
+    train,
+    test,
+    ground_truth,
+    n_index,
+    n_vector_queries,
+    n_query,
+    storage_backends,
+    K,
+    D,
+):
+    table = Table(
+        title=f'DocArray Benchmarking n_index={n_index} n_query={n_query} D={D} K={K}'
+    )
+    benchmark_df = pd.DataFrame(
+        {
+            'Storage Backend': [],
+            'Indexing time (C)': [],
+            'Query (R)': [],
+            'Update (U)': [],
+            'Delete (D)': [],
+            'Find by vector': [],
+            f'Recall at k={K} for vector search': [],
+            'Find by condition': [],
+        }
+    )
+
+    for col in benchmark_df.columns:
+        table.add_column(col)
+
+    console = Console()
+
+    console.print(f'Reading dataset')
+    docs = get_docs(train)
+    docs_to_delete = random.sample(docs, n_query)
+    docs_to_update = random.sample(docs, n_query)
+    vector_queries = [x for x in test]
+
+    find_by_vector_time_all = []
+    create_time_all = []
+
+    for backend, config in storage_backends:
+        try:
+            console.print('\nBackend:', backend.title())
+            if not config:
+                da = DocumentArray(storage=backend)
+            else:
+                da = DocumentArray(storage=backend, config=config)
+
+            console.print(f'\tindexing {n_index} docs ...')
+            create_time, _ = create(da, docs)
+
+            # for n_q in n_query:
+            console.print(f'\treading {n_query} docs ...')
+            read_time, _ = read(
+                da,
+                random.sample([d.id for d in docs], n_query),
+            )
+
+            console.print(f'\tupdating {n_query} docs ...')
+            update_time, _ = update(da, docs_to_update)
+
+            console.print(f'\tdeleting {n_query} docs ...')
+            delete_time, _ = delete(da, [d.id for d in docs_to_delete])
+
+            console.print(
+                f'\tfinding {n_query} docs by vector averaged {n_vector_queries} times ...'
+            )
+            if backend == 'sqlite':
+                find_by_vector_time, result = find_by_vector(
+                    da, vector_queries[0], limit=K
+                )
+                recall_at_k = recall(result, ground_truth[0], K)
+            else:
+                recall_at_k_values = []
+                find_by_vector_times = []
+                for i, query in enumerate(vector_queries):
+                    find_by_vector_time, results = find_by_vector(da, query, limit=K)
+                    find_by_vector_times.append(find_by_vector_time)
+                    if backend == 'memory':
+                        ground_truth.append(results)
+                        recall_at_k_values.append(1)
+                    else:
+                        recall_at_k_values.append(recall(results, ground_truth[i], K))
+
+                recall_at_k = np.mean(recall_at_k_values)
+                find_by_vector_time = np.mean(find_by_vector_times)
+
+            console.print(f'\tfinding {n_query} docs by condition ...')
+            find_by_condition_time, _ = find_by_condition(
+                da, storage_backend_filters[backend]
+            )
+
+            table.add_row(
+                backend.title(),
+                fmt(create_time, 's'),
+                fmt(read_time * 1000, 'ms'),
+                fmt(update_time * 1000, 'ms'),
+                fmt(delete_time * 1000, 'ms'),
+                fmt(find_by_vector_time, 's'),
+                '{:.3f}'.format(recall_at_k),
+                fmt(find_by_condition_time, 's'),
+            )
+            benchmark_df.loc[len(benchmark_df.index)] = [
+                backend.title(),
+                create_time,
+                read_time,
+                update_time,
+                delete_time,
+                find_by_vector_time,
+                recall_at_k,
+                find_by_condition_time,
+            ]
+
+            find_by_vector_time_all.append(find_by_vector_time)
+            create_time_all.append(create_time)
+
+            da.clear()
+            del da
+        except Exception as e:
+            console.print(f'Storage Backend {backend} failed: {e}')
+
+    # print(find_by_vector_time_all)
+    console.print(table)
+    return find_by_vector_time_all, create_time_all, benchmark_df
+
+
+def save_benchmark_df(benchmark_df, n_index):
+    benchmark_df.to_csv(f'benchmark-seconds-{n_index}.csv')
 
     benchmark_df['Indexing time (C)'] = benchmark_df['Indexing time (C)'].apply(
         lambda value: 1_000_000 / value
@@ -114,7 +527,7 @@ def save_benchmark_df(benchmark_df):
         lambda value: 1 / value
     )
 
-    benchmark_df.to_csv('benchmark-qps.csv')
+    benchmark_df.to_csv(f'benchmark-qps-{n_index}.csv')
 
 
 def plot_results(
@@ -165,6 +578,6 @@ def plot_results(
     ax2.set_title('Indexing per backend', fontsize=18)
 
     plt.tight_layout()
-    # ax1.legend(fontsize=15)
-    # ax2.legend(fontsize=15)
+    ax1.legend(fontsize=15)
+    ax2.legend(fontsize=15)
     plt.savefig('benchmark.svg')
