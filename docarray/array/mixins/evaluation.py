cfb6b2d1e41c69797b37290757f0971ee3c2bb8b
guenthermi <guenthermi50@gmail.com>
2022-10-17 14:20:15 +0200

    refactor: implement review notes

--- docarray/array/mixins/evaluation.py
+++ docarray/array/mixins/evaluation.py
@@ -9,9 +9,38 @@ if TYPE_CHECKING:
     from docarray import Document, DocumentArray
 
 
+def _evaluate_deprecation(f):
+    """Raises a deprecation warning if the user executes the evaluate function with
+    the old interface and adjust the input to fit the new interface."""
+
+    def func(*args, **kwargs):
+        if len(args) > 1:
+            if not (isinstance(args[1], Callable) or isinstance(args[1], str)):
+                kwargs['ground_truth'] = args[1]
+                args = [args[0]] + list(args[2:])
+                warnings.warn(
+                    'The `other` attribute in `evaluate()` is transfered from a '
+                    'positional attribute into the keyword attribute `ground_truth`.'
+                    'Using it as a positional attribute is deprecated and will be removed '
+                    'in the next version.',
+                    DeprecationWarning,
+                )
+        if 'other' in kwargs:
+            kwargs['ground_truth'] = kwargs['other']
+            warnings.warn(
+                '`other` is renamed to `groundtruth` in `evaluate()`, the usage of `other` is '
+                'deprecated and will be removed in the next version.',
+                DeprecationWarning,
+            )
+        return f(*args, **kwargs)
+
+    return func
+
+
 class EvaluationMixin:
     """A mixin that provides ranking evaluation functionality to DocumentArrayLike objects"""
 
+    @_evaluate_deprecation
     def evaluate(
         self,
         metric: Union[str, Callable[..., float]],
@@ -19,18 +48,20 @@ class EvaluationMixin:
         hash_fn: Optional[Callable[['Document'], str]] = None,
         metric_name: Optional[str] = None,
         strict: bool = True,
-        label_tag='label',
+        label_tag: Optional[str] = 'label',
         **kwargs,
     ) -> Optional[float]:
         """
         Compute ranking evaluation metrics for a given `DocumentArray` when compared
         with a groundtruth.
 
-        This implementation expects the documents and their matches to have labels
-        annotated inside the tag with the key specified in the `label_tag` attribute.
-        Alternatively, one can provide a `ground_truth` DocumentArray that is
-        structurally identical to `self`. In this case, this function compares the
-        `matches` of `documents` inside the `DocumentArray`.
+        If one provides a `ground_truth` DocumentArray that is structurally identical
+        to `self`, this function compares the `matches` of `documents` inside the
+        `DocumentArray` to this `ground_truth`.
+        Alternatively, one can directly annotate the documents by adding labels in the
+        form of tags with the key specified in the `label_tag` attribute.
+        Those tags need to be added to `self` as well as to the documents in the
+        matches properties.
 
         This method will fill the `evaluations` field of Documents inside this
         `DocumentArray` and will return the average of the computations
@@ -46,6 +77,7 @@ class EvaluationMixin:
         :param strict: If set, then left and right sides are required to be fully
             aligned: on the length, and on the semantic of length. These are preventing
             you to evaluate on irrelevant matches accidentally.
+        :param label_tag: Specifies the tag which contains the labels.
         :param kwargs: Additional keyword arguments to be passed to `metric_fn`
         :return: The average evaluation computed or a list of them if multiple metrics
             are required
@@ -57,7 +89,7 @@ class EvaluationMixin:
         elif label_tag in self[0].tags:
             if ground_truth:
                 warnings.warn(
-                    'An ground_truth attribute is provided but does not '
+                    'A ground_truth attribute is provided but does not '
                     'contain matches. The labels are used instead and '
                     'ground_truth is ignored.'
                 )
@@ -117,7 +149,10 @@ class EvaluationMixin:
                     1 if m.tags[label_tag] == d.tags[label_tag] else 0 for m in targets
                 ]
             else:
-                raise RuntimeError(f'Unsupported groundtruth type {ground_truth_type}')
+                raise RuntimeError(
+                    'Could not identify which kind of ground truth'
+                    'information is provided to evaluate the matches.'
+                )
 
             r = metric_fn(binary_relevance, max_rel=max_rel, **kwargs)
             d.evaluations[metric_name] = NamedScore(
