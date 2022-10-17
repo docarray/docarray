76b49aca8fe74d2702cde8b0e958d63b9a24fbab
Jackmin801 <ongjackm@gmail.com>
2022-10-17 19:07:16 +0800

    fix: take out uneccessary override

--- docarray/array/storage/annlite/getsetdel.py
+++ docarray/array/storage/annlite/getsetdel.py
@@ -42,9 +42,6 @@ class GetSetDelMixin(BaseGetSetDelMixin):
     def _del_docs_by_ids(self, ids):
         self._annlite.delete(ids)
 
-    def sync(self) -> None:
-        super().sync()
-
     def __del__(self) -> None:
         if not self._persist:
             self._offset2ids.clear()
