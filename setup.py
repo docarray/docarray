b99d438ff376af81202d304e22754a4d97b96194
jemmyshin <jem.fu@jina.ai>
2022-10-20 14:50:21 +0800

    fix: ensure ci use main branch of annlite

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ setup(
             'jupyterlab',
             'transformers>=4.16.2',
             'weaviate-client~=3.3.0',
-            # 'annlite>=0.3.12',
+            'annlite>=0.3.12',
             'elasticsearch>=8.2.0',
             'redis>=4.3.0',
             'jina',
