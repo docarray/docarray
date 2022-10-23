df156a7c75ac0647628f4e602a9133fa742ea13d
Jackmin801 <ongjackm@gmail.com>
2022-10-24 00:31:17 +0800

    style: blacken doc scripts

--- docs/y2j.py
+++ docs/y2j.py
@@ -1,5 +1,7 @@
 import yaml
 import json
 
-with open('_versions.yml') as versions_yml, open('_versions.json', 'w') as versions_json:
+with open("_versions.yml") as versions_yml, open(
+    "_versions.json", "w"
+) as versions_json:
     json.dump(yaml.safe_load(versions_yml), versions_json)
