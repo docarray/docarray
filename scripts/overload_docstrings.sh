f58bd7be5b53092a1e3b0589e765e038fa205a27
Johannes Messner <messnerjo@gmail.com>
2022-10-31 13:51:03 +0100

    fix: print some stuff

--- scripts/overload_docstrings.sh
+++ scripts/overload_docstrings.sh
@@ -3,4 +3,4 @@
 # this script is run by jina-dev-bot
 
 # fix overload doccstrings && black it
-arr=( $(python overload_docstrings.py) ) && black -S "${arr[@]}"
+arr=( $(python overload_docstrings.py) ) && echo "${arr}" && black -S "${arr[@]}"
