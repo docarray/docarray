cb96d7f72c4155b9b9d87c3668284fb7eea2ef08
anna-charlotte <anna-charlotte@users.noreply.github.com>
2022-11-08 14:48:54 +0100

    fix: set newer hubble version in setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     long_description=_long_description,
     long_description_content_type='text/markdown',
     zip_safe=False,
-    install_requires=['numpy', 'rich>=12.0.0', 'jina-hubble-sdk>=0.13.1'],
+    install_requires=['numpy', 'rich>=12.0.0', 'jina-hubble-sdk>=0.24.0'],
     extras_require={
         # req usage, please see https://docarray.jina.ai/#install
         'common': [
