a7364ca96b4180e23a45971ca8f5f3904febf1bd
anna-charlotte <charlotte.gerhaher@jina.ai>
2022-11-15 13:44:19 +0100

    docs: update suggestion
    Co-authored-by: Alex Cureton-Griffiths <alexcg1@users.noreply.github.com>
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>

--- docarray/document/mixins/mesh.py
+++ docarray/document/mixins/mesh.py
@@ -114,7 +114,7 @@ class MeshDataMixin:
         return self
 
     def load_uris_to_rgbd_tensor(self: 'T') -> 'T':
-        """Load RGB image from :attr:`.uri` of :attr:`.chunks[0]` and the depth image from :attr:`.uri` of :attr:`.chunks[1]` and merge them into :attr:`.tensor`.
+        """Load RGB image from :attr:`.uri` of :attr:`.chunks[0]` and depth image from :attr:`.uri` of :attr:`.chunks[1]` and merge them into :attr:`.tensor`.
 
         :return: itself after processed
         """
