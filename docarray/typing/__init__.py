74e4ae35e6f397b9ab725a6a24ba14e8c7b4685c
Johannes Messner <messnerjo@gmail.com>
2022-11-16 09:53:28 +0100

    feat: torch tensor type

--- docarray/typing/__init__.py
+++ docarray/typing/__init__.py
@@ -1,6 +1,6 @@
 from docarray.typing.embedding import Embedding
 from docarray.typing.id import ID
-from docarray.typing.tensor import Tensor
+from docarray.typing.tensor import Tensor, TorchTensor
 from docarray.typing.url import AnyUrl, ImageUrl
 
-__all__ = ['Tensor', 'Embedding', 'ImageUrl', 'AnyUrl', 'ID']
+__all__ = ['Tensor', 'Embedding', 'ImageUrl', 'AnyUrl', 'ID', 'TorchTensor']
