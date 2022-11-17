81f7810f0f3ba3745d70b90a2f14bcd993b5063b
Johannes Messner <messnerjo@gmail.com>
2022-11-17 18:11:08 +0100

    feat: add new type for image urls
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>

--- docarray/typing/url/image_url.py
+++ docarray/typing/url/image_url.py
@@ -1,7 +1,14 @@
+import io
+import struct
+from typing import Optional, Union
+
 import numpy as np
 
 from docarray.proto import NodeProto
 from docarray.typing.url.any_url import AnyUrl
+from docarray.typing.url.helper import _uri_to_blob
+
+IMAGE_FILE_FORMATS = ('png', 'jpeg', 'jpg')
 
 
 class ImageUrl(AnyUrl):
@@ -14,12 +21,172 @@ class ImageUrl(AnyUrl):
         """
         return NodeProto(image_url=str(self))
 
-    def load(self) -> np.ndarray:
-        """
-        transform the url in a image Tensor
+    # TODO(johannes) add validation for image URI
 
-        this is just a patch we will move the function from old docarray
-        :return: tensor image
+    def load(
+        self,
+        width: Optional[int] = None,
+        height: Optional[int] = None,
+        channel_axis: int = -1,
+        timeout: Optional[float] = None,
+    ) -> np.ndarray:
         """
+        Load the data from the url into a numpy.ndarray image tensor
 
-        return np.zeros((3, 224, 224))
+        :param width: width of the image tensor.
+        :param height: height of the image tensor.
+        :param channel_axis: axis where to put the image color channel;
+            ``-1`` indicates the color channel info at the last axis
+        :param timeout: timeout (sec) for urlopen network request.
+            Only relevant if URL is not local
+        :return: np.ndarray representing the image as RGB values
+        """
+        # TODO(johannes) the axis argument is confusing, because it if it is not -1,
+        #  it is unclear where width and height end up
+        # instead we should allow the user to pass an axis for width, height, and axis
+
+        buffer = _uri_to_blob(self, timeout=timeout)
+        tensor = _to_image_tensor(io.BytesIO(buffer), width=width, height=height)
+        return _move_channel_axis(tensor, target_channel_axis=channel_axis)
+
+    def load_to_bytes(
+        self,
+        image_format: str = 'png',
+        width: Optional[int] = None,
+        height: Optional[int] = None,
+        timeout: Optional[float] = None,
+    ) -> bytes:
+        """Load image at URL to bytes (buffer).
+
+        :param image_format: File format of the file located the the url.
+            Supported formats are `png`, `jpg`, and `jpeg`.
+        :param width: Before converting to bytes, resize the image to this width.
+        :param height: Before converting to bytes, resize the image to this height.
+        :param timeout: timeout (sec) for urlopen network request.
+            Only relevant if URL is not local
+        :return: The image as bytes (buffer).
+        """
+        channel_axis = -1
+        image_tensor = self.load(
+            width=width, height=height, channel_axis=channel_axis, timeout=timeout
+        )
+        return _image_tensor_to_bytes(image_tensor, image_format=image_format)
+
+
+def _image_tensor_to_bytes(arr: np.ndarray, image_format: str) -> bytes:
+    """
+    Convert image-ndarray to buffer bytes.
+
+    :param arr: Data representations of the png.
+    :param image_format: `png` or `jpeg`
+    :return: Png in buffer bytes.
+    """
+
+    if image_format not in IMAGE_FILE_FORMATS:
+        raise ValueError(
+            f'image_format must be one of {IMAGE_FILE_FORMATS},'
+            f'receiving `{image_format}`'
+        )
+    if image_format == 'jpg':
+        image_format = 'jpeg'  # unify it to ISO standard
+
+    arr = arr.astype(np.uint8).squeeze()
+
+    if arr.ndim == 1:
+        # note this should be only used for MNIST/FashionMNIST dataset,
+        # because of the nature of these two datasets
+        # no other image data should flattened into 1-dim array.
+        image_bytes = _png_to_buffer_1d(arr, 28, 28)
+    elif arr.ndim == 2:
+        from PIL import Image
+
+        im = Image.fromarray(arr).convert('L')
+        image_bytes = _pillow_image_to_buffer(im, image_format=image_format.upper())
+    elif arr.ndim == 3:
+        from PIL import Image
+
+        im = Image.fromarray(arr).convert('RGB')
+        image_bytes = _pillow_image_to_buffer(im, image_format=image_format.upper())
+    else:
+        raise ValueError(
+            f'{arr.shape} ndarray can not be converted into an image buffer.'
+        )
+
+    return image_bytes
+
+
+def _png_to_buffer_1d(arr: np.ndarray, width: int, height: int) -> bytes:
+    import zlib
+
+    pixels = []
+    for p in arr[::-1]:
+        pixels.extend([p, p, p, 255])
+    buf = bytearray(pixels)
+
+    # reverse the vertical line order and add null bytes at the start
+    width_byte_4 = width * 4
+    raw_data = b''.join(
+        b'\x00' + buf[span : span + width_byte_4]
+        for span in range((height - 1) * width_byte_4, -1, -width_byte_4)
+    )
+
+    def png_pack(png_tag, data):
+        chunk_head = png_tag + data
+        return (
+            struct.pack('!I', len(data))
+            + chunk_head
+            + struct.pack('!I', 0xFFFFFFFF & zlib.crc32(chunk_head))
+        )
+
+    png_bytes = b''.join(
+        [
+            b'\x89PNG\r\n\x1a\n',
+            png_pack(b'IHDR', struct.pack('!2I5B', width, height, 8, 6, 0, 0, 0)),
+            png_pack(b'IDAT', zlib.compress(raw_data, 9)),
+            png_pack(b'IEND', b''),
+        ]
+    )
+
+    return png_bytes
+
+
+def _pillow_image_to_buffer(image, image_format: str) -> bytes:
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format=image_format)
+    img_byte_arr = img_byte_arr.getvalue()
+    return img_byte_arr
+
+
+def _move_channel_axis(
+    tensor: np.ndarray, original_channel_axis: int = -1, target_channel_axis: int = -1
+) -> np.ndarray:
+    """Moves channel axis around."""
+    if original_channel_axis != target_channel_axis:
+        tensor = np.moveaxis(tensor, original_channel_axis, target_channel_axis)
+    return tensor
+
+
+def _to_image_tensor(
+    source: Union[str, bytes, io.BytesIO],
+    width: Optional[int] = None,
+    height: Optional[int] = None,
+) -> 'np.ndarray':
+    """
+    Convert an image blob to tensor
+
+    :param source: binary blob or file path
+    :param width: the width of the image tensor.
+    :param height: the height of the tensor.
+    :return: image tensor
+    """
+    from PIL import Image as PILImage
+
+    raw_img = PILImage.open(source)
+    if width or height:
+        new_width = width or raw_img.width
+        new_height = height or raw_img.height
+        raw_img = raw_img.resize((new_width, new_height))
+    try:
+        return np.array(raw_img.convert('RGB'))
+    except Exception:
+        return np.array(raw_img)
--- tests/units/typing/url/test_image_url.py
+++ tests/units/typing/url/test_image_url.py
@@ -1,11 +1,27 @@
+import os
+import urllib
+
 import numpy as np
+import pytest
 from pydantic.tools import parse_obj_as
 
 from docarray.typing import ImageUrl
 
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+PATH_TO_IMAGE_DATA = os.path.join(CUR_DIR, '..', '..', '..', 'toydata', 'image-data')
+IMAGE_PATHS = {
+    'png': os.path.join(PATH_TO_IMAGE_DATA, 'so_good.png'),
+    'jpg': os.path.join(PATH_TO_IMAGE_DATA, '05984.jpg'),
+    'jpeg': os.path.join(PATH_TO_IMAGE_DATA, '05984-2.jpeg'),
+}
+REMOTE_JPG = (
+    'https://upload.wikimedia.org/wikipedia/commons/8/80/'
+    'Dag_Sebastian_Ahlander_at_G%C3%B6teborg_Book_Fair_2012b.jpg'
+)
+
 
 def test_image_url():
-    uri = parse_obj_as(ImageUrl, 'http://jina.ai/img.png')
+    uri = parse_obj_as(ImageUrl, REMOTE_JPG)
 
     tensor = uri.load()
 
@@ -14,6 +30,87 @@ def test_image_url():
 
 def test_proto_image_url():
 
-    uri = parse_obj_as(ImageUrl, 'http://jina.ai/img.png')
+    uri = parse_obj_as(ImageUrl, REMOTE_JPG)
 
     uri._to_node_protobuf()
+
+
+@pytest.mark.parametrize(
+    'image_format,path_to_img',
+    [
+        ('png', IMAGE_PATHS['png']),
+        ('jpg', IMAGE_PATHS['jpg']),
+        ('jpeg', IMAGE_PATHS['jpeg']),
+        ('remote-jpg', REMOTE_JPG),
+    ],
+)
+def test_load(image_format, path_to_img):
+    url = parse_obj_as(ImageUrl, path_to_img)
+    tensor = url.load()
+    assert isinstance(tensor, np.ndarray)
+
+
+@pytest.mark.parametrize(
+    'image_format,path_to_img',
+    [
+        ('png', IMAGE_PATHS['png']),
+        ('jpg', IMAGE_PATHS['jpg']),
+        ('jpeg', IMAGE_PATHS['jpeg']),
+        ('remote-jpg', REMOTE_JPG),
+    ],
+)
+@pytest.mark.parametrize('width,height', [(224, None), (None, 224), (224, 224)])
+def test_load_width_height(image_format, path_to_img, width, height):
+    url = parse_obj_as(ImageUrl, path_to_img)
+    tensor = url.load(width=width, height=height)
+    assert isinstance(tensor, np.ndarray)
+
+    shape = tensor.shape
+    if width:
+        assert shape[1] == width
+    if height:
+        assert shape[0] == height
+
+
+@pytest.mark.parametrize(
+    'image_format,path_to_img',
+    [
+        ('png', IMAGE_PATHS['png']),
+        ('jpg', IMAGE_PATHS['jpg']),
+        ('jpeg', IMAGE_PATHS['jpeg']),
+        ('remote-jpg', REMOTE_JPG),
+    ],
+)
+@pytest.mark.parametrize('channel_axis', [0, 1, 2, -1])
+def test_load_channel_axis(image_format, path_to_img, channel_axis):
+    url = parse_obj_as(ImageUrl, path_to_img)
+    tensor = url.load(channel_axis=channel_axis)
+    assert isinstance(tensor, np.ndarray)
+
+    shape = tensor.shape
+    assigned_channel_axis = 2 if channel_axis == -1 else channel_axis
+    assert shape[assigned_channel_axis] == 3
+
+
+def test_load_timeout():
+    url = parse_obj_as(ImageUrl, REMOTE_JPG)
+    with pytest.raises(urllib.error.URLError):
+        _ = url.load(timeout=0.001)
+
+
+@pytest.mark.parametrize(
+    'image_format,path_to_img',
+    [
+        ('png', IMAGE_PATHS['png']),
+        ('jpg', IMAGE_PATHS['jpg']),
+        ('jpeg', IMAGE_PATHS['jpeg']),
+        ('jpg', REMOTE_JPG),
+    ],
+)
+def test_load_to_bytes(image_format, path_to_img):
+    url = parse_obj_as(ImageUrl, path_to_img)
+    _bytes = url.load_to_bytes()
+    assert isinstance(_bytes, bytes)
+
+
+# TODO(johannes) test to_bytes more thoroughly
