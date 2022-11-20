25906b9074c1307369d26f4229bb37783fdf3703
Johannes Messner <messnerjo@gmail.com>
2022-11-20 19:18:57 +0100

    test: fix test
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>

--- tests/integrations/predefined_document/test_image.py
+++ tests/integrations/predefined_document/test_image.py
@@ -1,13 +1,12 @@
 import numpy as np
 
 from docarray import Image
-from docarray.typing import Tensor
 
 
 def test_image():
 
-    image = Image(uri='http://jina.ai')
+    image = Image(url='http://jina.ai')
 
-    image.tensor = image.uri.load()
+    image.tensor = image.url.load()
 
     assert isinstance(image.tensor, np.ndarray)
