c2f12f57b64b639adc09bd90c2581ee8b4ad01ef
Johannes Messner <44071807+JohannesMessner@users.noreply.github.com>
2022-11-21 14:03:35 +0100

    feat: image url (#811)
    * feat: add tensor type for ndarray
    
    * fix: fix mypy typing
    
    * feat: torch tensor type
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * fix: protobuf for pytorch type
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * ci: install all extras in the ci
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * refactor: make nice looking
    
    * docs: update docarray/typing/tensor/torch_tensor.py
    
    Co-authored-by: samsja <55492238+samsja@users.noreply.github.com>
    Signed-off-by: Johannes Messner <44071807+JohannesMessner@users.noreply.github.com>
    
    * refactor: code style
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * fix: black and mypy
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * fix: suppress mypy import error
    
    * ci: fix ci install
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * feat: add new type for image urls
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * feat: add new type for image urls
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * test: add real existing url to test
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * test: test output of image buffer loading
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * feat: add validation for image url
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * feat: specify image axis permutation
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * docs: add docstrings
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * docs: make strings uniform
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * test: pass valid url as imageurl
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * chore: fix dependencies
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    
    * chore(ci): add extras to mypy ci
    
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>
    
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>
    Signed-off-by: Johannes Messner <44071807+JohannesMessner@users.noreply.github.com>
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>
    Co-authored-by: samsja <55492238+samsja@users.noreply.github.com>
    Co-authored-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>

--- tests/units/typing/url/test_image_url.py
+++ tests/units/typing/url/test_image_url.py
@@ -1,11 +1,28 @@
+import os
+import urllib
+
 import numpy as np
+import PIL
+import pytest
 from pydantic.tools import parse_obj_as
 
 from docarray.typing import ImageUrl
 
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+PATH_TO_IMAGE_DATA = os.path.join(CUR_DIR, '..', '..', '..', 'toydata', 'image-data')
+IMAGE_PATHS = {
+    'png': os.path.join(PATH_TO_IMAGE_DATA, 'so_good.png'),
+    'jpg': os.path.join(PATH_TO_IMAGE_DATA, '05984.jpg'),
+    'jpeg': os.path.join(PATH_TO_IMAGE_DATA, '05984-2.jpeg'),
+}
+REMOTE_JPG = (
+    'https://upload.wikimedia.org/wikipedia/commons/8/80/'
+    'Dag_Sebastian_Ahlander_at_G%C3%B6teborg_Book_Fair_2012b.jpg'
+)
+
 
 def test_image_url():
-    uri = parse_obj_as(ImageUrl, 'http://jina.ai/img.png')
+    uri = parse_obj_as(ImageUrl, REMOTE_JPG)
 
     tensor = uri.load()
 
@@ -14,6 +31,120 @@ def test_image_url():
 
 def test_proto_image_url():
 
-    uri = parse_obj_as(ImageUrl, 'http://jina.ai/img.png')
+    uri = parse_obj_as(ImageUrl, REMOTE_JPG)
 
     uri._to_node_protobuf()
+
+
+@pytest.mark.parametrize(
+    'image_format,path_to_img',
+    [
+        ('png', IMAGE_PATHS['png']),
+        ('jpg', IMAGE_PATHS['jpg']),
+        ('jpeg', IMAGE_PATHS['jpeg']),
+        ('remote-jpg', REMOTE_JPG),
+    ],
+)
+def test_load(image_format, path_to_img):
+    url = parse_obj_as(ImageUrl, path_to_img)
+    tensor = url.load()
+    assert isinstance(tensor, np.ndarray)
+
+
+@pytest.mark.parametrize(
+    'image_format,path_to_img',
+    [
+        ('png', IMAGE_PATHS['png']),
+        ('jpg', IMAGE_PATHS['jpg']),
+        ('jpeg', IMAGE_PATHS['jpeg']),
+        ('remote-jpg', REMOTE_JPG),
+    ],
+)
+@pytest.mark.parametrize('width,height', [(224, None), (None, 224), (224, 224)])
+def test_load_width_height(image_format, path_to_img, width, height):
+    url = parse_obj_as(ImageUrl, path_to_img)
+    tensor = url.load(width=width, height=height)
+    assert isinstance(tensor, np.ndarray)
+
+    shape = tensor.shape
+    if width:
+        assert shape[1] == width
+    if height:
+        assert shape[0] == height
+
+
+@pytest.mark.parametrize(
+    'image_format,path_to_img',
+    [
+        ('png', IMAGE_PATHS['png']),
+        ('jpg', IMAGE_PATHS['jpg']),
+        ('jpeg', IMAGE_PATHS['jpeg']),
+        ('remote-jpg', REMOTE_JPG),
+    ],
+)
+@pytest.mark.parametrize(
+    'axis_layout',
+    [
+        ('H', 'W', 'C'),
+        ('H', 'C', 'W'),
+        ('C', 'H', 'W'),
+        ('C', 'W', 'H'),
+        ('W', 'C', 'H'),
+        ('W', 'H', 'C'),
+    ],
+)
+def test_load_channel_axis(image_format, path_to_img, axis_layout):
+    sizes = {'H': 100, 'W': 200, 'C': 3}
+    url = parse_obj_as(ImageUrl, path_to_img)
+    tensor = url.load(axis_layout=axis_layout, height=sizes['H'], width=sizes['W'])
+    assert isinstance(tensor, np.ndarray)
+
+    shape = tensor.shape
+    for axis, axis_name in enumerate(axis_layout):
+        assert shape[axis] == sizes[axis_name]
+
+
+def test_load_timeout():
+    url = parse_obj_as(ImageUrl, REMOTE_JPG)
+    with pytest.raises(urllib.error.URLError):
+        _ = url.load(timeout=0.001)
+
+
+@pytest.mark.parametrize(
+    'image_format,path_to_img',
+    [
+        ('png', IMAGE_PATHS['png']),
+        ('jpg', IMAGE_PATHS['jpg']),
+        ('jpeg', IMAGE_PATHS['jpeg']),
+        ('jpg', REMOTE_JPG),
+    ],
+)
+def test_load_to_bytes(image_format, path_to_img):
+    w, h = 224, 224
+    url = parse_obj_as(ImageUrl, path_to_img)
+    _bytes = url.load_to_bytes(width=w, height=h)
+    assert isinstance(_bytes, bytes)
+    img = PIL.Image.frombytes(mode='1', size=(w, h), data=_bytes)
+    assert isinstance(img, PIL.Image.Image)
+
+
+@pytest.mark.parametrize(
+    'image_format,path_to_img',
+    [
+        ('png', IMAGE_PATHS['png']),
+        ('jpg', IMAGE_PATHS['jpg']),
+        ('jpeg', IMAGE_PATHS['jpeg']),
+        ('jpg', REMOTE_JPG),
+        ('illegal', 'illegal'),
+        ('illegal', 'https://www.google.com'),
+        ('illegal', 'my/local/text/file.txt'),
+    ],
+)
+def test_validation(image_format, path_to_img):
+    if image_format == 'illegal':
+        with pytest.raises(ValueError):
+            parse_obj_as(ImageUrl, path_to_img)
+    else:
+        url = parse_obj_as(ImageUrl, path_to_img)
+        assert isinstance(url, ImageUrl)
+        assert isinstance(url, str)
