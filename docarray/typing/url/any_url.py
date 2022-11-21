14203851fe422bbd371773b7975c9244aaed0310
dong xiang <iamxiangdong@gmail.com>
2022-11-21 18:06:15 +0800

    refactor(v2): renamed variable and files (#796) (#806)
    * refactor(v2): renamed variable and files
    
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>
    
    * refactor(v2): renamd test
    
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>
    
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>
    
    feat: add read from proto
    
    Co-authored-by: samsja <55492238+samsja@users.noreply.github.com>

--- docarray/typing/url/any_url.py
+++ docarray/typing/url/any_url.py
@@ -1,7 +1,12 @@
+from typing import Type, TypeVar
+
 from pydantic import AnyUrl as BaseAnyUrl
+from pydantic import parse_obj_as
 
 from docarray.document.base_node import BaseNode
 from docarray.proto import NodeProto
+
+T = TypeVar('T', bound='AnyUrl')
 
 
 class AnyUrl(BaseAnyUrl, BaseNode):
@@ -13,3 +18,12 @@ class AnyUrl(BaseAnyUrl, BaseNode):
         :return: the nested item protobuf message
         """
         return NodeProto(any_url=str(self))
+
+    @classmethod
+    def from_protobuf(cls: Type[T], pb_msg: 'str') -> T:
+        """
+        read url from a proto msg
+        :param pb_msg:
+        :return: url
+        """
+        return parse_obj_as(cls, pb_msg)
