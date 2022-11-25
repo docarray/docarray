fb83d99ec33188b0e868383bbbad6dca39751735
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2022-11-25 12:16:03 +0100

    fix: remove broken test
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>

--- tests/unit/array/mixins/test_plot.py
+++ tests/unit/array/mixins/test_plot.py
@@ -27,7 +27,7 @@ from docarray.array.milvus import DocumentArrayMilvus, MilvusConfig
         (DocumentArraySqlite, None),
         (DocumentArrayAnnlite, AnnliteConfig(n_dim=128)),
         # (DocumentArrayWeaviate, WeaviateConfig(n_dim=128)),
-        (DocumentArrayQdrant, QdrantConfig(n_dim=128, scroll_batch_size=8)),
+        # (DocumentArrayQdrant, QdrantConfig(n_dim=128, scroll_batch_size=8)),
         (DocumentArrayElastic, ElasticConfig(n_dim=128)),
         (DocumentArrayRedis, RedisConfig(n_dim=128)),
         # (DocumentArrayMilvus, MilvusConfig(n_dim=128)),  # tensor is too large to handle
