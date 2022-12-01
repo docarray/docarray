32ed58780b650df3d65e710071adf43498669f74
anna-charlotte <charlotte.gerhaher@jina.ai>
2022-12-01 09:59:18 +0100

    fix: only store indices in only keyframes is false
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>

--- docarray/document/mixins/video.py
+++ docarray/document/mixins/video.py
@@ -100,7 +100,7 @@ class VideoDataMixin:
     def load_uri_to_video_tensor(
         self: 'T', only_keyframes: bool = False, **kwargs
     ) -> 'T':
-        """Convert a :attr:`.uri` to a video ndarray :attr:`.tensor` and store the indices of the key frames in :attr:`.tags`.
+        """Convert a :attr:`.uri` to a video ndarray :attr:`.tensor` and store the indices of the key frames in :attr:`.tags` if only_keyframes=False.
 
         :param only_keyframes: only keep the keyframes in the video
         :param kwargs: supports all keyword arguments that are being supported by av.open() as
@@ -118,6 +118,10 @@ class VideoDataMixin:
                 img = frame.to_image()
                 key_frames.append(img)
 
+            if only_keyframes:
+                self.tensor = np.moveaxis(np.stack(key_frames), 1, 2)
+                return self
+
         with av.open(self.uri, **kwargs) as container:
             i = 0
             curr_key_frame = key_frames[i]
@@ -133,13 +137,8 @@ class VideoDataMixin:
                     if i < len(key_frames):
                         curr_key_frame = key_frames[i]
 
+        self.tensor = np.moveaxis(np.stack(frames), 1, 2)
         self.tags['key_frame_indices'] = key_frame_indices
-
-        if only_keyframes:
-            self.tensor = np.moveaxis(np.stack(key_frames), 1, 2)
-        else:
-            self.tensor = np.moveaxis(np.stack(frames), 1, 2)
-
         return self
 
     def save_video_tensor_to_file(
