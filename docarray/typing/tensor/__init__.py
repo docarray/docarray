bbbde9621e0ecce46b373951d34a7b818163f200
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2022-12-07 16:29:34 +0100

    refactor: can import torch tensor if possible
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>

--- docarray/typing/tensor/__init__.py
+++ docarray/typing/tensor/__init__.py
@@ -1,17 +1,20 @@
-from docarray.typing.tensor.embedding import (  # , TorchEmbedding
-    Embedding,
-    NdArrayEmbedding,
-)
+from docarray.typing.tensor.embedding import Embedding, NdArrayEmbedding
 from docarray.typing.tensor.ndarray import NdArray
 from docarray.typing.tensor.tensor import Tensor
 
-# from docarray.typing.tensor.torch_tensor import TorchTensor
+try:
+    import torch  # noqa: F401
+
+    from docarray.typing.tensor.embedding import TorchEmbedding  # noqa: F401
+    from docarray.typing.tensor.torch_tensor import TorchTensor  # noqa: F401
+
+except ImportError:
+    pass
+
 
 __all__ = [
     'NdArray',
-    # 'TorchTensor',
     'Tensor',
     'Embedding',
-    # 'TorchEmbedding',
     'NdArrayEmbedding',
 ]
