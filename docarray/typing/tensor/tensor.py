9643c14eaa7f79d84fed849e28fcfa3c72a3e667
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2022-12-07 16:15:15 +0100

    refactor: first version of not needed torch
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>
    
    refactor: first version of not needed torch
    
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>

--- docarray/typing/tensor/tensor.py
+++ docarray/typing/tensor/tensor.py
@@ -2,12 +2,6 @@ from typing import Union
 
 from docarray.typing.tensor.ndarray import NdArray
 
-try:
-    import torch  # noqa: F401
-except ImportError:
-    Tensor = Union[NdArray]  # type: ignore
+# from docarray.typing.tensor.torch_tensor import TorchTensor
 
-else:
-    from docarray.typing.tensor.torch_tensor import TorchTensor  # noqa: F401
-
-    Tensor = Union[NdArray, TorchTensor]  # type: ignore
+Tensor = Union[NdArray]  # , TorchTensor]
