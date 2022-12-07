af9630688803904ee3f3c44af048480f2c5f4c1c
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2022-12-07 16:41:59 +0100

    refactor: fix mypy partially
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>

--- docarray/typing/__init__.py
+++ docarray/typing/__init__.py
@@ -1,12 +1,17 @@
 from docarray.typing.id import ID
 from docarray.typing.tensor import NdArray, Tensor
 
-# from docarray.typing.tensor import TorchEmbedding, TorchTensor
+try:
+    import torch  # noqa: F401
+
+    from docarray.typing.tensor import TorchEmbedding, TorchTensor  # noqa: F401
+except ImportError:
+    pass
+
 from docarray.typing.tensor.embedding import Embedding
 from docarray.typing.url import AnyUrl, ImageUrl, TextUrl
 
 __all__ = [
-    # 'TorchTensor',
     'NdArray',
     'Embedding',
     'ImageUrl',
@@ -14,7 +19,6 @@ __all__ = [
     'AnyUrl',
     'ID',
     'Tensor',
-    # 'TorchEmbedding',
 ]
 
 try:
