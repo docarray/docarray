976cda172b7c2a0232dd16900fe983a06263c3b4
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2022-12-08 07:07:01 +0100

    fix: fix circular import
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>

--- docarray/typing/tensor/embedding/__init__.py
+++ docarray/typing/tensor/embedding/__init__.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     pass
 else:
-    from docarray.typing.tensor import TorchEmbedding  # type: ignore # noqa: F401
+    from docarray.typing.tensor.embedding.embedding_torch import TorchEmbedding  # type: ignore # noqa: [F401,E510]
 
 
 from docarray.typing.tensor.embedding.embedding import Embedding
