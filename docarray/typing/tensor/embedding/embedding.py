6397f8dc22b009d249102d9f10c94b46ae369c3d
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2022-12-08 11:39:20 +0100

    fix: better support of union of optional torch
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>

--- docarray/typing/tensor/embedding/embedding.py
+++ docarray/typing/tensor/embedding/embedding.py
@@ -1,8 +1,15 @@
-from typing import TYPE_CHECKING, Union
+from typing import Union
 
 from docarray.typing.tensor.embedding.ndarray import NdArrayEmbedding
 
-if TYPE_CHECKING:
-    from docarray.typing.tensor.embedding.torch import TorchEmbedding
+try:
+    import torch  # noqa: F401
+except ImportError:
+    Embedding = Union[NdArrayEmbedding]  # type: ignore
 
-Embedding = Union[NdArrayEmbedding, 'TorchEmbedding']
+else:
+    from docarray.typing.tensor.embedding.torch import TorchEmbedding  # noqa: F401
+
+    Embedding = Union[NdArrayEmbedding, TorchEmbedding]  # type: ignore
+
+__all__ = ['Embedding']
