e1a74b928033a4c3cc964a520a7d7bd0fd799bfb
Michael Guenther <guenthermi50@gmail.com>
2022-12-15 17:36:50 +0100

    refactor: change metrics in tests
    Signed-off-by: Michael Guenther <guenthermi50@gmail.com>

--- tests/unit/array/mixins/oldproto/test_eval_class.py
+++ tests/unit/array/mixins/oldproto/test_eval_class.py
@@ -541,7 +541,6 @@ def test_embed_and_evaluate_single_da(storage, config, start_storage):
                 'reciprocal_rank': 1.0,
                 'recall_at_k': 5.0 / 9.0,
                 'f1_score_at_k': (10.0 / 9.0) / (5.0 / 9.0 + 1),
-                'ndcg_at_k': 1.0,
             },
         ),
         (
@@ -554,7 +553,6 @@ def test_embed_and_evaluate_single_da(storage, config, start_storage):
                 'reciprocal_rank': 1.0,
                 'recall_at_k': 0.5,
                 'f1_score_at_k': 1.0 / 1.5,
-                'ndcg_at_k': 1.0,
             },
         ),
     ],
@@ -622,12 +620,23 @@ def test_embed_and_evaluate_two_different_das():
     )
     res = queries_da.embed_and_evaluate(
         index_data=index_da,
-        metrics=['precision_at_k', 'reciprocal_rank', 'recall_at_k', 'f1_score_at_k'],
+        metrics=[
+            'r_precision',
+            'precision_at_k',
+            'hit_at_k',
+            'average_precision',
+            'reciprocal_rank',
+            'recall_at_k',
+            'f1_score_at_k',
+        ],
         embed_funcs=dummy_embed_function,
         match_batch_size=1,
         limit=10,
     )
+    assert res['r_precision'] == 1.0
     assert res['precision_at_k'] == 1.0
+    assert res['hit_at_k'] == 1.0
+    assert res['average_precision'] == 1.0
     assert res['reciprocal_rank'] == 1.0
     assert res['recall_at_k'] == 0.5
     assert abs(res['f1_score_at_k'] - 1.0 / 1.5) < 1e-5
@@ -713,7 +722,7 @@ def test_embed_and_evaluate_on_real_data(two_embed_funcs, kwargs):
         'reciprocal_rank',
         'recall_at_k',
         'f1_score_at_k',
-        'ndcg_at_k',
+        'dcg_at_k',
     ]
 
     labels = ['18828_alt.atheism', '18828_comp.graphics']
