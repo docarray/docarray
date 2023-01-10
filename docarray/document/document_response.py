5ed0828efe6fe5953b76afb1fc97bde204cf9b27
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2023-01-10 15:20:11 +0100

    fix: starlette not needed
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>

--- docarray/document/document_response.py
+++ docarray/document/document_response.py
@@ -1,4 +1,10 @@
-from starlette.responses import JSONResponse
+try:
+    from starlette.responses import JSONResponse
+except ImportError:
+
+    class JSONResponse:
+        raise ImportError("Starlette is required to use JSONResponse")
+
 
 from docarray import BaseDocument
 from docarray.document.io.json import orjson_dumps
