bc9f9e5c8c4732b28d0e18ad9ec07f16a99be6e1
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2023-01-10 17:28:20 +0100

    merge: merge dev branch
    Signed-off-by: Sami Jaghouar <sami.jaghouar@hotmail.fr>

--- docarray/document/__init__.py
+++
@@ -1,6 +0,0 @@
-from docarray.document.any_document import AnyDocument
-from docarray.document.base_node import BaseNode
-from docarray.document.document import BaseDocument
-from docarray.document.document_response import DocumentResponse
-
-__all__ = ['AnyDocument', 'BaseDocument', 'BaseNode', 'DocumentResponse']
