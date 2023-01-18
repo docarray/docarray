5949e7c9287f5d415bef43f9c7d4e72eab1d99f1
anna-charlotte <charlotte.gerhaher@jina.ai>
2023-01-18 16:44:24 +0100

    fix: wip plot
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>

--- docarray/base_document/mixins/plot.py
+++ docarray/base_document/mixins/plot.py
@@ -1,7 +1,10 @@
+import typing
 from typing import Optional
 
 import numpy as np
 from rich.tree import Tree
+
+import docarray
 
 
 class PlotMixin:
@@ -17,30 +20,35 @@ class PlotMixin:
             tree = Tree(self)
         else:
             tree = tree.add(self)
-        print(f"tree.label = {tree.label}")
-        print(f"tree.label.__class__.__name__ = {tree.label.__class__.__name__}")
-        print(f"type(tree.label) = {type(tree.label)}")
-        # tree.label = 'label'
+        try:
+            iterable_attrs = [
+                x
+                for x in self.__dict__.keys()
+                if (
+                    isinstance(getattr(self, x), typing.List)
+                    or isinstance(getattr(self, x), typing.Tuple)
+                    or isinstance(getattr(self, x), docarray.DocumentArray)
+                )
+            ]
 
-        from collections.abc import Iterable
-
-        iterable_attrs = [x for x in self.__dict__.keys() if isinstance(x, Iterable)]
-        print(f"iterable_attrs = {iterable_attrs}")
-        print(f"self.__dict__.keys() = {self.__dict__.keys()}")
-
-        for attr in iterable_attrs:
-            print(f"attr = {attr}")
-            if getattr(self, attr):
-                _icon = ':diamond_with_a_dot:'
-                _match_tree = tree.add(f'{_icon} [b]{attr.capitalize()}[/b]')
-                for d in getattr(self, attr):
-                    d._plot_recursion(_match_tree)
-
+            for attr in iterable_attrs:
+                value = getattr(self, attr)
+                if value:
+                    _icon = ':diamond_with_a_dot:'
+                    _match_tree = tree.add(
+                        f'{_icon} [b]{attr.capitalize()}: '
+                        f'{value.__class__.__name__}[/b]'
+                    )
+                    for d in value:
+                        self._plot_recursion.__func__(d, _match_tree)
+        except ():
+            pass
         return tree
 
     def __rich_console__(self, console, options):
-
-        yield f":page_facing_up: [b]Document[/b]: [cyan]{self.id}[cyan]"
+        print('in rich console')
+        kls = self.__class__.__name__
+        yield f":page_facing_up: [b]{kls}" f"[/b]: [cyan]{getattr(self, 'id')}[cyan]"
         from collections.abc import Iterable
 
         import torch
@@ -48,43 +56,43 @@ class PlotMixin:
         from rich.table import Table
 
         my_table = Table(
-            'Attribute', 'Value', width=80, box=box.ROUNDED, highlight=True
+            'Attribute', 'Type', 'Value', width=80, box=box.ROUNDED, highlight=True
         )
-        print(f"self.__dict__.keys() = {self.__dict__.keys()}")
-        for f in self.__dict__.keys():
-            print(f"f = {f}")
+        annotations = self.__annotations__
+
+        print(f"annotations = {annotations}")
+        for f, d in self.__dict__.items():
             v = getattr(self, f)
-            print(f"v = {v}")
-            print(f"isinstance(v, str) = {isinstance(v, str)}")
             if f.startswith('_') or f == 'id':
                 continue
             elif isinstance(v, str):
                 v_str = str(v)[:100]
                 if len(v) > 100:
                     v_str += f'... (length: {len(v)})'
-                my_table.add_row(f, text.Text(v_str))
+                my_table.add_row(f, 'string', text.Text(v_str))
             elif v is None:
-                my_table.add_row(f, text.Text('None'))
+                my_table.add_row(
+                    f'{f}: {v.__class__.__name__}',
+                    f'{v.__class__.__name__}',
+                    text.Text('None'),
+                )
             elif isinstance(v, np.ndarray) or isinstance(v, torch.Tensor):
                 x = f'{type(getattr(self, f))} in shape {v.shape}, dtype: {v.dtype}'
-                my_table.add_row(f, x)
+                my_table.add_row(
+                    f'{f}: {v.__class__.__name__}', f'{v.__class__.__name__}', x
+                )
             elif not isinstance(v, Iterable):
-                my_table.add_row(f, text.Text(str(getattr(self, f))))
+                my_table.add_row(
+                    f'{f}: {v.__class__.__name__}',
+                    f'{v.__class__.__name__}',
+                    text.Text(str(getattr(self, f))),
+                )
+            elif isinstance(v, tuple) or isinstance(v, list):
+                my_table.add_row(
+                    f'{f}: {v.__class__.__name__}',
+                    f'{v.__class__.__name__}',
+                    text.Text(str(getattr(self, f))),
+                )
 
-        #     elif f in ('embedding', 'tensor'):
-        #         from docarray.math.ndarray import to_numpy_array
-        #
-        #         v = to_numpy_array(getattr(self, f))
-        #         if v.squeeze().ndim == 1 and len(v) < 1000:
-        #             from docarray.document.mixins.rich_embedding import (
-        #                 ColorBoxEmbedding,
-        #             )
-        #
-        #             v = ColorBoxEmbedding(v.squeeze())
-        #         else:
-        #             v = f'{type(getattr(self, f))} shape {v.shape}, dtype: {v.dtype}'
-        #         my_table.add_row(f, v)
-        #     elif f not in ('id', 'chunks', 'matches'):
-        #         my_table.add_row(f, Text(str(getattr(self, f))))
         if my_table.rows:
             yield my_table
