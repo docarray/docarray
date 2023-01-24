59bd3a61dd17461ffc115ceb5a1117f046aba24a
anna-charlotte <charlotte.gerhaher@jina.ai>
2023-01-24 11:19:54 +0100

    fix: add plotmixin to mixin init
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>

--- docarray/base_document/document.py
+++ docarray/base_document/document.py
@@ -7,8 +7,7 @@ from pydantic import BaseModel, Field, parse_obj_as
 from docarray.base_document.abstract_document import AbstractDocument
 from docarray.base_document.base_node import BaseNode
 from docarray.base_document.io.json import orjson_dumps, orjson_dumps_and_decode
-from docarray.base_document.mixins import ProtoMixin
-from docarray.base_document.mixins.plot import PlotMixin
+from docarray.base_document.mixins import PlotMixin, ProtoMixin
 from docarray.typing import ID
 
 
