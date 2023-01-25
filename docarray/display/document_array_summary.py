b072ff3f7e20888bb423fd104d3d0109ba91dd9d
anna-charlotte <charlotte.gerhaher@jina.ai>
2023-01-25 15:58:52 +0100

    feat: add columns information for stacked array to display
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>

--- docarray/display/document_array_summary.py
+++ docarray/display/document_array_summary.py
@@ -18,10 +18,22 @@ class DocumentArraySummary:
         from rich.panel import Panel
         from rich.table import Table
 
+        from docarray.array import DocumentArrayStacked
+
         table = Table(box=box.SIMPLE, highlight=True)
         table.show_header = False
         table.add_row('Type', self.da.__class__.__name__)
         table.add_row('Length', str(len(self.da)))
+
+        if isinstance(self.da, DocumentArrayStacked):
+            table.add_section()
+            table.add_row('Stacked columns:')
+            for field_name, value in self.da._columns.items():
+                shape = value.get_comp_backend().shape(value)
+                table.add_row(
+                    f'  • {field_name}:',
+                    f'{value.__class__.__name__} of shape {shape}',
+                )
 
         Console().print(Panel(table, title='DocumentArray Summary', expand=False))
         self.da.document_type.schema_summary()
