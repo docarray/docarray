c006fb39c1baa95bb6cb9bc4d1ae328656c2aa3b
Joan Fontanals Martinez <joan.martinez@jina.ai>
2023-01-25 15:53:35 +0100

    feat: add filter capability to DocumentArray
    Signed-off-by: Joan Fontanals Martinez <joan.martinez@jina.ai>

---
+++ docarray/utils/filter.py
@@ -0,0 +1,41 @@
+import json
+
+from typing import NamedTuple, Union, Dict, List
+
+
+from docarray.array.abstract_array import AnyDocumentArray
+from docarray.array.array import DocumentArray
+from docarray.typing import AnyTensor
+
+
+class FilterResult(NamedTuple):
+    documents: DocumentArray
+    scores: AnyTensor
+
+
+def filter(
+    index: AnyDocumentArray,
+    query: Union[str, Dict, List[Dict]],
+) -> AnyDocumentArray:
+    """
+    Filter the Documents in the index according to the given filter query.
+
+
+    EXAMPLE USAGE
+
+    .. code-block:: python
+
+        TODO: fill it
+
+    :param index: the index of Documents to filter in
+    :param query: the query to filter by
+    :return: A DocumentArray containing the Documents inside DocumentArray that fullfil the filter conditions
+    """
+    from docarray.utils.query_parser import QueryParser
+
+    if query:
+        query = query if not isinstance(query, str) else json.loads(query)
+        parser = QueryParser(query)
+        return DocumentArray(d for d in index if parser.evaluate(d))
+    else:
+        return index
