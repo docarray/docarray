c88c87b55a7fca559bef3eb98a37d3fe17c4fbe9
samsja <sami.jaghouar@hotmail.fr>
2023-01-26 14:42:41 +0100

    feat: add validate on Text document
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

---
+++ tests/integrations/predefined_document/test_text.py
@@ -0,0 +1,22 @@
+from pydantic import parse_obj_as
+
+from docarray import BaseDocument
+from docarray.documents import Text
+
+
+def test_simple_init():
+    t = Text(text='hello')
+    t.text == 'hello'
+
+
+def test_str_init():
+    t = parse_obj_as(Text, 'hello')
+    t.text == 'hello'
+
+
+def test_doc():
+    class MyDoc(BaseDocument):
+        text1: Text
+        text2: Text
+
+    MyDoc(text1='hello', text2=Text(text='world'))
