77c6120c0505c2e841718bf234acffd1ffcd2e7d
anna-charlotte <charlotte.gerhaher@jina.ai>
2023-01-26 12:03:05 +0100

    fix: check for nested stacked fields in summary
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>

--- docarray/display/document_array_summary.py
+++ docarray/display/document_array_summary.py
@@ -1,6 +1,9 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
+
+from docarray.typing.tensor.abstract_tensor import AbstractTensor
 
 if TYPE_CHECKING:
+    from docarray.array import DocumentArrayStacked
     from docarray.array.abstract_array import AnyDocumentArray
 
 
@@ -28,12 +31,47 @@ class DocumentArraySummary:
         if isinstance(self.da, DocumentArrayStacked):
             table.add_section()
             table.add_row('Stacked columns:')
-            for field_name, value in self.da._columns.items():
-                shape = value.get_comp_backend().shape(value)
-                table.add_row(
-                    f'  • {field_name}:',
-                    f'{value.__class__.__name__} of shape {shape}',
-                )
+            stacked_fields = self._get_stacked_fields(da=self.da)
+            for field in stacked_fields:
+                da = self.da
+                for attr in field.split('.'):
+                    da = getattr(da, attr)
+
+                if isinstance(da, AbstractTensor):
+                    col_1 = f'  • {field}:'
+                    comp_be = da.get_comp_backend()
+                    cls_name = da.__class__.__name__
+                    if comp_be.isnan(da).all():
+                        col_2 = f'None ({cls_name})'
+                    else:
+                        col_2 = (
+                            f'{cls_name} of shape {comp_be.shape(da)}, '
+                            f'dtype: {comp_be.shape(da)}'
+                        )
+                    table.add_row(col_1, col_2)
 
         Console().print(Panel(table, title='DocumentArray Summary', expand=False))
         self.da.document_type.schema_summary()
+
+    @staticmethod
+    def _get_stacked_fields(da: 'DocumentArrayStacked') -> List[str]:
+        """
+        Returns a list of field names that are stacked of a DocumentArrayStacked
+        instance, i.e. all the fields that are of type AbstractTensor. Nested field
+        paths are dot separated.
+        """
+        from docarray.array import DocumentArrayStacked
+
+        fields = []
+        for field_name, value in da._columns.items():
+            if isinstance(value, AbstractTensor):
+                fields.append(field_name)
+            elif isinstance(value, DocumentArrayStacked):
+                fields.extend(
+                    [
+                        f'{field_name}.{x}'
+                        for x in DocumentArraySummary._get_stacked_fields(da=value)
+                    ]
+                )
+
+        return fields
