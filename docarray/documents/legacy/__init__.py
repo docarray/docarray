035528ed9e46e46ca297147d7f674f1939865422
samsja <sami.jaghouar@hotmail.fr>
2023-02-07 10:45:18 +0100

    fix: add docstring
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- docarray/documents/legacy/__init__.py
+++ docarray/documents/legacy/__init__.py
@@ -1,6 +1,3 @@
-from docarray import DocumentArray as BaseDocumentArray
 from docarray.documents.legacy.legacy_document import Document
 
-DocumentArray = BaseDocumentArray[Document]
-
-__all__ = ['Document', 'DocumentArray']
+__all__ = ['Document']
--- docarray/documents/legacy/legacy_document.py
+++ docarray/documents/legacy/legacy_document.py
@@ -7,6 +7,31 @@ from docarray.typing import AnyEmbedding, AnyTensor
 
 
 class Document(BaseDocument):
+    """
+    This Document is the LegacyDocument. It follows the same schema as in DocArray v1.
+    It can be useful to start migrating a codebase from v1 to v2.
+
+    Nevertheless, the API is not totally compatible with DocAray v1 `Document`.
+    Indeed, none of the method associated with `Document` are present. Only the schema
+    of the data is similar.
+    .. code-block:: python
+
+        from docarray import DocumentArray
+        from docarray.documents.legacy import Document
+        import numpy as np
+
+        doc = Document(text='hello')
+        doc.url = 'http://myimg.png'
+        doc.tensor = np.zeros((3, 224, 224))
+        doc.embedding = np.zeros((100, 1))
+
+        doc.tags['price'] = 10
+
+        doc.chunks = DocumentArray[Document]([Document() for _ in range(10)])
+
+        doc.chunks = DocumentArray[Document]([Document() for _ in range(10)])
+
+    """
 
     tensor: Optional[AnyTensor]
     chunks: Optional[DocumentArray[Document]]
@@ -15,5 +40,5 @@ class Document(BaseDocument):
     text: Optional[str]
     url: Optional[str]
     embedding: Optional[AnyEmbedding]
-    # tags: Dict[str, Any] = dict()
+    tags: Dict[str, Any] = dict()
     scores: Optional[Dict[str, Any]]
