2cbcf72f072dc9445a89388c74d949ae37a15030
samsja <sami.jaghouar@hotmail.fr>
2023-02-10 09:11:27 +0100

    feat: add more test
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- tests/units/document/test_smart_cast.py
+++ tests/units/document/test_smart_cast.py
@@ -1,7 +1,8 @@
 import numpy as np
+import torch
 
 from docarray import BaseDocument
-from docarray.typing import AnyUrl, NdArray
+from docarray.typing import AnyUrl, NdArray, TorchTensor
 
 
 def test_simple_casting():
@@ -22,13 +23,28 @@ def test_simple_casting():
 
 def test_casting_with_key_match():
     class A(BaseDocument):
-        ch: AnyUrl
+        url: AnyUrl
         title: str
 
     class B(BaseDocument):
         url: str
 
     a = A(url='file.png', title='hello')
+
+    b = B.smart_parse_obj(a.dict())
+
+    assert b.url == a.url
+
+
+def test_casting_with_key_match_2():
+    class A(BaseDocument):
+        url: TorchTensor  # mistake done on purpose
+        title: str
+
+    class B(BaseDocument):
+        url: str
+
+    a = A(url=torch.zeros(5), title='hello')
 
     b = B.smart_parse_obj(a.dict())
 
