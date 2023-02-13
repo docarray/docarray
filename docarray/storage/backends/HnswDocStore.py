831d2cea64e62ddfbfd50f205ebe1aad4f1441e3
Johannes Messner <messnerjo@gmail.com>
2023-02-13 17:37:35 +0100

    docs: add sqlite integration
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>

--- docarray/storage/backends/HnswDocStore.py
+++ docarray/storage/backends/HnswDocStore.py
@@ -1,3 +1,7 @@
+import os
+import sqlite3
+from dataclasses import dataclass
+from pathlib import Path
 from typing import Any, Generic, Optional, Sequence, Type, TypeVar, Union
 
 import hnswlib
@@ -5,6 +9,7 @@ import numpy as np
 
 import docarray.typing
 from docarray import BaseDocument, DocumentArray
+from docarray.proto import DocumentProto
 from docarray.storage.abstract_doc_store import BaseDocumentStore, FindResultBatched
 from docarray.typing import AnyTensor
 from docarray.utils.find import FindResult
@@ -18,6 +23,11 @@ if torch_imported:
     import torch
 
     HNSWLIB_PY_VEC_TYPES.append(torch.Tensor)
+
+
+@dataclass
+class HNSWConfig:
+    work_dir: str = '.'
 
 
 class HnswDocumentStore(BaseDocumentStore, Generic[TSchema]):
@@ -34,11 +44,15 @@ class HnswDocumentStore(BaseDocumentStore, Generic[TSchema]):
 
     def __init__(self, config: Optional[IsDataclass] = None):
         super().__init__(config)
+        self._work_dir = config.work_dir
+        Path(self._work_dir).mkdir(parents=True, exist_ok=True)
+
+        self._doc_id_to_hnsw_id = {}
+        self._hnsw_id_to_doc_id = {}
+
+        # HNSWLib setup
         self._index_construct_params = ('space', 'dim')
         self._index_init_params = ('max_elements', 'ef_construction', 'M')
-
-        self._docs_buffer = DocumentArray[self._schema]()
-        self._doc_id_to_hnsw_id = {}
 
         self._hnsw_indices = {}
         for col_name, col in self._columns.items():
@@ -54,6 +68,17 @@ class HnswDocumentStore(BaseDocumentStore, Generic[TSchema]):
             init_params = dict((k, col_config[k]) for k in self._index_init_params)
             index.init_index(**init_params)
             self._hnsw_indices[col_name] = index
+        self._hnsw_locations = {
+            col_name: os.path.join(self._work_dir, f'{col_name}.bin')
+            for col_name in self._hnsw_indices.keys()
+        }
+
+        # SQLite setup
+        self._sqlite_db_path = os.path.join(self._work_dir, 'docs_sqlite.db')
+        self._sqlite_conn = sqlite3.connect(self._sqlite_db_path)
+        self._sqlite_cursor = self._sqlite_conn.cursor()
+        _create_docs_table(self._sqlite_cursor)
+        self._sqlite_conn.commit()
 
     def python_type_to_db_type(self, python_type: Type) -> Any:
         """Map python type to database type."""
@@ -79,19 +104,27 @@ class HnswDocumentStore(BaseDocumentStore, Generic[TSchema]):
     def index(self, docs: Union[TSchema, Sequence[TSchema]]):
         """Index a document into the store"""
         data_by_columns = self.get_data_by_columns(docs)
-        doc_buf_len_before = len(self._docs_buffer)
-        self._docs_buffer.extend(docs)
 
-        hnsw_ids = range(doc_buf_len_before, len(self._docs_buffer))
+        num_docs_before = _get_num_docs_sqlite(self._sqlite_cursor)
+        hnsw_ids = range(num_docs_before, num_docs_before + len(docs))
+        # the dict updates below could maybe be optimized?
         self._doc_id_to_hnsw_id.update(
             (doc_id, hnsw_id) for doc_id, hnsw_id in zip(docs.id, hnsw_ids)
         )
+        self._hnsw_id_to_doc_id.update(
+            (hnsw_id, doc_id) for doc_id, hnsw_id in zip(docs.id, hnsw_ids)
+        )
 
+        # indexing into HNSWLib and SQLite sequentially
+        # could be improved by processing in parallel
         for col_name, index in self._hnsw_indices.items():
             data = data_by_columns[col_name]
             data_np = [self._to_numpy(arr) for arr in data]
             data_stacked = np.stack(data_np)
             index.add_items(data_stacked, ids=hnsw_ids)
+
+        _send_docs_to_sqlite(self._sqlite_cursor, docs)
+        self._sqlite_conn.commit()
 
     def find_batched(
         self,
@@ -111,7 +144,11 @@ class HnswDocumentStore(BaseDocumentStore, Generic[TSchema]):
         index = self._hnsw_indices[embedding_field]
         labels, distances = index.knn_query(query_vec_np, k=limit)
         result_das = [
-            DocumentArray[self._schema]([self._docs_buffer[i] for i in ids_per_query])
+            _get_docs_from_sqlite(
+                self._sqlite_cursor,
+                [self._hnsw_id_to_doc_id[id_] for id_ in ids_per_query],
+                self._schema,
+            )
             for ids_per_query in labels
         ]
         return FindResultBatched(documents=result_das, scores=distances)
@@ -132,9 +169,58 @@ class HnswDocumentStore(BaseDocumentStore, Generic[TSchema]):
                 index.mark_deleted(id_)
             del self._doc_id_to_hnsw_id[k]
 
-        # delete from the buffer
-        # TODO(johannes): this will become delete from SQLite
-        # this is inefficient, but temporary anyways
-        for doc in self._docs_buffer:
-            if doc.id in key:
-                self._docs_buffer.remove(doc)
+        _delete_docs_from_sqlite(self._sqlite_cursor, key)
+        self._sqlite_conn.commit()
+
+
+# serialization helpers
+def _doc_to_bytes(doc: BaseDocument) -> bytes:
+    return doc.to_protobuf().SerializeToString()
+
+
+TSerialize = TypeVar('TSerialize', bound=BaseDocument)
+
+
+def _doc_from_bytes(data: bytes, doc_class: Type[TSerialize]) -> TSerialize:
+    return doc_class.from_protobuf(DocumentProto.FromString(data))
+
+
+# SQLite helpers
+
+
+def _create_docs_table(cursor: sqlite3.Cursor):
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS docs (doc_id TEXT PRIMARY KEY, data BLOB)'
+    )
+
+
+def _send_docs_to_sqlite(cursor: sqlite3.Cursor, docs: Sequence[BaseDocument]):
+    cursor.executemany(
+        'INSERT INTO docs VALUES (?, ?)',
+        ((doc.id, _doc_to_bytes(doc)) for doc in docs),
+    )
+
+
+def _get_docs_from_sqlite(
+    cursor: sqlite3.Cursor, doc_ids: Sequence[str], doc_class: Type[TSerialize]
+) -> DocumentArray:
+    cursor.execute(
+        'SELECT data FROM docs WHERE doc_id IN (%s)' % ','.join('?' * len(doc_ids)),
+        doc_ids,
+    )
+    rows = cursor.fetchall()
+    return DocumentArray[doc_class](
+        (_doc_from_bytes(row[0], doc_class) for row in rows)
+    )
+
+
+def _delete_docs_from_sqlite(cursor: sqlite3.Cursor, doc_ids: Sequence[str]):
+    cursor.execute(
+        'DELETE FROM docs WHERE doc_id IN (%s)' % ','.join('?' * len(doc_ids)),
+        doc_ids,
+    )
+
+
+def _get_num_docs_sqlite(cursor: sqlite3.Cursor) -> int:
+    cursor.execute('SELECT COUNT(*) FROM docs')
+    return cursor.fetchone()[0]
