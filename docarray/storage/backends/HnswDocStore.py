d59eae8b9e15d6eae6f5cfb81fe331f925b2fbb1
Johannes Messner <messnerjo@gmail.com>
2023-02-14 15:12:16 +0100

    reafctor: use universal id for sqlite and hnswlib
    Signed-off-by: Johannes Messner <messnerjo@gmail.com>

--- docarray/storage/backends/HnswDocStore.py
+++ docarray/storage/backends/HnswDocStore.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 import sqlite3
 from dataclasses import dataclass
@@ -52,9 +53,6 @@ class HnswDocumentStore(BaseDocumentStore, Generic[TSchema]):
         load_existing = os.path.exists(self._work_dir)
         Path(self._work_dir).mkdir(parents=True, exist_ok=True)
 
-        self._doc_id_to_hnsw_id = {}
-        self._hnsw_id_to_doc_id = {}
-
         # HNSWLib setup
         self._index_construct_params = ('space', 'dim')
         self._index_init_params = ('max_elements', 'ef_construction', 'M')
@@ -77,8 +75,14 @@ class HnswDocumentStore(BaseDocumentStore, Generic[TSchema]):
         self._sqlite_db_path = os.path.join(self._work_dir, 'docs_sqlite.db')
         self._sqlite_conn = sqlite3.connect(self._sqlite_db_path)
         self._sqlite_cursor = self._sqlite_conn.cursor()
-        _create_docs_table(self._sqlite_cursor)
+        self._create_docs_table()
         self._sqlite_conn.commit()
+
+    @staticmethod
+    def _to_universal_id(doc_id: str) -> int:
+        # https://stackoverflow.com/questions/16008670/how-to-hash-a-string-into-8-digits
+        # hashing to 18 digits avoids overflow of sqlite INTEGER
+        return int(hashlib.sha256(doc_id.encode('utf-8')).hexdigest(), 16) % 10**18
 
     def _create_index_class(self, col: '_Column') -> hnswlib.Index:
         '''Create an instance of hnswlib.Index without initializing it.'''
@@ -126,16 +130,7 @@ class HnswDocumentStore(BaseDocumentStore, Generic[TSchema]):
     def index(self, docs: Union[TSchema, Sequence[TSchema]]):
         """Index a document into the store"""
         data_by_columns = self.get_data_by_columns(docs)
-
-        num_docs_before = _get_num_docs_sqlite(self._sqlite_cursor)
-        hnsw_ids = range(num_docs_before, num_docs_before + len(docs))
-        # the dict updates below could maybe be optimized?
-        self._doc_id_to_hnsw_id.update(
-            (doc_id, hnsw_id) for doc_id, hnsw_id in zip(docs.id, hnsw_ids)
-        )
-        self._hnsw_id_to_doc_id.update(
-            (hnsw_id, doc_id) for doc_id, hnsw_id in zip(docs.id, hnsw_ids)
-        )
+        hnsw_ids = tuple(self._to_universal_id(doc.id) for doc in docs)
 
         # indexing into HNSWLib and SQLite sequentially
         # could be improved by processing in parallel
@@ -146,7 +141,7 @@ class HnswDocumentStore(BaseDocumentStore, Generic[TSchema]):
             index.add_items(data_stacked, ids=hnsw_ids)
             index.save_index(self._hnsw_locations[col_name])
 
-        _send_docs_to_sqlite(self._sqlite_cursor, docs)
+        self._send_docs_to_sqlite(docs)
         self._sqlite_conn.commit()
 
     def find_batched(
@@ -167,10 +162,8 @@ class HnswDocumentStore(BaseDocumentStore, Generic[TSchema]):
         index = self._hnsw_indices[embedding_field]
         labels, distances = index.knn_query(query_vec_np, k=limit)
         result_das = [
-            _get_docs_from_sqlite(
-                self._sqlite_cursor,
-                [self._hnsw_id_to_doc_id[id_] for id_ in ids_per_query],
-                self._schema,
+            self._get_docs_from_sqlite(
+                ids_per_query,
             )
             for ids_per_query in labels
         ]
@@ -186,64 +179,60 @@ class HnswDocumentStore(BaseDocumentStore, Generic[TSchema]):
         # delete from the indices
         if isinstance(key, str):
             key = [key]
-        for k in key:
-            id_ = self._doc_id_to_hnsw_id[k]
+        for doc_id in key:
+            id_ = self._to_universal_id(doc_id)
             for col_name, index in self._hnsw_indices.items():
                 index.mark_deleted(id_)
-            del self._doc_id_to_hnsw_id[k]
 
-        _delete_docs_from_sqlite(self._sqlite_cursor, key)
+        self._delete_docs_from_sqlite(key)
         self._sqlite_conn.commit()
 
+    # SQLite helpers
+    def _create_docs_table(self):
+        self._sqlite_cursor.execute(
+            'CREATE TABLE IF NOT EXISTS docs (doc_id INTEGER PRIMARY KEY, data BLOB)'
+        )
 
-# serialization helpers
-def _doc_to_bytes(doc: BaseDocument) -> bytes:
-    return doc.to_protobuf().SerializeToString()
+    def _send_docs_to_sqlite(self, docs: Sequence[BaseDocument]):
+        ids = (self._to_universal_id(doc.id) for doc in docs)
+        self._sqlite_cursor.executemany(
+            'INSERT INTO docs VALUES (?, ?)',
+            ((id_, self._doc_to_bytes(doc)) for id_, doc in zip(ids, docs)),
+        )
 
+    def _get_docs_from_sqlite(
+        self, doc_ids: Sequence[Union[str, int]]
+    ) -> DocumentArray:
+        ids = tuple(
+            self._to_universal_id(id_) if isinstance(id_, str) else id_
+            for id_ in doc_ids
+        )
+        self._sqlite_cursor.execute(
+            'SELECT data FROM docs WHERE doc_id IN (%s)' % ','.join('?' * len(ids)),
+            ids,
+        )
+        rows = self._sqlite_cursor.fetchall()
+        return DocumentArray[self._schema](
+            (self._doc_from_bytes(row[0]) for row in rows)
+        )
 
-TSerialize = TypeVar('TSerialize', bound=BaseDocument)
+    def _delete_docs_from_sqlite(self, doc_ids: Sequence[Union[str, int]]):
+        ids = tuple(
+            self._to_universal_id(id_) if isinstance(id_, str) else id_
+            for id_ in doc_ids
+        )
+        self._sqlite_cursor.execute(
+            'DELETE FROM docs WHERE doc_id IN (%s)' % ','.join('?' * len(ids)),
+            ids,
+        )
 
+    def _get_num_docs_sqlite(self) -> int:
+        self._sqlite_cursor.execute('SELECT COUNT(*) FROM docs')
+        return self._sqlite_cursor.fetchone()[0]
 
-def _doc_from_bytes(data: bytes, doc_class: Type[TSerialize]) -> TSerialize:
-    return doc_class.from_protobuf(DocumentProto.FromString(data))
+    # serialization helpers
+    def _doc_to_bytes(self, doc: BaseDocument) -> bytes:
+        return doc.to_protobuf().SerializeToString()
 
-
-# SQLite helpers
-
-
-def _create_docs_table(cursor: sqlite3.Cursor):
-    cursor.execute(
-        'CREATE TABLE IF NOT EXISTS docs (doc_id TEXT PRIMARY KEY, data BLOB)'
-    )
-
-
-def _send_docs_to_sqlite(cursor: sqlite3.Cursor, docs: Sequence[BaseDocument]):
-    cursor.executemany(
-        'INSERT INTO docs VALUES (?, ?)',
-        ((doc.id, _doc_to_bytes(doc)) for doc in docs),
-    )
-
-
-def _get_docs_from_sqlite(
-    cursor: sqlite3.Cursor, doc_ids: Sequence[str], doc_class: Type[TSerialize]
-) -> DocumentArray:
-    cursor.execute(
-        'SELECT data FROM docs WHERE doc_id IN (%s)' % ','.join('?' * len(doc_ids)),
-        doc_ids,
-    )
-    rows = cursor.fetchall()
-    return DocumentArray[doc_class](
-        (_doc_from_bytes(row[0], doc_class) for row in rows)
-    )
-
-
-def _delete_docs_from_sqlite(cursor: sqlite3.Cursor, doc_ids: Sequence[str]):
-    cursor.execute(
-        'DELETE FROM docs WHERE doc_id IN (%s)' % ','.join('?' * len(doc_ids)),
-        doc_ids,
-    )
-
-
-def _get_num_docs_sqlite(cursor: sqlite3.Cursor) -> int:
-    cursor.execute('SELECT COUNT(*) FROM docs')
-    return cursor.fetchone()[0]
+    def _doc_from_bytes(self, data: bytes) -> BaseDocument:
+        return self._schema.from_protobuf(DocumentProto.FromString(data))
