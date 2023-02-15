a7b512ea1d182a4adbfc477027f54ae5eabe5d6b
samsja <sami.jaghouar@hotmail.fr>
2023-02-15 17:18:26 +0100

    refactor: add update mixin
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- docarray/base_document/mixins/__init__.py
+++ docarray/base_document/mixins/__init__.py
@@ -1,3 +1,4 @@
 from docarray.base_document.mixins.proto import ProtoMixin
+from docarray.base_document.mixins.update import UpdateMixin
 
-__all__ = ['ProtoMixin']
+__all__ = ['ProtoMixin', 'UpdateMixin']
