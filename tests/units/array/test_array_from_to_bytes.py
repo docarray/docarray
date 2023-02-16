38c19e01dcb191f262bccbaa48acb3671d4d449c
Joan Fontanals <joan.martinez@jina.ai>
2023-02-16 14:44:20 +0100

    feat: port to_from_* and load/save_binary (#1132)
    * feat: first approach load/save binary
    
    Signed-off-by: Joan Fontanals Martinez <joan.martinez@jina.ai>
    
    * feat: add to/from json
    
    Signed-off-by: Joan Fontanals Martinez <joan.martinez@jina.ai>
    
    * test: add test array to-from-json
    
    Signed-off-by: Joan Fontanals Martinez <joan.martinez@jina.ai>
    
    * fix: fix some mypys
    
    Signed-off-by: Joan Fontanals Martinez <joan.martinez@jina.ai>
    
    * fix: small changes
    
    Signed-off-by: Joan Fontanals Martinez <joan.martinez@jina.ai>
    
    * test: add show progress test
    
    Signed-off-by: Joan Fontanals Martinez <joan.martinez@jina.ai>
    
    * fix: fix poetry lock
    
    Signed-off-by: Joan Fontanals Martinez <joan.martinez@jina.ai>
    
    * ci: fix black
    
    * Revert "ci: fix black"
    
    This reverts commit 54daab69b18d45afdd8661a03787d7f5a4881f26.
    
    * ci: fix poetry
    
    Signed-off-by: Joan Fontanals Martinez <joan.martinez@jina.ai>
    
    * fix: update pyproject.toml
    
    Co-authored-by: samsja <55492238+samsja@users.noreply.github.com>
    Signed-off-by: Joan Fontanals <jfontanalsmartinez@gmail.com>
    Signed-off-by: Joan Fontanals Martinez <joan.martinez@jina.ai>
    
    * docs: fix docstrings
    
    Signed-off-by: Joan Fontanals Martinez <joan.martinez@jina.ai>
    
    ---------
    
    Signed-off-by: Joan Fontanals Martinez <joan.martinez@jina.ai>
    Signed-off-by: Joan Fontanals <joan.martinez@jina.ai>
    Signed-off-by: Joan Fontanals <jfontanalsmartinez@gmail.com>
    Co-authored-by: samsja <55492238+samsja@users.noreply.github.com>

---
+++ tests/units/array/test_array_from_to_bytes.py
@@ -0,0 +1,68 @@
+import pytest
+
+from docarray import BaseDocument
+from docarray.typing import NdArray
+from docarray.documents import Image
+from docarray import DocumentArray
+
+
+class MyDoc(BaseDocument):
+    embedding: NdArray
+    text: str
+    image: Image
+
+
+@pytest.mark.parametrize(
+    'protocol', ['pickle-array', 'protobuf-array', 'protobuf', 'pickle']
+)
+@pytest.mark.parametrize('compress', ['lz4', 'bz2', 'lzma', 'zlib', 'gzip', None])
+@pytest.mark.parametrize('show_progress', [False, True])
+def test_from_to_bytes(protocol, compress, show_progress):
+    da = DocumentArray[MyDoc](
+        [
+            MyDoc(embedding=[1, 2, 3, 4, 5], text='hello', image=Image(url='aux.png')),
+            MyDoc(embedding=[5, 4, 3, 2, 1], text='hello world', image=Image()),
+        ]
+    )
+    bytes_da = da.to_bytes(
+        protocol=protocol, compress=compress, show_progress=show_progress
+    )
+    da2 = DocumentArray[MyDoc].from_bytes(
+        bytes_da, protocol=protocol, compress=compress, show_progress=show_progress
+    )
+    assert len(da2) == 2
+    assert len(da) == len(da2)
+    for d1, d2 in zip(da, da2):
+        assert d1.embedding.tolist() == d2.embedding.tolist()
+        assert d1.text == d2.text
+        assert d1.image.url == d2.image.url
+    assert da[1].image.url is None
+    assert da2[1].image.url is None
+
+
+@pytest.mark.parametrize(
+    'protocol', ['pickle-array', 'protobuf-array', 'protobuf', 'pickle']
+)
+@pytest.mark.parametrize('compress', ['lz4', 'bz2', 'lzma', 'zlib', 'gzip', None])
+@pytest.mark.parametrize('show_progress', [False, True])
+def test_from_to_base64(protocol, compress, show_progress):
+    da = DocumentArray[MyDoc](
+        [
+            MyDoc(embedding=[1, 2, 3, 4, 5], text='hello', image=Image(url='aux.png')),
+            MyDoc(embedding=[5, 4, 3, 2, 1], text='hello world', image=Image()),
+        ]
+    )
+    bytes_da = da.to_base64(
+        protocol=protocol, compress=compress, show_progress=show_progress
+    )
+    da2 = DocumentArray[MyDoc].from_base64(
+        bytes_da, protocol=protocol, compress=compress, show_progress=show_progress
+    )
+    assert len(da2) == 2
+    assert len(da) == len(da2)
+    for d1, d2 in zip(da, da2):
+        assert d1.embedding.tolist() == d2.embedding.tolist()
+        assert d1.text == d2.text
+        assert d1.image.url == d2.image.url
+    assert da[1].image.url is None
+    assert da2[1].image.url is None
