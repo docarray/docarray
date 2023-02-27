f49f5186c721775ca5efc320398028d159b5c4ae
AnneY <evangeline-lun@foxmail.com>
2023-02-27 15:27:57 +0800

    test: add test for create_from_dict
    Signed-off-by: AnneY <evangeline-lun@foxmail.com>

--- tests/integrations/document/test_document.py
+++ tests/integrations/document/test_document.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from docarray import BaseDocument, DocumentArray
 from docarray.documents import Image, Text
+from docarray.documents.helper import create_from_dict
 
 
 def test_multi_modal_doc():
@@ -32,3 +33,22 @@ def test_nested_chunks_document():
     )
 
     assert isinstance(doc.images, DocumentArray)
+
+
+def test_create_from_doct():
+    MyMultiModalDoc = create_from_dict(
+        'MyMultiModalDoc', image=(Image, ...), text=(Text, ...)
+    )
+
+    assert issubclass(MyMultiModalDoc, BaseDocument)
+
+    doc = MyMultiModalDoc(
+        image=Image(tensor=np.zeros((3, 224, 224))), text=Text(text='hello')
+    )
+
+    assert isinstance(doc.image, BaseDocument)
+    assert isinstance(doc.image, Image)
+    assert isinstance(doc.text, Text)
+
+    assert doc.text.text == 'hello'
+    assert (doc.image.tensor == np.zeros((3, 224, 224))).all()
