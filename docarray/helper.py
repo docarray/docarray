28b96fe81e691363e935f332cf3441ee2f0cbf13
Charlotte Gerhaher <charlotte.gerhaher@jina.ai>
2023-02-27 17:35:00 +0100

    feat: nested attribute access in `find()` (#1176)
    * test: add test for find on nested attribute
    
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>
    
    * test: add test for nested attr in stacked da
    
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>
    
    * fix: nested access
    
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>
    
    * fix: mypy
    
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>
    
    * fix: field type by access path for da
    
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>
    
    * fix: clean up
    
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>
    
    * fix: move get field type by access path
    
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>
    
    * fix: imports
    
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>
    
    * fix: apply suggestion and fix import
    
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>
    
    * fix: apply suggestion from code review
    
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>
    
    * fix: apply suggestion from code review
    
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>
    
    * fix: apply suggestions
    
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>
    
    ---------
    
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>

--- docarray/helper.py
+++ docarray/helper.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Any, Dict, List, Type
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Type
 
 if TYPE_CHECKING:
     from docarray import BaseDocument
@@ -8,21 +8,9 @@ def _is_access_path_valid(doc_type: Type['BaseDocument'], access_path: str) -> b
     """
     Check if a given access path ("__"-separated) is a valid path for a given Document class.
     """
-    from docarray import BaseDocument
 
-    field, _, remaining = access_path.partition('__')
-    if len(remaining) == 0:
-        return access_path in doc_type.__fields__.keys()
-    else:
-        valid_field = field in doc_type.__fields__.keys()
-        if not valid_field:
-            return False
-        else:
-            d = doc_type._get_field_type(field)
-            if not issubclass(d, BaseDocument):
-                return False
-            else:
-                return _is_access_path_valid(d, remaining)
+    field_type = _get_field_type_by_access_path(doc_type, access_path)
+    return field_type is not None
 
 
 def _all_access_paths_valid(
@@ -121,3 +109,32 @@ def _update_nested_dicts(
             to_update[k] = v
         else:
             _update_nested_dicts(to_update[k], update_with[k])
+
+
+def _get_field_type_by_access_path(
+    doc_type: Type['BaseDocument'], access_path: str
+) -> Optional[Type]:
+    """
+    Get field type by "__"-separated access path.
+    :param doc_type: type of document
+    :param access_path: "__"-separated access path
+    :return: field type of accessed attribute. If access path is invalid, return None.
+    """
+    from docarray import BaseDocument, DocumentArray
+
+    field, _, remaining = access_path.partition('__')
+    field_valid = field in doc_type.__fields__.keys()
+
+    if field_valid:
+        if len(remaining) == 0:
+            return doc_type._get_field_type(field)
+        else:
+            d = doc_type._get_field_type(field)
+            if issubclass(d, DocumentArray):
+                return _get_field_type_by_access_path(d.document_type, remaining)
+            elif issubclass(d, BaseDocument):
+                return _get_field_type_by_access_path(d, remaining)
+            else:
+                return None
+    else:
+        return None
