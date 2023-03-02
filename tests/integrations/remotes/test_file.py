3fa8d2ccd5291cab425c6abf0f00f480b348fb23
Jackmin801 <56836461+Jackmin801@users.noreply.github.com>
2023-03-02 21:18:14 +0800

    test: add tests for file backend
    Signed-off-by: Jackmin801 <56836461+Jackmin801@users.noreply.github.com>

---
+++ tests/integrations/remotes/test_file.py
@@ -0,0 +1,180 @@
+from pathlib import Path
+
+import pytest
+
+from docarray import DocumentArray
+from docarray.array.array.pushpull.file import PushPullFile, __cache_path__
+from docarray.documents import Text
+from tests.integrations.remotes import gen_text_docs, get_test_da, profile_memory
+
+DA_LEN: int = 2**10
+TOLERANCE_BYTES = 2**10
+
+
+def test_path_resolution():
+    assert PushPullFile._abs_filepath('meow') == __cache_path__ / 'meow'
+    assert PushPullFile._abs_filepath('/meow') == Path('/meow')
+    assert PushPullFile._abs_filepath('~/meow') == Path.home() / 'meow'
+    assert PushPullFile._abs_filepath('./meow') == Path.cwd() / 'meow'
+    assert PushPullFile._abs_filepath('../meow') == Path.cwd().parent / 'meow'
+
+
+def test_pushpull_correct(capsys, tmp_path: Path):
+    tmp_path.mkdir(parents=True, exist_ok=True)
+    namespace_dir = tmp_path
+    da1 = get_test_da(DA_LEN)
+
+    # Verbose
+    da1.push(f'file://{namespace_dir}/meow', show_progress=True)
+    da2 = DocumentArray[Text].pull(f'file://{namespace_dir}/meow', show_progress=True)
+    assert len(da1) == len(da2)
+    assert all(d1.id == d2.id for d1, d2 in zip(da1, da2))
+    assert all(d1.text == d2.text for d1, d2 in zip(da1, da2))
+
+    captured = capsys.readouterr()
+    assert len(captured.out) > 0
+    assert len(captured.err) == 0
+
+    # Quiet
+    da2.push(f'file://{namespace_dir}/meow')
+    da1 = DocumentArray[Text].pull(f'file://{namespace_dir}/meow')
+    assert len(da1) == len(da2)
+    assert all(d1.id == d2.id for d1, d2 in zip(da1, da2))
+    assert all(d1.text == d2.text for d1, d2 in zip(da1, da2))
+
+    captured = capsys.readouterr()
+    assert len(captured.out) == 0
+    assert len(captured.err) == 0
+
+
+def test_pushpull_stream_correct(capsys, tmp_path: Path):
+    tmp_path.mkdir(parents=True, exist_ok=True)
+    namespace_dir = tmp_path
+    da1 = get_test_da(DA_LEN)
+
+    # Verbosity and correctness
+    DocumentArray[Text].push_stream(
+        iter(da1), f'file://{namespace_dir}/meow', show_progress=True
+    )
+    doc_stream2 = DocumentArray[Text].pull_stream(
+        f'file://{namespace_dir}/meow', show_progress=True
+    )
+
+    assert all(d1.id == d2.id for d1, d2 in zip(da1, doc_stream2))
+    with pytest.raises(StopIteration):
+        next(doc_stream2)
+
+    captured = capsys.readouterr()
+    assert len(captured.out) > 0
+    assert len(captured.err) == 0
+
+    # Quiet and chained
+    doc_stream = DocumentArray[Text].pull_stream(
+        f'file://{namespace_dir}/meow', show_progress=False
+    )
+    DocumentArray[Text].push_stream(
+        doc_stream, f'file://{namespace_dir}/meow2', show_progress=False
+    )
+
+    captured = capsys.readouterr()
+    assert len(captured.out) == 0
+    assert len(captured.err) == 0
+
+
+def test_pushpull_stream_vs_full(tmp_path: Path):
+    tmp_path.mkdir(parents=True, exist_ok=True)
+    namespace_dir = tmp_path
+    DocumentArray[Text].push_stream(
+        gen_text_docs(DA_LEN * 1),
+        f'file://{namespace_dir}/meow-short',
+        show_progress=False,
+    )
+    DocumentArray[Text].push_stream(
+        gen_text_docs(DA_LEN * 4),
+        f'file://{namespace_dir}/meow-long',
+        show_progress=False,
+    )
+
+    @profile_memory
+    def get_total_stream(url: str):
+        return sum(
+            len(d.text)
+            for d in DocumentArray[Text].pull_stream(url, show_progress=False)
+        )
+
+    @profile_memory
+    def get_total_full(url: str):
+        return sum(
+            len(d.text) for d in DocumentArray[Text].pull(url, show_progress=False)
+        )
+
+    # A warmup is needed to get accurate memory usage comparison
+    _ = get_total_stream(f'file://{namespace_dir}/meow-short')
+    short_total_stream, (_, short_stream_peak) = get_total_stream(
+        f'file://{namespace_dir}/meow-short'
+    )
+    long_total_stream, (_, long_stream_peak) = get_total_stream(
+        f'file://{namespace_dir}/meow-long'
+    )
+
+    _ = get_total_full(f'file://{namespace_dir}/meow-short')
+    short_total_full, (_, short_full_peak) = get_total_full(
+        f'file://{namespace_dir}/meow-short'
+    )
+    long_total_full, (_, long_full_peak) = get_total_full(
+        f'file://{namespace_dir}/meow-long'
+    )
+
+    assert (
+        short_total_stream == short_total_full
+    ), 'Streamed and non-streamed pull should have similar statistics'
+    assert (
+        long_total_stream == long_total_full
+    ), 'Streamed and non-streamed pull should have similar statistics'
+
+    assert (
+        abs(long_stream_peak - short_stream_peak) < TOLERANCE_BYTES
+    ), 'Streamed memory usage should not be dependent on the size of the data'
+    assert (
+        abs(long_full_peak - short_full_peak) > TOLERANCE_BYTES
+    ), 'Full pull memory usage should be dependent on the size of the data'
+
+
+def test_list_and_delete(capsys, tmp_path: Path):
+    tmp_path.mkdir(parents=True, exist_ok=True)
+    namespace_dir = tmp_path
+
+    da_names = DocumentArray.list(f'file://{namespace_dir}', show_table=False)
+    assert len(da_names) == 0
+
+    DocumentArray[Text].push_stream(
+        gen_text_docs(DA_LEN), f'file://{namespace_dir}/meow', show_progress=False
+    )
+    da_names = DocumentArray.list(f'file://{namespace_dir}', show_table=False)
+    assert set(da_names) == {'meow'}
+    DocumentArray[Text].push_stream(
+        gen_text_docs(DA_LEN), f'file://{namespace_dir}/woof', show_progress=False
+    )
+    da_names = DocumentArray.list(f'file://{namespace_dir}', show_table=False)
+    assert set(da_names) == {'meow', 'woof'}
+
+    assert DocumentArray.delete(
+        f'file://{namespace_dir}/meow'
+    ), 'Deleting an existing DA should return True'
+    da_names = DocumentArray.list(f'file://{namespace_dir}', show_table=False)
+    assert set(da_names) == {'woof'}
+
+    with pytest.raises(
+        FileNotFoundError
+    ):  # Deleting a non-existent DA without safety should raise an error
+        DocumentArray.delete(f'file://{namespace_dir}/meow')
+    assert not DocumentArray.delete(
+        f'file://{namespace_dir}/meow', missing_ok=True
+    ), 'Deleting a non-existent DA should return False'
+
+
+@pytest.mark.skip('TODO: Implement concurrent push lock')
+def test_concurrent_push():
+    # Push to DA that is being pulled should not mess up the pull
+    # Double push should fail the second push
+    pass
