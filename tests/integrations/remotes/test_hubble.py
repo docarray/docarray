42079ad0c2ac7d40af832ec459b2ebb0839e20f2
Jackmin801 <56836461+Jackmin801@users.noreply.github.com>
2023-03-10 13:52:22 +0800

    test: update hubble tests (failing)
    Signed-off-by: Jackmin801 <56836461+Jackmin801@users.noreply.github.com>

--- tests/integrations/remotes/test_hubble.py
+++ tests/integrations/remotes/test_hubble.py
@@ -1,33 +1,244 @@
+import multiprocessing as mp
+import uuid
+
+import hubble
 import pytest
 
 from docarray import DocumentArray
 from docarray.documents import TextDoc
+from tests.integrations.remotes import gen_text_docs, get_test_da, profile_memory
+
+DA_LEN: int = 2**10
+TOLERANCE_RATIO = 0.1  # Percentage of difference allowed in stream vs non-stream test
+RANDOM: str = uuid.uuid4().hex[:8]
 
 
-def get_test_da(n: int):
-    return DocumentArray[TextDoc](TextDoc(text=f'text {i}') for i in range(n))
+@pytest.fixture(scope='session', autouse=True)
+def testing_namespace_cleanup():
+    yield
+    da_names = list(
+        filter(
+            lambda x: x.startswith(f'test{RANDOM}'),
+            DocumentArray.list('jinaai://', show_table=False),
+        )
+    )
+    for da_name in da_names:
+        DocumentArray.delete(f'jinaai://{da_name}')
 
 
-@pytest.mark.skip('Test env has no jina api key?')
-def test_pushpull():
-    da1 = get_test_da(2**8)
-    da1.push('jinaai://test-da', public=True, show_progress=True)
-    da2 = DocumentArray[TextDoc].pull('jinaai://test-da', show_progress=True)
+def test_pushpull_correct(capsys):
+    DA_NAME: str = f'test{RANDOM}-pushpull-correct'
+    da1 = get_test_da(DA_LEN)
+
+    # Verbose
+    da1.push(f'jinaai://{DA_NAME}', show_progress=True)
+    da2 = DocumentArray[TextDoc].pull(f'jinaai://{DA_NAME}', show_progress=True)
     assert len(da1) == len(da2)
+    assert all(d1.id == d2.id for d1, d2 in zip(da1, da2))
     assert all(d1.text == d2.text for d1, d2 in zip(da1, da2))
+
+    captured = capsys.readouterr()
+    assert len(captured.out) > 0
+    assert len(captured.err) == 0
+
+    # Quiet
+    da2.push(f'jinaai://{DA_NAME}')
+    da1 = DocumentArray[TextDoc].pull(f'jinaai://{DA_NAME}')
+    assert len(da1) == len(da2)
     assert all(d1.id == d2.id for d1, d2 in zip(da1, da2))
+    assert all(d1.text == d2.text for d1, d2 in zip(da1, da2))
+
+    captured = capsys.readouterr()
+    assert (
+        len(captured.out) == 0
+    ), 'No output should be printed when show_progress=False'
+    assert len(captured.err) == 0, 'No error should be printed when show_progress=False'
 
 
-@pytest.mark.skip('Not implemented yet')
-def test_pushpull_stream():
-    da1 = get_test_da(2**8)
+def test_pushpull_stream_correct(capsys):
+    DA_NAME_1: str = f'test{RANDOM}-pushpull-stream-correct-da1'
+    DA_NAME_2: str = f'test{RANDOM}-pushpull-stream-correct-da2'
+
+    da1 = get_test_da(DA_LEN)
+
+    # Verbosity and correctness
     DocumentArray[TextDoc].push_stream(
-        iter(da1), 'jinaai://test-da-stream', public=True, show_progress=True
+        iter(da1), f'jinaai://{DA_NAME_1}', show_progress=True
     )
-    da2 = DocumentArray[TextDoc].pull_stream(
-        'jinaai://test-da-stream', show_progress=True
+    doc_stream2 = DocumentArray[TextDoc].pull_stream(
+        f'jinaai://{DA_NAME_1}', show_progress=True
     )
 
-    assert all(d1.id == d2.id for d1, d2 in zip(da1, da2))
+    assert all(d1.id == d2.id for d1, d2 in zip(da1, doc_stream2))
     with pytest.raises(StopIteration):
-        next(da2)
+        next(doc_stream2)
+
+    captured = capsys.readouterr()
+    assert len(captured.out) > 0
+    assert len(captured.err) == 0
+
+    # Quiet and chained
+    doc_stream = DocumentArray[TextDoc].pull_stream(
+        f'jinaai://{DA_NAME_1}', show_progress=False
+    )
+    DocumentArray[TextDoc].push_stream(
+        doc_stream, f'jinaai://{DA_NAME_2}', show_progress=False
+    )
+
+    captured = capsys.readouterr()
+    assert (
+        len(captured.out) == 0
+    ), 'No output should be printed when show_progress=False'
+    assert len(captured.err) == 0, 'No error should be printed when show_progress=False'
+
+
+def test_pull_stream_vs_pull_full():
+    DA_NAME_SHORT: str = f'test{RANDOM}-pull-stream-vs-pull-full-short'
+    DA_NAME_LONG: str = f'test{RANDOM}-pull-stream-vs-pull-full-long'
+
+    DocumentArray[TextDoc].push_stream(
+        gen_text_docs(DA_LEN * 1),
+        f'jinaai://{DA_NAME_SHORT}',
+        show_progress=False,
+    )
+    DocumentArray[TextDoc].push_stream(
+        gen_text_docs(DA_LEN * 4),
+        f'jinaai://{DA_NAME_LONG}',
+        show_progress=False,
+    )
+
+    @profile_memory
+    def get_total_stream(url: str):
+        return sum(
+            len(d.text)
+            for d in DocumentArray[TextDoc].pull_stream(url, show_progress=False)
+        )
+
+    @profile_memory
+    def get_total_full(url: str):
+        return sum(
+            len(d.text) for d in DocumentArray[TextDoc].pull(url, show_progress=False)
+        )
+
+    # A warmup is needed to get accurate memory usage comparison
+    _ = get_total_stream(f'jinaai://{DA_NAME_SHORT}')
+    short_total_stream, (_, short_stream_peak) = get_total_stream(
+        f'jinaai://{DA_NAME_SHORT}'
+    )
+    long_total_stream, (_, long_stream_peak) = get_total_stream(
+        f'jinaai://{DA_NAME_LONG}'
+    )
+
+    _ = get_total_full(f'jinaai://{DA_NAME_SHORT}')
+    short_total_full, (_, short_full_peak) = get_total_full(f'jinaai://{DA_NAME_SHORT}')
+    long_total_full, (_, long_full_peak) = get_total_full(f'jinaai://{DA_NAME_LONG}')
+
+    assert (
+        short_total_stream == short_total_full
+    ), 'Streamed and non-streamed pull should have similar statistics'
+    assert (
+        long_total_stream == long_total_full
+    ), 'Streamed and non-streamed pull should have similar statistics'
+
+    assert (
+        abs(long_stream_peak - short_stream_peak) / short_stream_peak < TOLERANCE_RATIO
+    ), 'Streamed memory usage should not be dependent on the size of the data'
+    assert (
+        abs(long_full_peak - short_full_peak) / short_full_peak > TOLERANCE_RATIO
+    ), 'Full pull memory usage should be dependent on the size of the data'
+
+
+def test_list_and_delete():
+    DA_NAME_0 = f'test{RANDOM}-list-and-delete-da0'
+    DA_NAME_1 = f'test{RANDOM}-list-and-delete-da1'
+
+    da_names = list(
+        filter(
+            lambda x: x.startswith(f'test{RANDOM}-list-and-delete'),
+            DocumentArray.list('jinaai://', show_table=False),
+        )
+    )
+    assert len(da_names) == 0
+
+    DocumentArray[TextDoc].push(
+        get_test_da(DA_LEN), f'jinaai://{DA_NAME_0}', show_progress=False
+    )
+    da_names = list(
+        filter(
+            lambda x: x.startswith(f'test{RANDOM}-list-and-delete'),
+            DocumentArray.list('jinaai://', show_table=False),
+        )
+    )
+    assert set(da_names) == {DA_NAME_0}
+    DocumentArray[TextDoc].push(
+        get_test_da(DA_LEN), f'jinaai://{DA_NAME_1}', show_progress=False
+    )
+    da_names = list(
+        filter(
+            lambda x: x.startswith(f'test{RANDOM}-list-and-delete'),
+            DocumentArray.list('jinaai://', show_table=False),
+        )
+    )
+    assert set(da_names) == {DA_NAME_0, DA_NAME_1}
+
+    assert DocumentArray.delete(
+        f'jinaai://{DA_NAME_0}'
+    ), 'Deleting an existing DA should return True'
+    da_names = list(
+        filter(
+            lambda x: x.startswith(f'test{RANDOM}-list-and-delete'),
+            DocumentArray.list('jinaai://', show_table=False),
+        )
+    )
+    assert set(da_names) == {DA_NAME_1}
+
+    with pytest.raises(
+        hubble.excepts.RequestedEntityNotFoundError
+    ):  # Deleting a non-existent DA without safety should raise an error
+        DocumentArray.delete(f'jinaai://{DA_NAME_0}')
+    assert not DocumentArray.delete(
+        f'jinaai://{DA_NAME_0}', missing_ok=True
+    ), 'Deleting a non-existent DA should return False'
+
+
+def test_concurrent_push_pull():
+    # Push to DA that is being pulled should not mess up the pull
+    DA_NAME_0 = f'test{RANDOM}-concurrent-push-pull-da0'
+
+    DocumentArray[TextDoc].push_stream(
+        gen_text_docs(DA_LEN),
+        f'jinaai://{DA_NAME_0}',
+        show_progress=False,
+    )
+
+    global _task
+
+    def _task(choice: str):
+        if choice == 'push':
+            DocumentArray[TextDoc].push_stream(
+                gen_text_docs(DA_LEN),
+                f'jinaai://{DA_NAME_0}',
+                show_progress=False,
+            )
+        elif choice == 'pull':
+            pull_len = sum(
+                1 for _ in DocumentArray[TextDoc].pull_stream(f'jinaai://{DA_NAME_0}')
+            )
+            assert pull_len == DA_LEN
+        else:
+            raise ValueError(f'Unknown choice {choice}')
+
+    with mp.get_context('fork').Pool(3) as p:
+        p.map(_task, ['pull', 'push', 'pull'])
+
+
+@pytest.mark.skip(reason='Not Applicable')
+def test_concurrent_push():
+    """
+    Amazon S3 does not support object locking for concurrent writers.
+    If two PUT requests are simultaneously made to the same key, the request with the latest timestamp wins.
+    However, there is no way for the processes to know if they are the latest or not.
+
+    https://docs.aws.amazon.com/AmazonS3/latest/userguide/Welcome.html#ConsistencyModel
+    """
+    pass
