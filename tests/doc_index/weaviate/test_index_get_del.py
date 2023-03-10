eedfe7dccb21bfb537f7ea65ad9d4ba2a911893f
hsm207 <hsm207@users.noreply.github.com>
2023-03-10 21:56:23 +0000

    refactor: reformat code
    Signed-off-by: hsm207 <hsm207@users.noreply.github.com>

--- tests/doc_index/weaviate/test_index_get_del.py
+++ tests/doc_index/weaviate/test_index_get_del.py
@@ -1,16 +1,20 @@
-from docarray.doc_index.backends.weaviate_doc_index import WeaviateDocumentIndex
-from docarray import BaseDocument
-from docarray.typing import NdArray
-from pydantic import Field
-import pytest
 import numpy as np
+import pytest
+from pydantic import Field
+
+from docarray import BaseDocument
+from docarray.doc_index.backends.weaviate_doc_index import WeaviateDocumentIndex
+from docarray.typing import NdArray
+
 
 class SimpleDoc(BaseDocument):
     tens: NdArray[10] = Field(dim=1000)
 
+
 @pytest.fixture
 def ten_simple_docs():
     return [SimpleDoc(tens=np.random.randn(10)) for _ in range(10)]
+
 
 def test_index_simple_schema(ten_simple_docs):
     store = WeaviateDocumentIndex[SimpleDoc]()
