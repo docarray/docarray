ca2a73bdb3fba9164af6b4c8c2b91922f899ec0e
samsja <sami.jaghouar@hotmail.fr>
2023-03-15 18:49:35 +0100

    fix: fix import
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- docarray/__init__.py
+++ docarray/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.30.0a3'
 
-from docarray.array.array.array import DocumentArray, DocumentArrayStacked
+from docarray.array.array import DocumentArray, DocumentArrayStacked
 from docarray.base_document.document import BaseDocument
 
 __all__ = ['BaseDocument', 'DocumentArray', 'DocumentArrayStacked']
