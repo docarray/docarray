74c1890995f2e2cbe9d20d280d3f6dca11a98d43
hsm207 <hsm207@users.noreply.github.com>
2023-03-15 10:05:40 +0000

    test: add fixture for weaviate client
    Signed-off-by: hsm207 <hsm207@users.noreply.github.com>

--- tests/doc_index/weaviate/test_index_get_del.py
+++ tests/doc_index/weaviate/test_index_get_del.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+import weaviate
 from pydantic import Field
 
 from docarray import BaseDocument
@@ -8,12 +9,20 @@ from docarray.typing import NdArray
 
 
 class SimpleDoc(BaseDocument):
-    tens: NdArray[10] = Field(dim=1000)
+    tens: NdArray[10] = Field(dim=1000, hsnw=True)
 
 
 @pytest.fixture
 def ten_simple_docs():
     return [SimpleDoc(tens=np.random.randn(10)) for _ in range(10)]
+
+
+@pytest.fixture
+def weaviate_client():
+    client = weaviate.Client("http://weaviate:8080")
+    client.schema.delete_all()
+    yield client
+    client.schema.delete_all()
 
 
 def test_index_simple_schema(ten_simple_docs):
