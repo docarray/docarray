e9ab146433c05408a16adef88fbec1f6f3346276
jupyterjazz <saba.sturua@jina.ai>
2023-03-20 09:38:37 +0100

    feat: add minimal logger
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>

--- docarray/__init__.py
+++ docarray/__init__.py
@@ -7,3 +7,14 @@ __all__ = [
     'BaseDocument',
     'DocumentArray',
 ]
+
+import logging
+
+logger = logging.getLogger('docarray')
+
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(levelname)s - %(name)s - %(message)s")
+
+handler.setFormatter(formatter)
+logger.addHandler(handler)
