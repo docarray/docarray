12cd7fee9b2f96e0ae8e62385b1a8106a02d4552
samsja <55492238+samsja@users.noreply.github.com>
2023-03-28 17:03:08 +0200

    fix: docstring polish typing (#1299)
    * wip
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: cleanup namespace utils
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * feat: add docstring test
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: fix video url docstring
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: fix text url
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: fix image url
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: fic audio url
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: mesh 3d url
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: mesh 3d url
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: remove useless data
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: fix docstring ndarray and torch tensor
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: fix docstring ndarray and torch tensor
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: fix fix audio url and audio ndarray
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: fix fix audio url and audio ndarray
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: fix video tensor
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: fix video tensor
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: fix audio bytes
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * fix: video and image bytes
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    * docs: move typing section
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    
    ---------
    
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>
    Signed-off-by: RStar2022 <risingstar2880@gmail.com>

--- docarray/typing/tensor/image/image_tensorflow_tensor.py
+++ docarray/typing/tensor/image/image_tensorflow_tensor.py
@@ -18,31 +18,32 @@ class ImageTensorFlowTensor(
     optimized to send over the wire
 
 
-    EXAMPLE USAGE
+    ---
 
-    .. code-block:: python
+    ```python
+    from typing import Optional
 
-        from typing import Optional
-
-        from docarray import BaseDoc
-        from docarray.typing import ImageTensorFlowTensor, ImageUrl
-
-
-        class MyImageDoc(BaseDoc):
-            title: str
-            tensor: Optional[ImageTensorFlowTensor]
-            url: Optional[ImageUrl]
-            bytes: Optional[bytes]
+    from docarray import BaseDoc
+    from docarray.typing import ImageTensorFlowTensor, ImageUrl
 
 
-        doc = MyImageDoc(
-            title='my_second_image_doc',
-            url="https://upload.wikimedia.org/wikipedia/commons/8/80/"
-            "Dag_Sebastian_Ahlander_at_G%C3%B6teborg_Book_Fair_2012b.jpg",
-        )
-        doc.tensor = doc.url.load()
-        doc.bytes = doc.tensor.to_bytes()
+    class MyImageDoc(BaseDoc):
+        title: str
+        tensor: Optional[ImageTensorFlowTensor]
+        url: Optional[ImageUrl]
+        bytes: Optional[bytes]
 
+
+    doc = MyImageDoc(
+        title='my_second_image_doc',
+        url="https://upload.wikimedia.org/wikipedia/commons/8/80/"
+        "Dag_Sebastian_Ahlander_at_G%C3%B6teborg_Book_Fair_2012b.jpg",
+    )
+    doc.tensor = doc.url.load()
+    doc.bytes = doc.tensor.to_bytes()
+    ```
+
+    ---
     """
 
     ...
