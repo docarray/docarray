bf055c5ed149cd0f9abae7ff6ea23a2c1d95c998
anna-charlotte <charlotte.gerhaher@jina.ai>
2023-03-29 11:34:55 +0200

    fix: try short version in typing init getattr
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>

--- docarray/typing/__init__.py
+++ docarray/typing/__init__.py
@@ -55,62 +55,63 @@ __all__ = [
 
 
 def __getattr__(name: str):
-    # if 'Torch' in name:
-    #     import_library('torch', raise_error=True)
-    # elif 'TensorFlow' in name:
-    #     import_library('tensorflow', raise_error=True)
-    #
-    # from docarray.typing import tensor
-    #
-    # T = getattr(tensor, name)
-    # if name not in __all__:
-    #     __all__.append(name)
-    # return T
-
-    if name not in __all__:
-        __all__.append(name)
     if 'Torch' in name:
         import_library('torch', raise_error=True)
-
-        if name == 'TorchTensor':
-            from docarray.typing.tensor import TorchTensor  # noqa: F811
-
-            return TorchTensor
-        elif name == 'TorchEmbedding':
-            from docarray.typing.tensor import TorchEmbedding  # noqa: F811
-
-            return TorchEmbedding
-        elif name == 'AudioTorchTensor':
-            from docarray.typing.tensor.audio import AudioTorchTensor  # noqa: F811
-
-            return AudioTorchTensor
-        elif name == 'ImageTorchTensor':
-            from docarray.typing.tensor.image import ImageTorchTensor  # noqa: F811
-
-            return ImageTorchTensor
-        elif name == 'VideoTorchTensor':
-            from docarray.typing.tensor.video import VideoTorchTensor  # noqa: F811
-
-            return VideoTorchTensor
     elif 'TensorFlow' in name:
         import_library('tensorflow', raise_error=True)
-        if name == 'TensorFlowTensor':
-            from docarray.typing.tensor import TensorFlowTensor  # noqa
 
-            return TensorFlowTensor
-        elif name == 'TensorFlowEmbedding':
-            from docarray.typing.tensor.embedding import TensorFlowEmbedding  # noqa
+    from docarray.typing import tensor
 
-            return TensorFlowEmbedding
-        elif name == 'AudioTensorFlowTensor':
-            from docarray.typing.tensor.audio import AudioTensorFlowTensor  # noqa
+    T = getattr(tensor, name)
+    if name not in __all__:
+        __all__.append(name)
 
-            return AudioTensorFlowTensor
-        elif name == 'ImageTensorFlowTensor':
-            from docarray.typing.tensor.image import ImageTensorFlowTensor  # noqa
+    return T
 
-            return ImageTensorFlowTensor
-        elif name == 'VideoTensorFlowTensor':
-            from docarray.typing.tensor.video import VideoTensorFlowTensor  # noqa
-
-            return VideoTensorFlowTensor
+    # if name not in __all__:
+    #     __all__.append(name)
+    # if 'Torch' in name:
+    #     import_library('torch', raise_error=True)
+    #
+    #     if name == 'TorchTensor':
+    #         from docarray.typing.tensor import TorchTensor  # noqa: F811
+    #
+    #         return TorchTensor
+    #     elif name == 'TorchEmbedding':
+    #         from docarray.typing.tensor import TorchEmbedding  # noqa: F811
+    #
+    #         return TorchEmbedding
+    #     elif name == 'AudioTorchTensor':
+    #         from docarray.typing.tensor.audio import AudioTorchTensor  # noqa: F811
+    #
+    #         return AudioTorchTensor
+    #     elif name == 'ImageTorchTensor':
+    #         from docarray.typing.tensor.image import ImageTorchTensor  # noqa: F811
+    #
+    #         return ImageTorchTensor
+    #     elif name == 'VideoTorchTensor':
+    #         from docarray.typing.tensor.video import VideoTorchTensor  # noqa: F811
+    #
+    #         return VideoTorchTensor
+    # elif 'TensorFlow' in name:
+    #     import_library('tensorflow', raise_error=True)
+    #     if name == 'TensorFlowTensor':
+    #         from docarray.typing.tensor import TensorFlowTensor  # noqa
+    #
+    #         return TensorFlowTensor
+    #     elif name == 'TensorFlowEmbedding':
+    #         from docarray.typing.tensor.embedding import TensorFlowEmbedding  # noqa
+    #
+    #         return TensorFlowEmbedding
+    #     elif name == 'AudioTensorFlowTensor':
+    #         from docarray.typing.tensor.audio import AudioTensorFlowTensor  # noqa
+    #
+    #         return AudioTensorFlowTensor
+    #     elif name == 'ImageTensorFlowTensor':
+    #         from docarray.typing.tensor.image import ImageTensorFlowTensor  # noqa
+    #
+    #         return ImageTensorFlowTensor
+    #     elif name == 'VideoTensorFlowTensor':
+    #         from docarray.typing.tensor.video import VideoTensorFlowTensor  # noqa
+    #
+    #         return VideoTensorFlowTensor
