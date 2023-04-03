2af12d746a58b4096bc129a1908fe53399ea6f44
samsja <sami.jaghouar@hotmail.fr>
2023-04-03 17:30:14 +0200

    fix: add docv vec to init
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- docarray/array/__init__.py
+++ docarray/array/__init__.py
@@ -1,4 +1,5 @@
+from docarray.array.any_array import AnyDocArray
 from docarray.array.doc_list.doc_list import DocList
 from docarray.array.doc_vec.doc_vec import DocVec
 
-__all__ = ['DocList', 'DocVec']
+__all__ = ['DocList', 'DocVec', 'AnyDocArray']
