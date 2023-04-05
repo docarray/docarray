9cee67346d49194a411cb3b7a0f3d918bcf14c34
anna-charlotte <charlotte.gerhaher@jina.ai>
2023-04-05 19:07:14 +0200

    test: add data types section to tests
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>

--- tests/documentation/test_docs.py
+++ tests/documentation/test_docs.py
@@ -7,7 +7,12 @@ from mktestdocs import check_md_file
 # @pytest.mark.parametrize('fpath', pathlib.Path("docs").glob("**/*.md"), ids=str)
 # to use later
 @pytest.mark.parametrize(
-    'fpath', pathlib.Path('docs/user_guide').glob('**/*.md'), ids=str
+    'fpath',
+    [
+        *list(pathlib.Path('docs/user_guide').glob('**/*.md')),
+        *list(pathlib.Path('docs/data_types').glob('**/*.md')),
+    ],
+    ids=str,
 )
 def test_files_good(fpath):
     check_md_file(fpath=fpath, memory=True)
