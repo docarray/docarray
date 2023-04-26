76caae686733f5e2cde637e8e481639b026c18cc
samsja <sami.jaghouar@hotmail.fr>
2023-04-26 09:37:49 +0200

    docs: add BaseDoc docstring
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- docarray/base_doc/doc.py
+++ docarray/base_doc/doc.py
@@ -36,7 +36,28 @@ T_update = TypeVar('T_update', bound='UpdateMixin')
 
 class BaseDoc(BaseModel, IOMixin, UpdateMixin, BaseNode):
     """
-    The base class for Documents
+    BaseDoc is the base class for all Documents. This class should be subclassed
+    to create new Document types with a specific schema.
+
+    The schema of a Document is defined by the fields of the class.
+
+    Example:
+    ```python
+    from docarray import BaseDoc
+    from docarray.typing import NdArray, ImageUrl
+    import numpy as np
+
+
+    class MyDoc(BaseDoc):
+        embedding: NdArray[512]
+        image: ImageUrl
+
+
+    doc = MyDoc(embedding=np.zeros(512), image='https://example.com/image.jpg')
+    ```
+
+
+    BaseDoc is a subclass of [pydantic.BaseModel](https://docs.pydantic.dev/usage/models/) and can be used in a similar way.
     """
 
     id: Optional[ID] = Field(default_factory=lambda: ID(os.urandom(16).hex()))
