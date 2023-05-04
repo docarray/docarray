b4a52a4d76cf22985e0ec0a3f506d8e18a91f042
Jackmin801 <56836461+Jackmin801@users.noreply.github.com>
2023-05-04 15:12:24 +0200

    fix: hardcode high page size
    Signed-off-by: Jackmin801 <56836461+Jackmin801@users.noreply.github.com>

--- docarray/store/jac.py
+++ docarray/store/jac.py
@@ -102,7 +102,7 @@ class JACDocStore(AbstractDocStore):
         from rich.table import Table
 
         resp = HubbleClient(jsonify=True).list_artifacts(
-            filter={'type': 'documentArray'}, sort={'createdAt': 1}
+            filter={'type': 'documentArray'}, sort={'createdAt': 1}, pageSize=10000,
         )
 
         table = Table(
