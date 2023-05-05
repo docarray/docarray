81e94751578a057a606afc59d5f744a3ca7a5120
AnneY <evangeline-lun@foxmail.com>
2023-05-05 14:15:55 +0800

    feat: support subindex for qdrant
    Signed-off-by: AnneY <evangeline-lun@foxmail.com>

--- docarray/index/backends/qdrant.py
+++ docarray/index/backends/qdrant.py
@@ -1,3 +1,4 @@
+import copy
 import uuid
 from dataclasses import dataclass, field
 from typing import (
@@ -21,6 +22,7 @@ from grpc import RpcError  # type: ignore[import]
 
 import docarray.typing.id
 from docarray import BaseDoc, DocList
+from docarray.array.any_array import AnyDocArray
 from docarray.index.abstract import (
     BaseDocIndex,
     _ColumnInfo,
@@ -252,11 +254,23 @@ class QdrantDocumentIndex(BaseDocIndex, Generic[TSchema]):
         try:
             self._client.get_collection(self._db_config.collection_name)
         except (UnexpectedResponse, RpcError, ValueError):
-            vectors_config = {
-                column_name: self._to_qdrant_vector_params(column_info)
-                for column_name, column_info in self._column_infos.items()
-                if column_info.db_type == 'vector'
-            }
+            vectors_config = {}
+
+            for column_name, column_info in self._column_infos.items():
+                if issubclass(column_info.docarray_type, AnyDocArray):
+                    sub_db_config = copy.deepcopy(self._db_config)
+                    sub_db_config.collection_name = (
+                        f'{self._db_config.collection_name}__{column_name}'
+                    )
+                    self._subindices[column_name] = self.__class__[
+                        column_info.docarray_type.doc_type
+                    ](sub_db_config, subindex=True)
+                    continue
+                if column_info.db_type == 'vector':
+                    vectors_config[column_name] = self._to_qdrant_vector_params(
+                        column_info
+                    )
+
             self._client.create_collection(
                 collection_name=self._db_config.collection_name,
                 vectors_config=vectors_config,
@@ -276,6 +290,8 @@ class QdrantDocumentIndex(BaseDocIndex, Generic[TSchema]):
             )
 
     def _index(self, column_to_data: Dict[str, Generator[Any, None, None]]):
+        self._index_subindex(column_to_data)
+
         rows = self._transpose_col_value_dict(column_to_data)
         # TODO: add batching the documents to avoid timeouts
         points = [self._build_point_from_row(row) for row in rows]
@@ -320,7 +336,10 @@ class QdrantDocumentIndex(BaseDocIndex, Generic[TSchema]):
             with_payload=True,
             with_vectors=True,
         )
-        return [self._convert_to_doc(point) for point in response]
+        return sorted(
+            [self._convert_to_doc(point) for point in response],
+            key=lambda x: doc_ids.index(x['id']),
+        )
 
     def execute_query(self, query: Union[Query, RawQuery], *args, **kwargs) -> DocList:
         """
@@ -520,11 +539,29 @@ class QdrantDocumentIndex(BaseDocIndex, Generic[TSchema]):
             ],
         )
 
+    def _filter_by_parent_id(self, id: str) -> Optional[List[str]]:
+        response, _ = self._client.scroll(
+            collection_name=self._db_config.collection_name,
+            scroll_filter=rest.Filter(
+                must=[
+                    rest.FieldCondition(
+                        key='parent_id', match=rest.MatchValue(value=id)
+                    )
+                ]
+            ),
+            with_payload=rest.PayloadSelectorInclude(include=['id']),
+        )
+
+        ids = [point.payload['id'] for point in response]
+        return ids
+
     def _build_point_from_row(self, row: Dict[str, Any]) -> rest.PointStruct:
         point_id = self._to_qdrant_id(row.get('id'))
         vectors: Dict[str, List[float]] = {}
         payload: Dict[str, Any] = {'__generated_vectors': []}
         for column_name, column_info in self._column_infos.items():
+            if issubclass(column_info.docarray_type, AnyDocArray):
+                continue
             if column_info.db_type in ['id', 'payload']:
                 payload[column_name] = row.get(column_name)
                 continue
