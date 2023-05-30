4eec5599bf2905a191e4cc5614a1813e91f4c02f
Mohammad Kalim Akram <kalim.akram@jina.ai>
2023-05-30 11:16:38 +0200

    refactor: make AnyTensor a class (#1552)
    Signed-off-by: Mohammad Kalim Akram <kalim.akram@jina.ai>

--- docarray/typing/tensor/image/image_tensor.py
+++ docarray/typing/tensor/image/image_tensor.py
@@ -1,23 +1,104 @@
-from typing import Union
+from typing import TYPE_CHECKING, Any, Type, TypeVar, Union, cast
 
+import numpy as np
+
+from docarray.typing.tensor.image.abstract_image_tensor import AbstractImageTensor
 from docarray.typing.tensor.image.image_ndarray import ImageNdArray
+from docarray.typing.tensor.tensor import AnyTensor
 from docarray.utils._internal.misc import is_tf_available, is_torch_available
 
 torch_available = is_torch_available()
 if torch_available:
-    from docarray.typing.tensor.image.image_torch_tensor import ImageTorchTensor
+    import torch
 
+    from docarray.typing.tensor.image.image_torch_tensor import ImageTorchTensor
+    from docarray.typing.tensor.torch_tensor import TorchTensor
 
 tf_available = is_tf_available()
 if tf_available:
-    from docarray.typing.tensor.image.image_tensorflow_tensor import (
-        ImageTensorFlowTensor as ImageTFTensor,
-    )
+    import tensorflow as tf  # type: ignore
 
-ImageTensor = Union[ImageNdArray]  # type: ignore
-if tf_available and torch_available:
-    ImageTensor = Union[ImageNdArray, ImageTorchTensor, ImageTFTensor]  # type: ignore
-elif tf_available:
-    ImageTensor = Union[ImageNdArray, ImageTFTensor]  # type: ignore
-elif torch_available:
-    ImageTensor = Union[ImageNdArray, ImageTorchTensor]  # type: ignore
+    from docarray.typing.tensor.image.image_tensorflow_tensor import (
+        ImageTensorFlowTensor,
+    )
+    from docarray.typing.tensor.tensorflow_tensor import TensorFlowTensor
+
+
+if TYPE_CHECKING:
+    from pydantic import BaseConfig
+    from pydantic.fields import ModelField
+
+
+T = TypeVar("T", bound="ImageTensor")
+
+
+class ImageTensor(AnyTensor, AbstractImageTensor):
+    """
+    Represents an image tensor object that can be used with TensorFlow, PyTorch, and NumPy type.
+
+    ---
+    '''python
+    from docarray import BaseDoc
+    from docarray.typing import ImageTensor
+
+
+    class MyImageDoc(BaseDoc):
+        image: ImageTensor
+
+
+    # Example usage with TensorFlow:
+    import tensorflow as tf
+
+    doc = MyImageDoc(image=tf.zeros((1000, 2)))
+    type(doc.image) # ImageTensorFlowTensor
+
+    # Example usage with PyTorch:
+    import torch
+
+    doc = MyImageDoc(image=torch.zeros((1000, 2)))
+    type(doc.image) # ImageTorchTensor
+
+    # Example usage with NumPy:
+    import numpy as np
+
+    doc = MyImageDoc(image=np.zeros((1000, 2)))
+    type(doc.image) # ImageNdArray
+    '''
+    ---
+
+    Returns:
+        Union[ImageTorchTensor, ImageTensorFlowTensor, ImageNdArray]: The validated and converted image tensor.
+
+    Raises:
+        TypeError: If the input type is not one of [torch.Tensor, tensorflow.Tensor, numpy.ndarray].
+    """
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(
+        cls: Type[T],
+        value: Union[T, np.ndarray, Any],
+        field: "ModelField",
+        config: "BaseConfig",
+    ):
+        if torch_available:
+            if isinstance(value, TorchTensor):
+                return cast(ImageTorchTensor, value)
+            elif isinstance(value, torch.Tensor):
+                return ImageTorchTensor._docarray_from_native(value)  # noqa
+        if tf_available:
+            if isinstance(value, TensorFlowTensor):
+                return cast(ImageTensorFlowTensor, value)
+            elif isinstance(value, tf.Tensor):
+                return ImageTensorFlowTensor._docarray_from_native(value)  # noqa
+        try:
+            return ImageNdArray.validate(value, field, config)
+        except Exception:  # noqa
+            pass
+        raise TypeError(
+            f"Expected one of [torch.Tensor, tensorflow.Tensor, numpy.ndarray] "
+            f"compatible type, got {type(value)}"
+        )
--- docarray/typing/tensor/tensor.py
+++ docarray/typing/tensor/tensor.py
@@ -1,22 +1,141 @@
-from typing import Union
+from typing import TYPE_CHECKING, Any, Generic, Type, TypeVar, Union
 
+import numpy as np
+
+from docarray.typing.tensor.abstract_tensor import AbstractTensor
 from docarray.typing.tensor.ndarray import NdArray
-from docarray.utils._internal.misc import is_tf_available, is_torch_available
+from docarray.utils._internal.misc import is_tf_available, is_torch_available  # noqa
 
 torch_available = is_torch_available()
 if torch_available:
-    from docarray.typing.tensor.torch_tensor import TorchTensor  # noqa: F401
+    import torch
 
+    from docarray.typing.tensor.torch_tensor import TorchTensor  # noqa: F401
 
 tf_available = is_tf_available()
 if tf_available:
+    import tensorflow as tf  # type: ignore
+
     from docarray.typing.tensor.tensorflow_tensor import TensorFlowTensor  # noqa: F401
 
 
-AnyTensor = Union[NdArray]
-if torch_available and tf_available:
-    AnyTensor = Union[NdArray, TorchTensor, TensorFlowTensor]  # type: ignore
-elif torch_available:
-    AnyTensor = Union[NdArray, TorchTensor]  # type: ignore
-elif tf_available:
-    AnyTensor = Union[NdArray, TensorFlowTensor]  # type: ignore
+if TYPE_CHECKING:
+    from pydantic import BaseConfig
+    from pydantic.fields import ModelField
+
+    # Below is the hack to make the type checker happy. But `AnyTensor` is defined as a class and with same underlying
+    # behavior as `Union[TorchTensor, TensorFlowTensor, NdArray]` so it should be fine to use `AnyTensor` as
+    # the type for `tensor` field in `BaseDoc` class.
+    AnyTensor = Union[NdArray]
+    if torch_available and tf_available:
+        AnyTensor = Union[NdArray, TorchTensor, TensorFlowTensor]  # type: ignore
+    elif torch_available:
+        AnyTensor = Union[NdArray, TorchTensor]  # type: ignore
+    elif tf_available:
+        AnyTensor = Union[NdArray, TensorFlowTensor]  # type: ignore
+
+else:
+
+    T = TypeVar("T", bound="AnyTensor")
+    ShapeT = TypeVar('ShapeT')
+
+    class AnyTensor(AbstractTensor, Generic[ShapeT]):
+        """
+        Represents a tensor object that can be used with TensorFlow, PyTorch, and NumPy type.
+
+        ---
+        '''python
+        from docarray import BaseDoc
+        from docarray.typing import AnyTensor
+
+
+        class MyTensorDoc(BaseDoc):
+            tensor: AnyTensor
+
+
+        # Example usage with TensorFlow:
+        import tensorflow as tf
+
+        doc = MyTensorDoc(tensor=tf.zeros(1000, 2))
+
+        # Example usage with PyTorch:
+        import torch
+
+        doc = MyTensorDoc(tensor=torch.zeros(1000, 2))
+
+        # Example usage with NumPy:
+        import numpy as np
+
+        doc = MyTensorDoc(tensor=np.zeros((1000, 2)))
+        '''
+        ---
+
+        Returns:
+            Union[TorchTensor, TensorFlowTensor, NdArray]: The validated and converted tensor.
+
+        Raises:
+            TypeError: If the input value is not a compatible type (torch.Tensor, tensorflow.Tensor, numpy.ndarray).
+
+        """
+
+        def __getitem__(self: T, item):
+            pass
+
+        def __setitem__(self, index, value):
+            pass
+
+        def __iter__(self):
+            pass
+
+        def __len__(self):
+            pass
+
+        @classmethod
+        def _docarray_from_native(cls: Type[T], value: Any):
+            raise RuntimeError(f'This method should not be called on {cls}.')
+
+        @staticmethod
+        def get_comp_backend():
+            raise RuntimeError('This method should not be called on AnyTensor.')
+
+        def to_protobuf(self):
+            raise RuntimeError(f'This method should not be called on {self.__class__}.')
+
+        def _docarray_to_json_compatible(self):
+            raise RuntimeError(f'This method should not be called on {self.__class__}.')
+
+        @classmethod
+        def from_protobuf(cls: Type[T], pb_msg: T):
+            raise RuntimeError(f'This method should not be called on {cls}.')
+
+        @classmethod
+        def __get_validators__(cls):
+            yield cls.validate
+
+        @classmethod
+        def validate(
+            cls: Type[T],
+            value: Union[T, np.ndarray, Any],
+            field: "ModelField",
+            config: "BaseConfig",
+        ):
+            # Check for TorchTensor first, then TensorFlowTensor, then NdArray
+            if torch_available:
+                if isinstance(value, TorchTensor):
+                    return value
+                elif isinstance(value, torch.Tensor):
+                    return TorchTensor._docarray_from_native(value)  # noqa
+            if tf_available:
+                if isinstance(value, TensorFlowTensor):
+                    return value
+                elif isinstance(value, tf.Tensor):
+                    return TensorFlowTensor._docarray_from_native(value)  # noqa
+            try:
+                return NdArray.validate(value, field, config)
+            except Exception as e:  # noqa
+                print(e)
+                pass
+            raise TypeError(
+                f"Expected one of [torch.Tensor, tensorflow.Tensor, numpy.ndarray] "
+                f"compatible type, got {type(value)}"
+            )
