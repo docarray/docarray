5e45c797970fd7cc494152237413a159e5a050bc
samsja <sami.jaghouar@hotmail.fr>
2023-05-31 10:46:48 +0200

    fix: better test
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- tests/units/typing/tensor/test_tensor_coercion.py
+++ tests/units/typing/tensor/test_tensor_coercion.py
@@ -1,10 +1,29 @@
 import numpy as np
 import pytest
-import tensorflow as tf
 import torch
 from pydantic import parse_obj_as
 
-from docarray.typing import NdArray, TensorFlowTensor, TorchTensor
+from docarray.typing import NdArray, TorchTensor
+from docarray.utils._internal.misc import is_tf_available
+
+tf_available = is_tf_available()
+if tf_available:
+    import tensorflow as tf
+
+    from docarray.typing import TensorFlowTensor
+else:
+
+    ### This is needed to fake the import of tensorflow when it is not installed
+    class TfNotInstalled:
+        def zeros(self, *args, **kwargs):
+            return 0
+
+    class TensorFlowTensor:
+        def _docarray_from_native(self, *args, **kwargs):
+            return 0
+
+    tf = TfNotInstalled()
+
 
 pure_tensor_to_test = [
     np.zeros((3, 224, 224)),
@@ -26,6 +45,6 @@ def test_torch_tensor_coerse(tensor_cls, tensor):
     t = parse_obj_as(tensor_cls, tensor)
     assert isinstance(t, tensor_cls)
 
-    t_numpy = t._docarray_to_numpy()
+    t_numpy = t._docarray_to_ndarray()
     assert t_numpy.shape == (3, 224, 224)
     assert (t_numpy == np.zeros((3, 224, 224))).all()
