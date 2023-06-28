a52cdfde72d8dd41867e1735aeb33e1e0e9ad608
jupyterjazz <saba.sturua@jina.ai>
2023-06-28 13:28:32 +0200

    docs: add docstrings
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>

--- docarray/index/backends/redis.py
+++ docarray/index/backends/redis.py
@@ -70,6 +70,7 @@ VALID_TEXT_SCORERS = [
 
 class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
     def __init__(self, db_config=None, **kwargs):
+        """Initialize RedisDocumentIndex"""
         super().__init__(db_config=db_config, **kwargs)
         self._db_config = cast(RedisDocumentIndex.DBConfig, self._db_config)
 
@@ -89,24 +90,23 @@ class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
         self._logger.info(f'{self.__class__.__name__} has been initialized')
 
     @staticmethod
-    def _random_name():
+    def _random_name() -> str:
+        """Generate a random index name."""
         return uuid.uuid4().hex
 
-    def _create_index(self):
+    def _create_index(self) -> None:
+        """Create a new index in the Redis database if it doesn't already exist."""
         if not self._check_index_exists(self._db_config.index_name):  # type: ignore[arg-type]
             schema = []
             for column, info in self._column_infos.items():
                 if info.db_type == VectorField:
                     space = info.config.get('space') or info.config.get('distance')
-                    for valid_dist in VALID_DISTANCES:
-                        if space.upper() == valid_dist:  # type: ignore[union-attr]
-                            space = valid_dist
-                    if not space:
+                    space = space.upper()
+                    if space not in VALID_DISTANCES:
                         raise ValueError(
                             f"Invalid distance metric '{space}' provided. "
                             f"Must be one of: {', '.join(VALID_DISTANCES)}"
                         )
-
                     attributes = {
                         'TYPE': 'FLOAT32',
                         'DIM': info.n_dim or info.config.get('dim'),
@@ -151,13 +151,18 @@ class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
             )
 
     def _check_index_exists(self, index_name: str) -> bool:
-        """Check if Redis index exists."""
+        """
+        Check if an index exists in the Redis database.
+
+        :param index_name: The name of the index.
+        :return: True if the index exists, False otherwise.
+        """
         try:
             self._client.ft(index_name).info()
         except:  # noqa: E722
-            self._logger.info("Index does not exist")
+            self._logger.info(f'Index {index_name} does not exist')
             return False
-        self._logger.info("Index already exists")
+        self._logger.info(f'Index {index_name} already exists')
         return True
 
     class QueryBuilder(BaseDocIndex.QueryBuilder):
@@ -179,8 +184,18 @@ class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
 
     @dataclass
     class DBConfig(BaseDocIndex.DBConfig):
-        """Dataclass that contains all "static" configurations of RedisDocumentIndex."""
+        """Dataclass that contains all "static" configurations of RedisDocumentIndex.
 
+        :param host: The host address for the Redis server. Default is 'localhost'.
+        :param port: The port number for the Redis server. Default is 6379.
+        :param index_name: The name of the index in the Redis database.
+            In case it's not provided, a random index name will be generated.
+        :param username: The username for the Redis server. Default is None.
+        :param password: The password for the Redis server. Default is None.
+        :param text_scorer: The method for scoring text during text search.
+            Default is 'BM25'.
+        :param default_column_config: Default configuration for columns.
+        """
         host: str = 'localhost'
         port: int = 6379
         index_name: Optional[str] = None
@@ -225,6 +240,12 @@ class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
         )
 
     def python_type_to_db_type(self, python_type: Type) -> Any:
+        """
+        Map python types to corresponding Redis types.
+
+        :param python_type: Python type.
+        :return: Corresponding Redis type.
+        """
         type_map = {
             int: NumericField,
             float: NumericField,
@@ -273,32 +294,57 @@ class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
 
             yield item_dict
 
-    def _index(self, column_to_data: Dict[str, Generator[Any, None, None]]):
+    def _index(self, column_to_data: Dict[str, Generator[Any, None, None]]) -> List[str]:
+        """
+        Indexes the given data into Redis.
+
+        :param column_to_data: A dictionary where each key is a column and each value is a generator.
+        :return: A list of document ids that have been indexed.
+        """
         ids = []
         for item in self._generate_item(column_to_data):
             ids.append(item['id'])
             doc_id = self._prefix + item['id']
             self._client.json().set(doc_id, '$', item)
-
-        num_docs = self.num_docs()
-        print('indexed', num_docs)
         return ids
 
     def num_docs(self) -> int:
+        """
+        Fetch the number of documents in the index.
+
+        :return: Number of documents in the index.
+        """
         num_docs = self._client.ft(self._db_config.index_name).info()['num_docs']  # type: ignore[arg-type]
         return int(num_docs)
 
-    def _del_items(self, doc_ids: Sequence[str]):
+    def _del_items(self, doc_ids: Sequence[str]) -> None:
+        """
+        Deletes documents from the index based on document ids.
+
+        :param doc_ids: A sequence of document ids to be deleted.
+        """
         doc_ids = [self._prefix + id for id in doc_ids if self._doc_exists(id)]
         if doc_ids:
             self._client.delete(*doc_ids)
 
-    def _doc_exists(self, doc_id):
-        return self._client.exists(self._prefix + doc_id)
+    def _doc_exists(self, doc_id) -> bool:
+        """
+        Checks if a document exists in the index.
+
+        :param doc_id: The id of the document.
+        :return: True if the document exists, False otherwise.
+        """
+        return bool(self._client.exists(self._prefix + doc_id))
 
     def _get_items(
         self, doc_ids: Sequence[str]
     ) -> Union[Sequence[TSchema], Sequence[Dict[str, Any]]]:
+        """
+        Fetches the documents from the index based on document ids.
+
+        :param doc_ids: A sequence of document ids.
+        :return: A sequence of documents from the index.
+        """
         if not doc_ids:
             return []
 
@@ -313,6 +359,12 @@ class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
         return docs
 
     def execute_query(self, query: Any, *args: Any, **kwargs: Any) -> Any:
+        """
+        Executes a hybrid query on the index.
+
+        :param query: Query to execute on the index.
+        :return: Query results.
+        """
         components: Dict[str, List[Dict[str, Any]]] = {}
         for component, value in query:
             if component not in components:
@@ -347,7 +399,16 @@ class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
 
     def _hybrid_search(
         self, query: np.ndarray, filter_query: str, search_field: str, limit: int
-    ):
+    ) -> _FindResult:
+        """
+        Conducts a hybrid search (a combination of vector search and filter-based search) on the index.
+
+        :param query: The query to search.
+        :param filter_query: The filter condition.
+        :param search_field: The vector field to search on.
+        :param limit: The maximum number of results to return.
+        :return: Query results.
+        """
         redis_query = (
             Query(f'{filter_query}=>[KNN {limit} @{search_field} $vec AS vector_score]')
             .sort_by('vector_score')
@@ -376,6 +437,14 @@ class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
     def _find(
         self, query: np.ndarray, limit: int, search_field: str = ''
     ) -> _FindResult:
+        """
+        Conducts a search on the index.
+
+        :param query: The vector query to search.
+        :param limit: The maximum number of results to return.
+        :param search_field: The field to search the query.
+        :return: Search results.
+        """
         return self._hybrid_search(
             query=query, filter_query='*', search_field=search_field, limit=limit
         )
@@ -383,6 +452,14 @@ class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
     def _find_batched(
         self, queries: np.ndarray, limit: int, search_field: str = ''
     ) -> _FindResultBatched:
+        """
+        Conducts a batched search on the index.
+
+        :param queries: The queries to search.
+        :param limit: The maximum number of results to return for each query.
+        :param search_field: The field to search the queries.
+        :return: Search results.
+        """
         docs, scores = [], []
         for query in queries:
             results = self._find(query=query, search_field=search_field, limit=limit)
@@ -392,6 +469,13 @@ class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
         return _FindResultBatched(documents=docs, scores=scores)
 
     def _filter(self, filter_query: Any, limit: int) -> Union[DocList, List[Dict]]:
+        """
+        Filters the index based on the given filter query.
+
+        :param filter_query: The filter condition.
+        :param limit: The maximum number of results to return.
+        :return: Filter results.
+        """
         q = Query(filter_query)
         q.paging(0, limit)
 
@@ -402,6 +486,13 @@ class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
     def _filter_batched(
         self, filter_queries: Any, limit: int
     ) -> Union[List[DocList], List[List[Dict]]]:
+        """
+        Filters the index based on the given batch of filter queries.
+
+        :param filter_queries: The filter conditions.
+        :param limit: The maximum number of results to return for each filter query.
+        :return: Filter results.
+        """
         results = []
         for query in filter_queries:
             results.append(self._filter(filter_query=query, limit=limit))
@@ -410,6 +501,14 @@ class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
     def _text_search(
         self, query: str, limit: int, search_field: str = ''
     ) -> _FindResult:
+        """
+        Conducts a text-based search on the index.
+
+        :param query: The query to search.
+        :param limit: The maximum number of results to return.
+        :param search_field: The field to search the query.
+        :return: Search results.
+        """
         query_str = '|'.join(query.split(' '))
         q = (
             Query(f'@{search_field}:{query_str}')
@@ -431,6 +530,14 @@ class RedisDocumentIndex(BaseDocIndex, Generic[TSchema]):
     def _text_search_batched(
         self, queries: Sequence[str], limit: int, search_field: str = ''
     ) -> _FindResultBatched:
+        """
+        Conducts a batched text-based search on the index.
+
+        :param queries: The queries to search.
+        :param limit: The maximum number of results to return for each query.
+        :param search_field: The field to search the queries.
+        :return: Search results.
+        """
         docs, scores = [], []
         for query in queries:
             results = self._text_search(
