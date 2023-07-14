963c1b3c5c07a3198faf7c3b8d00b7670d287391
agaraman0 <agaraman0@gmail.com>
2023-07-14 18:29:08 +0530

    fix: test_jax_integration changes
    Signed-off-by: agaraman0 <agaraman0@gmail.com>

--- tests/integrations/array/test_jax_integration.py
+++ tests/integrations/array/test_jax_integration.py
@@ -1,27 +1,28 @@
 from typing import Optional
 
-import jax.numpy as jnp
 import pytest
-from jax import jit
 
 from docarray import BaseDoc, DocList
-from docarray.typing import JaxArray
+from docarray.utils._internal.misc import is_jax_available
 
+if is_jax_available():
+    import jax.numpy as jnp
+    from jax import jit
 
-class Mmdoc(BaseDoc):
-    tensor: Optional[JaxArray[3, 224, 224]]
-
-
-def basic_jax_fn(x):
-    return jnp.sum(x)
-
-
-def abstract_JaxArray(array: JaxArray) -> jnp.ndarray:
-    return array.tensor
+    from docarray.typing import JaxArray
 
 
 @pytest.mark.jax
 def test_basic_jax_operation():
+    def basic_jax_fn(x):
+        return jnp.sum(x)
+
+    def abstract_JaxArray(array: JaxArray) -> jnp.ndarray:
+        return array.tensor
+
+    class Mmdoc(BaseDoc):
+        tensor: Optional[JaxArray[3, 224, 224]]
+
     N = 10
 
     batch = DocList[Mmdoc](Mmdoc() for _ in range(N))
