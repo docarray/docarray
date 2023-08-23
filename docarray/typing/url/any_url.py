efcc87743bf02c99c098aaaaf9c14ab8d23edfda
samsja <sami.jaghouar@hotmail.fr>
2023-08-23 14:02:27 +0200

    fix: fix some tests regarding anyurl

--- docarray/typing/url/any_url.py
+++ docarray/typing/url/any_url.py
@@ -166,17 +166,6 @@ else:
 
             return extension in cls.extra_extensions()
 
-        def _to_node_protobuf(self) -> 'NodeProto':
-            """Convert Document into a NodeProto protobuf message. This function should
-            be called when the Document is nested into another Document that need to
-            be converted into a protobuf
-
-            :return: the nested item protobuf message
-            """
-            from docarray.proto import NodeProto
-
-            return NodeProto(text=str(self), type=self._proto_type_name)
-
         @classmethod
         def validate(
             cls: Type[T],
@@ -200,19 +189,12 @@ else:
 
             url = super().validate(abs_path, field, config)  # basic url validation
 
-            if input_is_relative_path:
-                return cls(str(value), scheme=None)
-            else:
-                return cls(str(url), scheme=None)
+            if not cls.is_extension_allowed(value):
+                raise ValueError(
+                    f"The file '{value}' is not in a valid format for class '{cls.__name__}'."
+                )
 
-        @classmethod
-        def from_protobuf(cls: Type[T], pb_msg: 'str') -> T:
-            """
-            Read url from a proto msg.
-            :param pb_msg:
-            :return: url
-            """
-            return parse_obj_as(cls, pb_msg)
+            return cls(str(value if input_is_relative_path else url), scheme=None)
 
         @classmethod
         def validate_parts(cls, parts: 'Parts', validate_port: bool = True) -> 'Parts':
@@ -277,3 +259,31 @@ else:
                 # remove the `://` prefix, since scheme is missing
                 url = url[3:]
             return url
+
+        @classmethod
+        def from_protobuf(cls: Type[T], pb_msg: 'str') -> T:
+            """
+            Read url from a proto msg.
+            :param pb_msg:
+            :return: url
+            """
+            return parse_obj_as(cls, pb_msg)
+
+        def load_bytes(self, timeout: Optional[float] = None) -> bytes:
+            """Convert url to bytes. This will either load or download the file and save
+            it into a bytes object.
+            :param timeout: timeout for urlopen. Only relevant if URI is not local
+            :return: bytes.
+            """
+            if urllib.parse.urlparse(self).scheme in {'http', 'https', 'data'}:
+                req = urllib.request.Request(
+                    self, headers={'User-Agent': 'Mozilla/5.0'}
+                )
+                urlopen_kwargs = {'timeout': timeout} if timeout is not None else {}
+                with urllib.request.urlopen(req, **urlopen_kwargs) as fp:  # type: ignore
+                    return fp.read()
+            elif os.path.exists(self):
+                with open(self, 'rb') as fp:
+                    return fp.read()
+            else:
+                raise FileNotFoundError(f'`{self}` is not a URL or a valid local path')
