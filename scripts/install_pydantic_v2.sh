4e01dc0a9de1bf890d167ca0564017feaa36642a
samsja <sami.jaghouar@hotmail.fr>
2023-08-29 12:12:14 +0200

    chore: install v2 in c

---
+++ scripts/install_pydantic_v2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ONLY NEEDED IN CI
+
+# Get the input variable
+input_variable=$1
+
+# Check if the input variable is "true"
+if [ "$input_variable" == "true" ]; then
+  echo "Installing or updating pydantic..."
+  poetry run pip install -U pydantic
+else
+  echo "Skipping installation of pydantic."
+fi
