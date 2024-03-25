028f1beff9818ce8a3a37fc60eac68c1834a7758
Emanuel Lupi <emanuel.lupi91@gmail.com>
2024-03-24 22:11:02 -0300

    Add test filter.
    Signed-off-by: Casey Clements <casey.clements@mongodb.com>

---
+++ tests/index/mongo_atlas/test_filter.py
@@ -0,0 +1,25 @@
+from .fixtures import *  # noqa
+
+
+def test_filter(simple_index_with_docs):
+
+    db, base_docs = simple_index_with_docs
+
+    docs = db.filter(filter_query={"number": {"$lt": 1}})
+    assert len(docs) == 1
+    assert docs[0].number == 0
+
+    docs = db.filter(filter_query={"number": {"$gt": 8}})
+    assert len(docs) == 1
+    assert docs[0].number == 9
+
+    docs = db.filter(filter_query={"number": {"$lt": 8, "$gt": 3}})
+    assert len(docs) == 4
+
+    docs = db.filter(filter_query={"text": {"$regex": "introduction"}})
+    assert len(docs) == 1
+    assert 'introduction' in docs[0].text.lower()
+
+    docs = db.filter(filter_query={"text": {"$not": {"$regex": "Explore"}}})
+    assert len(docs) == 9
+    assert all("Explore" not in doc.text for doc in docs)
